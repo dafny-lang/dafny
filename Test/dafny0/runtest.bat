85f2965223f29e1f558170159f5c67414beab817
Jason Koenig <jrkoenig@stanford.edu>
2011-06-28 16:09:34 -0700

    Added regression test for loop modifies clauses.

--- Test/dafny0/runtest.bat
+++ Test/dafny0/runtest.bat
@@ -18,7 +18,7 @@ for %%f in (TypeTests.dfy NatTypes.dfy SmallTests.dfy Definedness.dfy
             Comprehensions.dfy Basics.dfy ControlStructures.dfy
             Termination.dfy DTypes.dfy
             TypeParameters.dfy Datatypes.dfy TypeAntecedents.dfy SplitExpr.dfy
-            Refinement.dfy RefinementErrors.dfy) do (
+            Refinement.dfy RefinementErrors.dfy LoopModifies.dfy) do (
   echo.
   echo -------------------- %%f --------------------
   %DAFNY_EXE% /compile:0 %* %%f
