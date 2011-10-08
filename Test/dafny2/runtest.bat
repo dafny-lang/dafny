40dce9833a47ed1d5b8db9ed16a0086264be9ef1
Rustan Leino <leino@microsoft.com>
2011-10-07 17:28:07 -0700

    Dafny: added COST Verification Competition challenge programs to test suite

--- Test/dafny2/runtest.bat
+++ Test/dafny2/runtest.bat
@@ -5,7 +5,13 @@ set BOOGIEDIR=..\..\Binaries
 set DAFNY_EXE=%BOOGIEDIR%\Dafny.exe
 set CSC=c:/Windows/Microsoft.NET/Framework/v4.0.30319/csc.exe
 
-for %%f in (SnapshotableTrees.dfy TreeBarrier.dfy) do (
+for %%f in (SnapshotableTrees.dfy TreeBarrier.dfy
+    COST-verif-comp-2011-1-MaxArray.dfy
+    COST-verif-comp-2011-2-MaxTree-class.dfy
+    COST-verif-comp-2011-2-MaxTree-datatype.dfy
+    COST-verif-comp-2011-3-TwoDuplicates.dfy
+    COST-verif-comp-2011-4-FloydCycleDetect.dfy
+  ) do (
   echo.
   echo -------------------- %%f --------------------
 
