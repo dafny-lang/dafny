d3eacad37c58e628cb910b5a3ab106f1bd6fdcc8
Rustan Leino <leino@microsoft.com>
2012-10-04 14:14:39 -0700

    Updates of various .sln and .*proj files

---
+++ Source/version.cs
@@ -0,0 +1,12 @@
+// ==++==
+// 
+//   
+// 
+// ==--==
+// Warning: Automatically generated file. DO NOT EDIT
+// Generated at Dienstag, 5. Juli 2011 11:26:45
+
+using System.Reflection;
+[assembly: AssemblyVersion("2.2.30705.1126")]
+[assembly: AssemblyFileVersion("2.2.30705.1126")]
+
