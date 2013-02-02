ee5addcc9dfa771cdec29a2681b0adfc55eaa1f9
Rustan Leino <leino@microsoft.com>
2013-02-02 10:43:30 -0800

    Added some test cases that show exmaples that iterate over set elements.

--- Test/dafny3/runtest.bat
+++ Test/dafny3/runtest.bat
@@ -7,7 +7,7 @@ set DAFNY_EXE=%BINARIES%\Dafny.exe
 for %%f in (
   Iter.dfy Streams.dfy Dijkstra.dfy CachedContainer.dfy
   SimpleInduction.dfy SimpleCoinduction.dfy CalcExample.dfy
-  InductionVsCoinduction.dfy Zip.dfy
+  InductionVsCoinduction.dfy Zip.dfy SetIterations.dfy
 ) do (
   echo.
   echo -------------------- %%f --------------------
