f3c4e65e41711d99f8d6766d96022cbece0b7eba
Rustan Leino <leino@microsoft.com>
2013-03-18 15:02:15 -0700

    Updated version to 1.6.2.00318

--- Source/version.cs
+++ Source/version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
-// Version 1.6.1, year 2013+0 month 03 day 09
-[assembly: AssemblyVersion("1.6.1.00309")]
-[assembly: AssemblyFileVersion("1.6.1.00309")]
+// Version 1.6.2, year 2013+0 month 03 day 18
+[assembly: AssemblyVersion("1.6.2.00318")]
+[assembly: AssemblyFileVersion("1.6.2.00318")]
