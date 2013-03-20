f494fdacba200734469ef7ba55b388fca6445515
Rustan Leino <leino@microsoft.com>
2013-03-20 13:18:22 -0700

    Bumped version to 1.6.3.00320, to appear on rise4fun

--- Source/version.cs
+++ Source/version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
-// Version 1.6.2, year 2013+0 month 03 day 18
-[assembly: AssemblyVersion("1.6.2.00318")]
-[assembly: AssemblyFileVersion("1.6.2.00318")]
+// Version 1.6.3, year 2013+0 month 03 day 20
+[assembly: AssemblyVersion("1.6.3.00320")]
+[assembly: AssemblyFileVersion("1.6.3.00320")]
