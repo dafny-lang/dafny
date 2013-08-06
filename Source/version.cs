e96d33ed15381c33008ed243ee37b799c3c0704b
Valentin Wuestholz <wuestholz@gmail.com>
2013-08-06 14:10:52 -0700

    Bumped version to 1.7.0, to be released as a binary and on rise4fun.

--- Source/version.cs
+++ Source/version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 // Version 1.6.3, year 2013+0 month 03 day 20
-[assembly: AssemblyVersion("1.6.3.00320")]
-[assembly: AssemblyFileVersion("1.6.3.00320")]
+[assembly: AssemblyVersion("1.7.0.00806")]
+[assembly: AssemblyFileVersion("1.7.0.00806")]
