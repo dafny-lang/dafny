1aac4574ae1b58a5680c5d69e863eebe3242d47c
Rustan Leino <leino@microsoft.com>
2013-12-18 16:19:41 -0800

    Added test3/GenericSort.dfy, which shows how modules can be used to write and use a sorting routine parameterized with a comparison function

--- Test/dafny3/runtest.bat
+++ Test/dafny3/runtest.bat
@@ -4,14 +4,14 @@ setlocal
 set BINARIES=..\..\Binaries
 set DAFNY_EXE=%BINARIES%\Dafny.exe
 
-%DAFNY_EXE% /compile:0 /verifySeparately %* Iter.dfy Streams.dfy Dijkstra.dfy CachedContainer.dfy SimpleInduction.dfy SimpleCoinduction.dfy CalcExample.dfy InductionVsCoinduction.dfy Zip.dfy SetIterations.dfy Paulson.dfy Filter.dfy WideTrees.dfy InfiniteTrees.dfy OpaqueTrees.dfy
+%DAFNY_EXE% /compile:0 /verifySeparately %* Iter.dfy Streams.dfy Dijkstra.dfy CachedContainer.dfy SimpleInduction.dfy SimpleCoinduction.dfy CalcExample.dfy InductionVsCoinduction.dfy Zip.dfy SetIterations.dfy Paulson.dfy Filter.dfy WideTrees.dfy InfiniteTrees.dfy OpaqueTrees.dfy GenericSort.dfy
 
 rem for %%f in (
 rem   Iter.dfy Streams.dfy Dijkstra.dfy CachedContainer.dfy
 rem   SimpleInduction.dfy SimpleCoinduction.dfy CalcExample.dfy
 rem   InductionVsCoinduction.dfy Zip.dfy SetIterations.dfy
 rem   Paulson.dfy Filter.dfy WideTrees.dfy InfiniteTrees.dfy
-rem   OpaqueTrees.dfy
+rem   OpaqueTrees.dfy GenericSort.dfy
 rem ) do (
 rem   echo.
 rem   echo -------------------- %%f --------------------
