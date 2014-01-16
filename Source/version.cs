88579458c00d83b9db79dac5b4c9176da9fe048e
Rustan Leino <leino@microsoft.com>
2014-01-15 20:32:01 -0800

    Version 1.8.0.10115 release candidate

--- Source/version.cs
+++ Source/version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
-// Version 1.6.3, year 2013+0 month 03 day 20
-[assembly: AssemblyVersion("1.7.0.00806")]
-[assembly: AssemblyFileVersion("1.7.0.00806")]
+// Version 1.8.0, year 2013+1 month 01 day 15
+[assembly: AssemblyVersion("1.8.0.10115")]
+[assembly: AssemblyFileVersion("1.8.0.10115")]
