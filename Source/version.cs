c8d03fd30861d820c53b0e27068665e9fb2f848f
Rustan Leino <leino@microsoft.com>
2014-03-24 13:30:03 -0700

    Bumped version to 1.8.1.10324. To go onto rise4fun and as a binary drop.

--- Source/version.cs
+++ Source/version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
-// Version 1.8.0, year 2013+1 month 01 day 15
-[assembly: AssemblyVersion("1.8.0.10115")]
-[assembly: AssemblyFileVersion("1.8.0.10115")]
+// Version 1.8.1, year 2013+1 month 01 day 15
+[assembly: AssemblyVersion("1.8.1.10324")]
+[assembly: AssemblyFileVersion("1.8.1.10324")]
