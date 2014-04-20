f5babc9fca0358ec969402b623c035e1570187ef
Rustan Leino <leino@microsoft.com>
2014-04-19 20:48:16 -0700

    Version 1.8.2.10419, binary release and (except for the "include" update in the previous check-in) rise4fun

--- Source/version.cs
+++ Source/version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
-// Version 1.8.1, year 2013+1 month 01 day 15
-[assembly: AssemblyVersion("1.8.1.10324")]
-[assembly: AssemblyFileVersion("1.8.1.10324")]
+// Version 1.8.2, year 2013+1 month 04 day 19
+[assembly: AssemblyVersion("1.8.2.10419")]
+[assembly: AssemblyFileVersion("1.8.2.10419")]
