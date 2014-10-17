07d735f43bdf99e9723b87a16d5d80467fb07818
Rustan Leino <leino@microsoft.com>
2014-10-16 17:54:08 -0700

    Updated version to 1.9.0.11016.  This version is going on rise4fun.com.

--- Source/version.cs
+++ Source/version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
-// Version 1.8.2, year 2013+1 month 04 day 19
-[assembly: AssemblyVersion("1.8.2.10419")]
-[assembly: AssemblyFileVersion("1.8.2.10419")]
+// Version 1.9.0, year 2013+1 month 10 day 16
+[assembly: AssemblyVersion("1.9.0.11016")]
+[assembly: AssemblyFileVersion("1.9.0.11016")]
