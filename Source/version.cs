6a51b4d1dd22e4b7be45701198d58578f450a7bf
Rustan Leino <leino@microsoft.com>
2014-10-21 23:43:41 -0700

    Changed version to 1.9.1.11022

--- Source/version.cs
+++ Source/version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
-// Version 1.9.0, year 2013+1 month 10 day 21
-[assembly: AssemblyVersion("1.9.1.11021")]
-[assembly: AssemblyFileVersion("1.9.1.11021")]
+// Version 1.9.0, year 2013+1 month 10 day 22
+[assembly: AssemblyVersion("1.9.1.11022")]
+[assembly: AssemblyFileVersion("1.9.1.11022")]
