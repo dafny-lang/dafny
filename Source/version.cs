c651d004f05c50ac4de04efa82b10a0a966ae55c
Rustan Leino <leino@microsoft.com>
2015-01-07 12:58:19 -0800

    Bumped the version number to 1.9.3.20107

--- Source/version.cs
+++ Source/version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
-// Version 1.9.0, year 2013+1 month 11 day 07
-[assembly: AssemblyVersion("1.9.2.11107")]
-[assembly: AssemblyFileVersion("1.9.2.11107")]
+// Version 1.9.3, year 2013+2 month 01 day 07
+[assembly: AssemblyVersion("1.9.3.20107")]
+[assembly: AssemblyFileVersion("1.9.3.20107")]
