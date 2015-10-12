ce43f012067321b2ee145a4cdcf6133a08c731fa
Rustan Leino <leino@microsoft.com>
2015-10-12 11:48:14 -0700

    Version 1.9.6.21012

--- Source/version.cs
+++ Source/version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
-// Version 1.9.5, year 2013+2 month 05 day 11
-[assembly: AssemblyVersion("1.9.5.20511")]
-[assembly: AssemblyFileVersion("1.9.5.20511")]
+// Version 1.9.6, year 2013+2 month 10 day 12
+[assembly: AssemblyVersion("1.9.6.21012")]
+[assembly: AssemblyFileVersion("1.9.6.21012")]
