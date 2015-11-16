75a2f69aea87705e667ef1b4b448ba3ae2bc8357
Rustan Leino <leino@microsoft.com>
2015-11-16 14:46:15 -0800

    Updated version number to 1.9.6.21116. This version is now on rise4fun.

--- Source/version.cs
+++ Source/version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
-// Version 1.9.6, year 2013+2 month 10 day 12
-[assembly: AssemblyVersion("1.9.6.21012")]
-[assembly: AssemblyFileVersion("1.9.6.21012")]
+// Version 1.9.6, year 2013+2 month 11 day 16
+[assembly: AssemblyVersion("1.9.6.21116")]
+[assembly: AssemblyFileVersion("1.9.6.21116")]
