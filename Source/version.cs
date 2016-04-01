5c2fbbd1d99c34e58d551e791469280706d98ab7
Rustan Leino <leino@microsoft.com>
2016-04-01 15:41:57 -0700

    New version number 1.9.7.30401, for binary release on Codeplex and Rise4fun. Changed copyright date to include 2016.

--- Source/version.cs
+++ Source/version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
-// Version 1.9.6, year 2013+2 month 11 day 16
-[assembly: AssemblyVersion("1.9.6.21116")]
-[assembly: AssemblyFileVersion("1.9.6.21116")]
+// Version 1.9.7, year 2013+3 month 04 day 01
+[assembly: AssemblyVersion("1.9.7.30401")]
+[assembly: AssemblyFileVersion("1.9.7.30401")]
