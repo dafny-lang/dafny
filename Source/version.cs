52a92eea963c57691b395f52ed55b54ea6d49961
Rustan Leino <leino@microsoft.com>
2017-04-18 16:29:54 -0700

    Updated version to 1.9.9.40418.
    About to move tag 1.9.9 here and re-release the binaries.

--- Source/version.cs
+++ Source/version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
-// Version 1.9.9, year 2013+4 month 04 day 14
-[assembly: AssemblyVersion("1.9.9.40414")]
-[assembly: AssemblyFileVersion("1.9.9.40414")]
+// Version 1.9.9, year 2013+4 month 04 day 18
+[assembly: AssemblyVersion("1.9.9.40418")]
+[assembly: AssemblyFileVersion("1.9.9.40418")]
