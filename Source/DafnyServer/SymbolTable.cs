d4e43f0bdc7e418151d3f5fc649ade5eac86149e
Markus Schaden <mrksschdn@gmail.com>
2017-05-25 09:57:30 +0200

    Preparation for pull request

--- Source/DafnyServer/SymbolTable.cs
+++ Source/DafnyServer/SymbolTable.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Runtime.Serialization;
-using System.Runtime.Serialization.Json;
-using System.Text;
 using Microsoft.Dafny;
 using Type = Microsoft.Dafny.Type;
 using Microsoft.Boogie;
