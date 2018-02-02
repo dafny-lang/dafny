c7471f41b9ed3b295eee3c66a05dee15f7ab2f52
James Bornholt <bornholt@cs.washington.edu>
2018-02-02 14:55:46 -0800

    Make DafnyServer use the same version number as Dafny
    To make the VSCode extension's auto-updater work

--- Source/DafnyServer/VersionCheck.cs
+++ Source/DafnyServer/VersionCheck.cs
@@ -2,10 +2,9 @@
 
 namespace DafnyServer {
   public class VersionCheck {
-    private static readonly string _currentVersion = "1.9.16";
-
     public static void CurrentVersion() {
-      Console.WriteLine("VERSION:" + _currentVersion);
+      var version = System.Diagnostics.FileVersionInfo.GetVersionInfo(System.Reflection.Assembly.GetExecutingAssembly().Location).FileVersion;
+      Console.WriteLine("VERSION:" + version);
     }
   }
 }
