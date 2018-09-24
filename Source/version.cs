f7b826cc1f217e1f815561aa0d35f5b3f65c5216
RustanLeino <rustan.leino@outlook.com>
2018-09-23 17:52:29 -0700

    Version 2.2.0.10923

--- Source/version.cs
+++ Source/version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
-// Version 2.1.1, year 2017+1 month 02 day 09
-[assembly: AssemblyVersion("2.1.1.10209")]
-[assembly: AssemblyFileVersion("2.1.1.10209")]
+// Version 2.2.0, year 2017+1 month 09 day 23
+[assembly: AssemblyVersion("2.2.0.10923")]
+[assembly: AssemblyFileVersion("2.2.0.10923")]
