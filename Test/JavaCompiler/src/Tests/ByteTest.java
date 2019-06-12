894b5d71da74a9c16480bf2a48af846214ef4f88
Jack Sturtevant <jsturte@amazon.com>
2019-06-12 15:07:33 -0700

    Add support for unsigned numerics
    cr https://code.amazon.com/reviews/CR-9563427

---
+++ Test/JavaCompiler/src/Tests/ByteTest.java
@@ -0,0 +1,79 @@
+import DafnyClasses.DafnyByte;
+import DafnyClasses.DafnyMultiset;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.math.BigInteger;
+import java.util.*;
+
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.assertEquals;
+
+public class ByteTest {
+
+    DafnyByte tenI = new DafnyByte(10);
+    byte tenByte = 10;
+    DafnyByte tenB = new DafnyByte(tenByte);
+    DafnyByte two = new DafnyByte(2);
+    DafnyByte zero = new DafnyByte(0);
+    DafnyByte max = new DafnyByte((byte)0xff);
+
+    @Test
+    public void testComparisons(){
+        assertTrue(tenB.equals(tenI));
+        assertFalse(tenB.equals(max));
+        assertEquals(0xff, max.intValue());
+        assertEquals(0, zero.intValue());
+        assertTrue(tenB.compareTo(zero) > 0);
+        assertTrue(tenB.compareTo(max) < 0);
+        assertTrue(tenB.compareTo(tenI) == 0);
+        assertTrue(DafnyByte.compare(tenB, zero) > 0);
+        assertTrue(DafnyByte.compare(tenB, max) < 0);
+        assertTrue(DafnyByte.compare(tenB, tenI) == 0);
+
+    }
+
+    @Test
+    public void testValues(){
+        float f = 10;
+        double d = 10;
+        int i = 10;
+        long l = 10;
+        assertEquals(f, tenB.floatValue());
+        assertEquals(d, tenB.doubleValue());
+        assertEquals(i, tenB.intValue());
+        assertEquals(l, tenB.longValue());
+        assertEquals(Integer.hashCode(10), tenB.hashCode());
+        assertEquals("10", tenB.toString());
+    }
+
+    @Test
+    public void testArithmetic(){
+        assertEquals(10+2, tenB.add(two).intValue());
+        assertEquals(0xff, max.add(zero).intValue());
+        assertEquals(8, tenB.subtract(two).intValue());
+        assertEquals(0xfd, max.subtract(two).intValue());
+        assertEquals(20, tenB.multiply(two).intValue());
+        assertEquals(0, max.multiply(zero).intValue());
+        assertEquals(5, tenI.divide(two).intValue());
+        assertEquals(0, zero.divide(max).intValue());
+        assertEquals(0, tenB.modulus(two).intValue());
+        assertEquals(0xff%10, max.modulus(tenI).intValue());
+    }
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void testFailures(){
+        thrown.expect(AssertionError.class);
+        DafnyByte fail = new DafnyByte(0xfff);
+        max.add(tenI);
+        zero.subtract(two);
+        max.multiply(tenB);
+        max.divide(zero);
+        max.modulus(zero);
+    }
+}
---
+++ Test/JavaCompiler/src/Tests/ULongTest.java
@@ -0,0 +1,78 @@
+import DafnyClasses.DafnyULong;
+import DafnyClasses.DafnyMultiset;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.math.BigInteger;
+import java.util.*;
+
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.assertEquals;
+
+public class ULongTest {
+
+    DafnyULong tenI = new DafnyULong(10);
+    short tenUShort = 10;
+    DafnyULong tenU = new DafnyULong(tenUShort);
+    DafnyULong two = new DafnyULong(2);
+    DafnyULong zero = new DafnyULong(0);
+    DafnyULong max = new DafnyULong(new BigInteger("18446744073709551615"));
+    DafnyULong large = new DafnyULong(0xfffffffffffffff0l);
+    DafnyULong grande = new DafnyULong(0x0ffffffffffffff0l);
+
+    @Test
+    public void testComparisons(){
+        assertTrue(tenU.equals(tenI));
+        assertFalse(tenU.equals(max));
+        assertTrue(tenU.compareTo(zero) > 0);
+        assertTrue(tenU.compareTo(max) < 0);
+        assertTrue(tenU.compareTo(tenI) == 0);
+        assertTrue(DafnyULong.compare(tenU, zero) > 0);
+        assertTrue(DafnyULong.compare(tenU, max) < 0);
+        assertTrue(DafnyULong.compare(tenU, tenI) == 0);
+
+    }
+
+    @Test
+    public void testValues(){
+        assertEquals(Integer.hashCode(10), tenU.hashCode());
+        assertEquals("10", tenU.toString());
+    }
+
+    @Test
+    public void testArithmetic(){
+        assertEquals(10+2, tenU.add(two).value());
+        assertEquals(DafnyULong.MAXVALUE, max.add(zero).value());
+        assertEquals(0xfffffffffffffffal, large.add(tenU).value());
+        assertEquals(10-2, tenU.subtract(two).value());
+        assertEquals(DafnyULong.MAXVALUE- 2, max.subtract(two).value());
+        assertEquals(0xffffffffffffffe6l, large.subtract(tenU).value());
+        assertEquals(10*2, tenU.multiply(two).value());
+        assertEquals(0, max.multiply(zero).value());
+        assertEquals(10l * 0x0ffffffffffffff0l, grande.multiply(tenU).value());
+        assertEquals(5, tenI.divide(two).value());
+        assertEquals(0, zero.divide(max).value());
+        assertEquals(1, max.divide(max).value());
+        assertEquals(DafnyULong.MAXBI.divide(BigInteger.TEN).longValue(), max.divide(tenU).value());
+        assertEquals(0, tenU.modulus(two).value());
+        assertEquals(DafnyULong.MAXBI.mod(BigInteger.TEN).longValue(), max.modulus(tenI).value());
+        assertEquals(tenU.value(), tenU.modulus(max).value());
+        assertEquals(DafnyULong.MAXBI.divide(BigInteger.TEN).longValue(), max.divide(tenI).value());
+    }
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void testFailures(){
+        thrown.expect(AssertionError.class);
+        DafnyULong fail = new DafnyULong(DafnyULong.MAXBI.add(BigInteger.TWO));
+        max.add(tenI);
+        zero.subtract(two);
+        max.multiply(tenU);
+        max.divide(zero);
+        max.modulus(zero);
+    }
+}
