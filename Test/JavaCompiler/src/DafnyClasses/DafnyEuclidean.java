158c7194f356710b6a56bbe4858d446627034b92
Jack Sturtevant <jsturte@amazon.com>
2019-06-19 16:59:27 -0700

    Add euclidean modulus
    cr https://code.amazon.com/reviews/CR-9811345

--- Test/JavaCompiler/src/DafnyClasses/DafnyEuclidean.java
+++ Test/JavaCompiler/src/DafnyClasses/DafnyEuclidean.java
@@ -6,14 +6,17 @@ public class DafnyEuclidean {
     // pre: b != 0
     // post: result == a/b, as defined by Euclidean Division (http://en.wikipedia.org/wiki/Modulo_operation)
     public static byte EuclideanDivision_sbyte(byte a, byte b) {
+        assert b != 0 : "Precondition Failure";
         return (byte) EuclideanDivision_int(a, b);
     }
 
     public static short EuclideanDivision_short(short a, short b) {
+        assert b != 0 : "Precondition Failure";
         return (short) EuclideanDivision_int(a, b);
     }
 
     public static int EuclideanDivision_int(int a, int b) {
+        assert b != 0 : "Precondition Failure";
         if (0 <= a) {
             if (0 <= b) {
                 // +a +b: a/b
@@ -41,6 +44,7 @@ public class DafnyEuclidean {
     }
 
     public static long EuclideanDivision_long(long a, long b) {
+        assert b != 0 : "Precondition Failure";
         if (0 <= a) {
             if (0 <= b) {
                 // +a +b: a/b
@@ -68,6 +72,7 @@ public class DafnyEuclidean {
     }
 
     public static BigInteger EuclideanDivision(BigInteger a, BigInteger b) {
+        assert b.compareTo(BigInteger.ZERO) != 0 : "Precondition Failure";
         if (0 <= a.signum()) {
             if (0 <= b.signum()) {
                 // +a +b: a/b
@@ -84,6 +89,87 @@ public class DafnyEuclidean {
                 // -a -b: ((-a-1)/(-b)) + 1
                 return a.add(BigInteger.ONE).negate().divide(b.negate()).add(BigInteger.ONE);
             }
+        }
+    }
+
+    // pre: b != 0
+    // post: result == a%b, as defined by Euclidean Division (http://en.wikipedia.org/wiki/Modulo_operation)
+    public static byte EuclideanModulus_byte(byte a, byte b) {
+        assert b != 0 : "Precondition Failure";
+        return (byte) EuclideanModulus_int(a, b);
+    }
+
+    public static short EuclideanModulus_short(short a, short b) {
+        assert b != 0 : "Precondition Failure";
+        return (short) EuclideanModulus_int(a, b);
+    }
+
+    public static int EuclideanModulus_int(int a, int b) {
+        assert b != 0 : "Precondition Failure";
+        if (0 <= a) {
+            // +a: a % b'
+            if (b == Integer.MIN_VALUE) return (int) new DafnyUInt(a).modulus(new DafnyUInt(b)).value();
+            else if (b < 0) return a % -b;
+            else return a % b;
+        } else {
+            // c = ((-a) % b')
+            // -a: b' - c if c > 0
+            // -a: 0 if c == 0
+            if (a == Integer.MIN_VALUE || b == Integer.MIN_VALUE) {
+                if (a == b) return 0;
+                else if (b == Integer.MIN_VALUE) {
+                    return new DafnyUInt(b).subtract(new DafnyUInt(-a).modulus(new DafnyUInt(b))).value();
+                } else {
+                    int bp = b < 0 ? -b : b;
+                    return new DafnyUInt(bp).subtract(new DafnyUInt(a).modulus(new DafnyUInt(bp))).value();
+                }
+            } else {
+                int bp = b < 0 ? -b : b;
+                int c = ((-a) % bp);
+                return c == 0 ? c : bp - c;
+            }
+        }
+    }
+
+    public static long EuclideanModulus_long(long a, long b) {
+        assert b != 0 : "Precondition Failure";
+        if (0 <= a) {
+            // +a: a % b'
+            if (b == Long.MIN_VALUE) return (int) new DafnyULong(a).modulus(new DafnyULong(b)).value();
+            else if (b < 0) return a % -b;
+            else return a % b;
+        } else {
+            // c = ((-a) % b')
+            // -a: b' - c if c > 0
+            // -a: 0 if c == 0
+            if (a == Long.MIN_VALUE || b == Long.MIN_VALUE) {
+                if (a == b) return 0;
+                else if (b == Long.MIN_VALUE) {
+                    return new DafnyULong(b).subtract(new DafnyULong(-a).modulus(new DafnyULong(b))).value();
+                } else {
+                    long bp = b < 0 ? -b : b;
+                    return new DafnyULong(bp).subtract(new DafnyULong(a).modulus(new DafnyULong(bp))).value();
+                }
+            } else {
+                long bp = b < 0 ? -b : b;
+                long c = ((-a) % bp);
+                return c == 0 ? c : bp - c;
+            }
+        }
+    }
+
+    public static BigInteger EuclideanModulus(BigInteger a, BigInteger b) {
+        assert b.compareTo(BigInteger.ZERO) != 0 : "Precondition Failure";
+        BigInteger bp = b.abs();
+        if (0 <= a.signum()) {
+            // +a: a % b'
+            return a.mod(bp);
+        } else {
+            // c = ((-a) % b')
+            // -a: b' - c if c > 0
+            // -a: 0 if c == 0
+            BigInteger c = a.negate().mod(bp);
+            return c.equals(BigInteger.ZERO) ? c : bp.subtract(c);
         }
     }
 }
--- Test/JavaCompiler/src/Tests/EuclideanTest.java
+++ Test/JavaCompiler/src/Tests/EuclideanTest.java
@@ -24,6 +24,7 @@ public class EuclideanTest {
         assertEquals(-6, DafnyEuclidean.EuclideanDivision_int(13, -2));
         assertEquals(2, DafnyEuclidean.EuclideanDivision_int(-2, -1));
         assertEquals(7, DafnyEuclidean.EuclideanDivision_int(-13, -2));
+        assertEquals(-2, DafnyEuclidean.EuclideanDivision_int(-14, 13));
         assertEquals(0, DafnyEuclidean.EuclideanDivision_int(14, Integer.MIN_VALUE));
         assertEquals(-2, DafnyEuclidean.EuclideanDivision_int(Integer.MIN_VALUE, Integer.MAX_VALUE));
         assertEquals(1, DafnyEuclidean.EuclideanDivision_int(Integer.MIN_VALUE, Integer.MIN_VALUE));
@@ -62,5 +63,68 @@ public class EuclideanTest {
         assertEquals(BigInteger.TWO.negate(), DafnyEuclidean.EuclideanDivision(BigInteger.TWO.negate(), BigInteger.ONE));
         assertEquals(new BigInteger("6"), DafnyEuclidean.EuclideanDivision(new BigInteger("13"), BigInteger.TWO));
         assertEquals(new BigInteger("-7"), DafnyEuclidean.EuclideanDivision(new BigInteger("-13"), BigInteger.TWO));
+    }
+
+
+    @Test
+    public void testIntMod() {
+        assertEquals(1, DafnyEuclidean.EuclideanModulus_int(7, 3));
+        assertEquals(1, DafnyEuclidean.EuclideanModulus_int(7, -3));
+        assertEquals(2, DafnyEuclidean.EuclideanModulus_int(-7, 3));
+        assertEquals(2, DafnyEuclidean.EuclideanModulus_int(-7, -3));
+
+        assertEquals(0, DafnyEuclidean.EuclideanModulus_int(0, 1));
+        assertEquals(0, DafnyEuclidean.EuclideanModulus_int(2, 1));
+        assertEquals(1, DafnyEuclidean.EuclideanModulus_int(13, 2));
+        assertEquals(0, DafnyEuclidean.EuclideanModulus_int(-2, 1));
+        assertEquals(1, DafnyEuclidean.EuclideanModulus_int(-13, 2));
+        assertEquals(0, DafnyEuclidean.EuclideanModulus_int(0, -1));
+        assertEquals(0, DafnyEuclidean.EuclideanModulus_int(2, -1));
+        assertEquals(1, DafnyEuclidean.EuclideanModulus_int(13, -2));
+        assertEquals(0, DafnyEuclidean.EuclideanModulus_int(-2, -1));
+        assertEquals(1, DafnyEuclidean.EuclideanModulus_int(-13, -2));
+
+        assertEquals(14, DafnyEuclidean.EuclideanModulus_int(14, Integer.MIN_VALUE));
+        assertEquals(12, DafnyEuclidean.EuclideanModulus_int(-14, 13));
+        assertEquals(Integer.MAX_VALUE - 1, DafnyEuclidean.EuclideanModulus_int(Integer.MIN_VALUE, Integer.MAX_VALUE));
+        assertEquals(0, DafnyEuclidean.EuclideanModulus_int(Integer.MIN_VALUE, Integer.MIN_VALUE));
+    }
+
+    @Test
+    public void testLongMod() {
+        assertEquals(1L, DafnyEuclidean.EuclideanModulus_long(7, 3));
+        assertEquals(1L, DafnyEuclidean.EuclideanModulus_long(7, -3));
+        assertEquals(2L, DafnyEuclidean.EuclideanModulus_long(-7, 3));
+        assertEquals(2L, DafnyEuclidean.EuclideanModulus_long(-7, -3));
+
+        assertEquals(0L, DafnyEuclidean.EuclideanModulus_long(0, 1));
+        assertEquals(0L, DafnyEuclidean.EuclideanModulus_long(2, 1));
+        assertEquals(1L, DafnyEuclidean.EuclideanModulus_long(13, 2));
+        assertEquals(0L, DafnyEuclidean.EuclideanModulus_long(-2, 1));
+        assertEquals(1L, DafnyEuclidean.EuclideanModulus_long(-13, 2));
+        assertEquals(0L, DafnyEuclidean.EuclideanModulus_long(0, -1));
+        assertEquals(0L, DafnyEuclidean.EuclideanModulus_long(2, -1));
+        assertEquals(1L, DafnyEuclidean.EuclideanModulus_long(13, -2));
+        assertEquals(0L, DafnyEuclidean.EuclideanModulus_long(-2, -1));
+        assertEquals(1L, DafnyEuclidean.EuclideanModulus_long(-13, -2));
+
+        assertEquals(14L, DafnyEuclidean.EuclideanModulus_long(14, Long.MIN_VALUE));
+        assertEquals(12L, DafnyEuclidean.EuclideanModulus_long(-14, 13));
+        assertEquals(Long.MAX_VALUE - 1, DafnyEuclidean.EuclideanModulus_long(Long.MIN_VALUE, Long.MAX_VALUE));
+        assertEquals(0L, DafnyEuclidean.EuclideanModulus_long(Long.MIN_VALUE, Long.MIN_VALUE));
+    }
+
+    @Test
+    public void testBigIntegerMod() {
+        assertEquals(BigInteger.ONE, DafnyEuclidean.EuclideanModulus(new BigInteger("7"), new BigInteger("3")));
+        assertEquals(BigInteger.ONE, DafnyEuclidean.EuclideanModulus(new BigInteger("7"), new BigInteger("-3")));
+        assertEquals(BigInteger.TWO, DafnyEuclidean.EuclideanModulus(new BigInteger("-7"), new BigInteger("3")));
+        assertEquals(BigInteger.TWO, DafnyEuclidean.EuclideanModulus(new BigInteger("-7"), new BigInteger("-3")));
+        assertEquals(BigInteger.ZERO, DafnyEuclidean.EuclideanModulus(BigInteger.ZERO, BigInteger.ONE));
+        assertEquals(BigInteger.ZERO, DafnyEuclidean.EuclideanModulus(BigInteger.ZERO, BigInteger.ONE.negate()));
+        assertEquals(BigInteger.ZERO, DafnyEuclidean.EuclideanModulus(BigInteger.TWO, BigInteger.ONE));
+        assertEquals(BigInteger.ZERO, DafnyEuclidean.EuclideanModulus(BigInteger.TWO.negate(), BigInteger.ONE));
+        assertEquals(BigInteger.ONE, DafnyEuclidean.EuclideanModulus(new BigInteger("13"), BigInteger.TWO));
+        assertEquals(BigInteger.ONE, DafnyEuclidean.EuclideanModulus(new BigInteger("-13"), BigInteger.TWO));
     }
 }
