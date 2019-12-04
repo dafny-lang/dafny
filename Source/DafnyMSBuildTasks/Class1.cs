f0be62663088b4eb78dd855a164a82d4742e0d93
Robin Salkeld <salkeldr@amazon.com>
2019-12-03 17:00:27 -0800

    First cut of task project

---
+++ Source/DafnyMSBuildTasks/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Build.Framework;
+
+namespace DafnyMSBuildTasks
+{
+    public class SayHello : Microsoft.Build.Utilities.Task
+    {
+        public override bool Execute()
+        {
+            Log.LogMessage(MessageImportance.High, "Aloha");
+            return true;
+        }
+    }
+}
