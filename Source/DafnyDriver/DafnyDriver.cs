a8a03dc52c0ab402db4181a9efd2a0b20d348c06
Alex Chew <alex@alexchew.net>
2020-05-18 18:25:09 -0700

    feat: add /version CLI flag
    Resolves #624.

--- Source/DafnyDriver/DafnyDriver.cs
+++ Source/DafnyDriver/DafnyDriver.cs
@@ -23,7 +23,7 @@ namespace Microsoft.Dafny
 
   public class DafnyDriver
   {
-    public enum ExitValue { VERIFIED = 0, PREPROCESSING_ERROR, DAFNY_ERROR, COMPILE_ERROR, NOT_VERIFIED }
+    public enum ExitValue { VERIFIED = 0, PREPROCESSING_ERROR, DAFNY_ERROR, COMPILE_ERROR, NOT_VERIFIED, DONT_PROCESS_FILES }
 
     public static int Main(string[] args)
     {
@@ -85,6 +85,12 @@ namespace Microsoft.Dafny
       } catch (ProverException pe) {
         ExecutionEngine.printer.ErrorWriteLine(Console.Out, "*** ProverException: {0}", pe.Message);
         return ExitValue.PREPROCESSING_ERROR;
+      }
+
+      if (DafnyOptions.O.PrintVersionAndExit)
+      {
+        Console.WriteLine(CommandLineOptions.Clo.Version);
+        return ExitValue.DONT_PROCESS_FILES;
       }
 
       if (CommandLineOptions.Clo.Files.Count == 0)
