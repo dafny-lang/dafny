5a538b159567c473170bb20a5c345b04436c919b
davidcok <davidcok@amazon.com>
2020-06-12 16:38:10 -0400

    fix: Fixing problem with titrace

--- Source/Dafny/Resolver.cs
+++ Source/Dafny/Resolver.cs
@@ -12145,6 +12145,7 @@ namespace Microsoft.Dafny
         if (DafnyOptions.O.TypeInferenceDebug) {
           Console.WriteLine("  ----> found no improvement, because meet does not determine type enough");
         }
+        return t;
       }
 
       // Compute the join of the proxy's supertypes
