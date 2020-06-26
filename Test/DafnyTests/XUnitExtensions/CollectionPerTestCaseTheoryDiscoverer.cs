926471cd21a28cefe768f4e709b9fa7b24acdbb6
Robin Salkeld <salkeldr@amazon.com>
2020-06-25 22:46:20 -0700

    Fix parallel execution from msbuild, add sharding

--- Test/DafnyTests/XUnitExtensions/CollectionPerTestCaseTheoryDiscoverer.cs
+++ Test/DafnyTests/XUnitExtensions/CollectionPerTestCaseTheoryDiscoverer.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using Xunit;
 using Xunit.Abstractions;
 using Xunit.Sdk;
 
@@ -19,8 +21,22 @@ namespace XUnitExtensions {
         }
 
         public IEnumerable<IXunitTestCase> Discover(ITestFrameworkDiscoveryOptions discoveryOptions, ITestMethod testMethod, IAttributeInfo factAttribute) {
-            return theoryDiscoverer.Discover(discoveryOptions, testMethod, factAttribute)
-                                   .Select(testCase => new TestCaseWithCollection(testCase, testCollectionForTestCase(testCase)));
+            // This discoverer requires pre-enumeration in order to assign a collection to each test case.
+            discoveryOptions.SetValue("xunit.discovery.PreEnumerateTheories", true);
+            
+            IEnumerable<IXunitTestCase> testCases = theoryDiscoverer.Discover(discoveryOptions, testMethod, factAttribute);
+            var shardEnvVar = Environment.GetEnvironmentVariable("XUNIT_SHARD");
+            var numShardsEnvVar = Environment.GetEnvironmentVariable("XUNIT_NUM_SHARDS");
+            if (shardEnvVar != null && numShardsEnvVar != null) {
+                var testCaseList = testCases.ToList();
+                var shard = Int32.Parse(shardEnvVar);
+                var numShards = Int32.Parse(numShardsEnvVar);
+                var shardStart = (shard - 1) * testCaseList.Count / numShards;
+                var shardEnd = shard * testCaseList.Count / numShards;
+                testCases = testCaseList.GetRange(shardStart, shardEnd);
+            }
+            
+            return testCases.Select(testCase => new TestCaseWithCollection(testCase, testCollectionForTestCase(testCase)));
         }
     }
 }
