093daf5389039eb459f0726b278072de36080aed
davidcok <davidcok@amazon.com>
2020-08-11 13:40:48 -0400

    fix: improved resolver reasoning when handling member lookup of types with type parameters

--- Source/Dafny/Resolver.cs
+++ Source/Dafny/Resolver.cs
@@ -12247,16 +12247,21 @@ namespace Microsoft.Dafny
                     Contract.Assert(mbr.EnclosingClass is TraitDecl);  // a proper supertype of a ClassDecl must be a TraitDecl
                     var typeMapping = cl.ParentFormalTypeParametersToActuals;
                     List<Type> proxyTypeArgs = null;
-                    // foreach (Type tt in cl.ParentTraits) {
-                    //   // If there is a match, the list of Type actuals is unique
-                    //   // (a class cannot inherit both Trait<T1> and Trait<T2> with T1 != T2).
-                    //   if (tt.AsTraitType == (TraitDecl) mbr.EnclosingClass) {
-                    //      proxyTypeArgs = tt.TypeArgs.ConvertAll(t0 => (t0.AsTypeParameter == null ? t0 : (typeMapping.ContainsKey(t0.AsTypeParameter) ? typeMapping[t0.AsTypeParameter] : (Type)new InferredTypeProxy()) ));
-                    //   }
-                    // }
+                    foreach (Type tt in cl.ParentTraits) {
+                      // If there is a match, the list of Type actuals is unique
+                      // (a class cannot inherit both Trait<T1> and Trait<T2> with T1 != T2).
+                      if (tt.AsTraitType == (TraitDecl) mbr.EnclosingClass) {
+                         proxyTypeArgs = tt.TypeArgs.ConvertAll(t0 => SubstType(t0,typeMapping));
+                      }
+                    }
                     if (proxyTypeArgs == null) {
+                      //proxyTypeArgs = mbr.EnclosingClass.TypeArgs.ConvertAll(t0 => SubstType(t0,typeMapping));
                       proxyTypeArgs = mbr.EnclosingClass.TypeArgs.ConvertAll(t0 => (typeMapping.ContainsKey(t0) ? typeMapping[t0] : (Type)new InferredTypeProxy()) );
                     }
+
+                    var meetMapping = TypeSubstitutionMap(cl.TypeArgs, meet.TypeArgs);
+                    proxyTypeArgs = proxyTypeArgs.ConvertAll(t0 => SubstType(t0,meetMapping));
+                    proxyTypeArgs = proxyTypeArgs.ConvertAll(t0 => t0.AsTypeParameter == null ? t0 : (Type)new InferredTypeProxy() );
                     var pickItFromHere = new UserDefinedType(tok, mbr.EnclosingClass.Name, mbr.EnclosingClass, proxyTypeArgs);
                     if (DafnyOptions.O.TypeInferenceDebug) {
                       Console.WriteLine("  ----> improved to {0} through meet and member lookup", pickItFromHere);
@@ -12480,7 +12485,8 @@ namespace Microsoft.Dafny
       }
 
       if (meet == null) {
-        meet = Type.HeadWithProxyArgs(t);
+        // stick with what we've got
+        meet = t;
         return true;
       } else if (Type.IsHeadSupertypeOf(meet, t)) {
         // stick with what we've got
