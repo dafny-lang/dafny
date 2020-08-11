0e7c1dc5d96eb62f618faf8ba459a87a25424fa7
davidcok <davidcok@amazon.com>
2020-08-11 14:30:23 -0400

    fix: Fixes crash because of a bad cast

--- Source/Dafny/Translator.cs
+++ Source/Dafny/Translator.cs
@@ -3646,7 +3646,7 @@ namespace Microsoft.Dafny {
       Contract.Ensures(Contract.ValueAtReturn(out arguments) != null);
 
       if (member.IsStatic) {
-        receiver = new StaticReceiverExpr(tok, (ClassDecl)member.EnclosingClass, true);  // this also resolves it
+        receiver = new StaticReceiverExpr(tok, (TopLevelDeclWithMembers)member.EnclosingClass, true);  // this also resolves it
       } else {
         receiver = new ImplicitThisExpr(tok);
         receiver.Type = Resolver.GetReceiverType(tok, member);  // resolve here
