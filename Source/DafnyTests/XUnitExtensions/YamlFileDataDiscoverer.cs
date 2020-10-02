0bdfddae7d07dc9e2d30a2aefb8be81c06c10422
Robin Salkeld <salkeldr@amazon.com>
2020-10-02 15:32:13 -0700

    Hook up GetYamlParser (whew)

--- Source/DafnyTests/XUnitExtensions/YamlFileDataDiscoverer.cs
+++ Source/DafnyTests/XUnitExtensions/YamlFileDataDiscoverer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -8,6 +9,9 @@ using YamlDotNet.Core;
 using YamlDotNet.Core.Events;
 using YamlDotNet.RepresentationModel;
 using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NodeDeserializers;
+using YamlDotNet.Serialization.ObjectFactories;
+using YamlDotNet.Serialization.Utilities;
 
 namespace XUnitExtensions {
   public class YamlFileDataDiscoverer : IDataDiscoverer {
@@ -19,67 +23,47 @@ namespace XUnitExtensions {
       return new Deserializer();
     }
 
+    private Func<IParser, Type, object> ForMethodInfoDeserializeFn(IDeserializer deserializer, MethodInfo testMethod, string path) {
+      return (parser, type) => {
+        if (type == typeof(MethodArguments)) {
+          MethodArguments arguments = new MethodArguments(testMethod, path);
+          arguments.Read(parser, type, t => deserializer.Deserialize(parser, t));
+          return arguments;
+        } else {
+          return deserializer.Deserialize(parser, type);
+        }
+      };
+    }
+    
     public bool SupportsDiscoveryEnumeration(IAttributeInfo dataAttribute, IMethodInfo testMethod) {
       return true;
     }
-    
-    private class ParsingEventBuffer : IEmitter {
-      public List<ParsingEvent> ParsingEvents = new List<ParsingEvent>();
-
-      public void Emit(ParsingEvent parsingEvent) {
-        ParsingEvents.Add(parsingEvent);
-      }
-    }
-    
-    private class ParsingEventListParser : IParser {
-      private readonly IEnumerator<ParsingEvent> Enumerator;
-
-      public ParsingEventListParser(IEnumerator<ParsingEvent> enumerator) {
-        Enumerator = enumerator;
-      }
-     
-      public bool MoveNext() {
-        return Enumerator.MoveNext();
-      }
-
-      public ParsingEvent Current => Enumerator.Current; 
-    }
-    
-    private IParser NodeParser(YamlNode node) {
-      ParsingEventBuffer buffer = new ParsingEventBuffer();
-      YamlDocument doc = new YamlDocument(node);
-      YamlStream stream = new YamlStream();
-      stream.Add(doc);
-      stream.Save(buffer, false);
-      return new ParsingEventListParser(buffer.ParsingEvents.GetEnumerator());
-    }
-
-    private object DeserializeParameter(IDeserializer deserializer, ParameterInfo parameterInfo, YamlNode mappingNode) {
-      return deserializer.Deserialize(NodeParser(mappingNode), parameterInfo.ParameterType);
-    }
-    
-    private IEnumerable<object> MethodArguments(IDeserializer deserializer, MethodInfo testMethod, string path, bool withParameterNames, YamlMappingNode mappingNode) {
-      IEnumerable<object> result;
-      IEnumerable<ParameterInfo> parameters = testMethod.GetParameters();
-      bool withSourceFile = parameters.First().ParameterType.Equals(typeof(ISourceInformation));
-      if (withSourceFile) {
-        parameters = parameters.Skip(1);
-      }
-      if (withParameterNames) {
-        result = parameters.Select(param => DeserializeParameter(deserializer, param, mappingNode[param.Name])).ToArray();
-      } else {
-        result = new[] {DeserializeParameter(deserializer, parameters.Single(), mappingNode)};
-      }
-
-      return withSourceFile ? new[] {new SourceFile(path)}.Concat(result) : result;
-    }
-    
     private IEnumerable<object[]> FileData(IDeserializer deserializer, MethodInfo testMethod, string path, bool withParameterNames) {
-      var yamlStream = new YamlStream();
-      yamlStream.Load(new StreamReader(path));
-      // TODO: error checking and expansion
-      YamlSequenceNode node = (YamlSequenceNode)yamlStream.Documents[0].RootNode;
-      return node.Select(childNode => MethodArguments(deserializer, testMethod, path, withParameterNames, (YamlMappingNode)childNode).ToArray());
+      IParser parser = GetYamlParser(path);
+      parser.Consume<StreamStart>();
+      parser.Consume<DocumentStart>();
+            
+      if (withParameterNames) {
+        IObjectFactory argumentsFactory = new DefaultObjectFactory();
+        INodeDeserializer collectionDeserializer = new CollectionNodeDeserializer(argumentsFactory);
+        if (collectionDeserializer.Deserialize(parser, typeof(List<MethodArguments>), ForMethodInfoDeserializeFn(deserializer, testMethod, path), out var value)) {
+          List<MethodArguments> argumentses = (List<MethodArguments>) value;
+          return argumentses.Select(a => a.Arguments);
+        } else {
+          throw new ArgumentException();
+        }
+      } else {
+        IEnumerable<ParameterInfo> parameters = testMethod.GetParameters();
+        bool withSourceFile = parameters.First().ParameterType.Equals(typeof(ISourceInformation));
+        if (withSourceFile) {
+          parameters = parameters.Skip(1);
+        }
+        IEnumerable<object> results = (IEnumerable<object>)deserializer.Deserialize(parser, typeof(List<>).MakeGenericType(parameters.Single().ParameterType));
+        
+        return withSourceFile ? 
+          results.Select(value => new[]{ new SourceFile(path), value }) : 
+          results.Select(value => new[]{ value });
+      }
     }
 
     public IEnumerable<object[]> GetData(IAttributeInfo attributeInfo, IMethodInfo testMethod) {
@@ -102,6 +86,48 @@ namespace XUnitExtensions {
                                  .Select(path => Path.GetRelativePath(rootPath, path));
         return filePaths.SelectMany(path => FileData(deserializer, methodInfo, path, withParameterNames));
       }
+    }
+  }
+
+  public class MethodArguments : IYamlConvertible {
+
+    private MethodInfo Method;
+    private Dictionary<string, int> ParameterNameIndexes;
+    public object[] Arguments { get; }
+
+    public MethodArguments(MethodInfo methodInfo, string path) {
+      Method = methodInfo;
+      Arguments = new object[Method.GetParameters().Length];
+      ParameterNameIndexes = Method.GetParameters()
+        .Select((value, index) => new KeyValuePair<string, int>(value.Name, index))
+        .ToDictionary(p => p.Key, p => p.Value);
+      var sourceFileParameter = Method.GetParameters().FirstOrDefault(p => p.ParameterType.Equals(typeof(ISourceInformation)));
+      if (sourceFileParameter != null) {
+        Arguments[ParameterNameIndexes[sourceFileParameter.Name]] = new SourceFile(path);
+      }
+    }
+    
+    public void Read(IParser parser, Type expectedType, ObjectDeserializer nestedObjectDeserializer) {
+      if (!parser.TryConsume(out MappingStart _)) {
+        throw new ArgumentException();
+      }
+      while (!parser.TryConsume(out MappingEnd _)) {
+        Scalar scalar = parser.Consume<Scalar>();
+        int parameterIndex = ParameterNameIndexes[scalar.Value];
+        Type parameterType = Method.GetParameters()[parameterIndex].ParameterType;
+        
+        object argument = nestedObjectDeserializer(parameterType);
+        IValuePromise valuePromise = argument as IValuePromise;
+        if (valuePromise != null) {
+          valuePromise.ValueAvailable += (Action<object>) (v => Arguments[parameterIndex] = TypeConverter.ChangeType(v, parameterType));
+        } else {
+          Arguments[parameterIndex] = TypeConverter.ChangeType(argument, parameterType);
+        }
+      }
+    }
+
+    public void Write(IEmitter emitter, ObjectSerializer nestedObjectSerializer) {
+      throw new NotImplementedException();
     }
   }
   
