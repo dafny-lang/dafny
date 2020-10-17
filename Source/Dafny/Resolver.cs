8ea4f14dd6d443705466fbc7fdcc8403f5ab3594
Rustan Leino <leino@amazon.com>
2020-10-16 18:14:27 -0700

    Remove unnecessary `using`

--- Source/Dafny/Resolver.cs
+++ Source/Dafny/Resolver.cs
@@ -10,7 +10,6 @@ using System.Linq;
 using System.Numerics;
 using System.Diagnostics.Contracts;
 using System.Linq.Expressions;
-using System.Security.Cryptography;
 using Microsoft.Boogie;
 
 namespace Microsoft.Dafny
