061cee70f40b5a0843cf060226c66f9d3d935aaa
Christoph Amrein <camrein@users.noreply.github.com>
2020-10-20 13:35:02 +0200

    Added note about symbol resolution when shadowing variables.

--- Source/DafnyLS/Language/Symbols/MethodSymbol.cs
+++ Source/DafnyLS/Language/Symbols/MethodSymbol.cs
@@ -13,7 +13,9 @@ namespace DafnyLS.Language.Symbols {
     public ISet<VariableSymbol> Returns { get; } = new HashSet<VariableSymbol>();
     public IList<VariableSymbol> Locals { get; } = new List<VariableSymbol>();
 
-    public override IEnumerable<ISymbol> Children => Parameters.Concat(Returns).Concat(Locals);
+    // TODO The resolution priority is currently given by the order of the children.
+    // TODO We have to properly align the locals to their enclosing block to correctly handle shadowing.
+    public override IEnumerable<ISymbol> Children => Locals.Concat(Parameters).Concat(Returns);
 
     public MethodSymbol(ISymbol? scope, Method method) : base(scope, method.Name) {
       Declaration = method;
