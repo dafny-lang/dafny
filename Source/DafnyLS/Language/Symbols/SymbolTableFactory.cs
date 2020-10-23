6706806dc8fe71339628f0088b69b6babe87d8f9
Christoph Amrein <camrein@users.noreply.github.com>
2020-10-23 12:57:48 +0200

    Added note about the pessimistic symbol resolution.

--- Source/DafnyLS/Language/Symbols/SymbolTableFactory.cs
+++ Source/DafnyLS/Language/Symbols/SymbolTableFactory.cs
@@ -23,6 +23,11 @@ namespace DafnyLS.Language.Symbols {
       var designatorVisitor = new DesignatorVisitor(_logger, declarations, compilationUnit, cancellationToken);
       var declarationVisitor = new SymbolDeclarationVisitor(cancellationToken);
       if(HasErrors(program)) {
+        // TODO This is an unfortunate situation. The syntax could be correct but contain some semantic errors.
+        //      However, due to the contracts of the resolver we cannot pro-actively check if certain information could be resolved or not.
+        //      Therefore, we only have "all or nothing" when re-using the semantic model. Check if there is really no possibility to
+        //      check if information was set without actually retrieving it (i.e., it's not possible to access the Type attribute due to a contract
+        //      prohibiting it to be null).
         _logger.LogDebug("cannot create symbol table from a program with errors");
         return new SymbolTable(compilationUnit, declarationVisitor.Declarations, designatorVisitor.SymbolLookup, false);
       }
