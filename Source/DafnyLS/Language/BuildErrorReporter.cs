b11790697620ac8d77dfa596ead6856cdb165772
Christoph Amrein <camrein@users.noreply.github.com>
2020-10-26 14:31:08 +0100

    Added setting of the actual error source.

--- Source/DafnyLS/Language/BuildErrorReporter.cs
+++ Source/DafnyLS/Language/BuildErrorReporter.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Dafny;
+﻿using Microsoft.Boogie;
+using Microsoft.Dafny;
 
 namespace DafnyLS.Language {
   /// <summary>
@@ -7,5 +8,14 @@ namespace DafnyLS.Language {
   /// all the necessary functionallity already.
   /// </summary>
   public class BuildErrorReporter : ErrorReporter {
+    // TODO this method is only overriden because the base class does not set propery "source" of "ErrorMessage".
+    public override bool Message(MessageSource source, ErrorLevel level, IToken tok, string msg) {
+      if(ErrorsOnly && level != ErrorLevel.Error) {
+        // discard the message
+        return false;
+      }
+      AllMessages[level].Add(new ErrorMessage { source = source, token = tok, message = msg });
+      return true;
+    }
   }
 }
