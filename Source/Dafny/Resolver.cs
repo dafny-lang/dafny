03742a58e88b37596ee498b899272920a22d1c92
davidcok <davidcok@amazon.com>
2020-11-03 11:53:42 -0500

    Fixing test regressions

--- Source/Dafny/Resolver.cs
+++ Source/Dafny/Resolver.cs
@@ -3768,7 +3768,7 @@ namespace Microsoft.Dafny
         var lhsWithProxyArgs = Type.HeadWithProxyArgs(lhs);
         ConstrainSubtypeRelation(lhsWithProxyArgs, rhs, errMsg, false, allowDecisions);
         ConstrainAssignableTypeArgs(lhs, lhsWithProxyArgs.TypeArgs, lhs.TypeArgs, errMsg, out moreXConstraints);
-        if (lhs.AsCollectionType == null) {
+        if (lhs.IsArrayType) {
           ConstrainAssignableTypeArgs(lhs, lhs.TypeArgs, rhs.TypeArgs, errMsg, out moreXConstraints);
         }
       }
