2e512ddcd9aca670c5810ff1300d94df58a331a5
Christoph Amrein <camrein@users.noreply.github.com>
2020-11-06 10:16:30 +0100

    Added note about missing hover information.

--- Source/DafnyLS/Handlers/DafnyHoverHandler.cs
+++ Source/DafnyLS/Handlers/DafnyHoverHandler.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace Microsoft.Dafny.LanguageServer.Handlers {
   public class DafnyHoverHandler : IHoverHandler {
+    // TODO add the range of the identifier to the hover.
     private readonly ILogger _logger;
     private readonly IDocumentDatabase _documents;
 
