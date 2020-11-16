578ee9f0545ada1873a5f5f36e1ae065e88e47a6
Christoph Amrein <camrein@users.noreply.github.com>
2020-11-16 10:39:44 +0100

    Refined the LspSymbol generator to support the new  scopes.

--- Source/DafnyLS/Language/Symbols/LspSymbolGeneratingVisitor.cs
+++ Source/DafnyLS/Language/Symbols/LspSymbolGeneratingVisitor.cs
@@ -1,4 +1,5 @@
 ﻿using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 
@@ -6,7 +7,7 @@ namespace Microsoft.Dafny.LanguageServer.Language.Symbols {
   /// <summary>
   /// Visitor responsible to generate the LSP symbol representation.
   /// </summary>
-  public class LspSymbolGeneratingVisitor : ISymbolVisitor<DocumentSymbol> {
+  public class LspSymbolGeneratingVisitor : ISymbolVisitor<IEnumerable<DocumentSymbol>> {
     private readonly SymbolTable _symbolTable;
     private readonly CancellationToken _cancellationToken;
 
@@ -15,113 +16,69 @@ namespace Microsoft.Dafny.LanguageServer.Language.Symbols {
       _cancellationToken = cancellationToken;
     }
 
-    public DocumentSymbol Visit(ISymbol symbol) {
+    private bool IsPartOfEntryDocument(Boogie.IToken token) {
+      // Tokens with line=0 usually represent a default/implicit class/module/etc. We do not want
+      // to show these in the symbol listing.
+      return token.line != 0 && _symbolTable.CompilationUnit.Program.IsPartOfEntryDocument(token);
+    }
+
+    public IEnumerable<DocumentSymbol> Visit(ISymbol symbol) {
       _cancellationToken.ThrowIfCancellationRequested();
       return symbol.Accept(this);
     }
 
-    public DocumentSymbol Visit(CompilationUnit compilationUnit) {
-      throw new System.NotImplementedException("cannot create a document symbol for the compilation unit");
+    public IEnumerable<DocumentSymbol> Visit(CompilationUnit compilationUnit) {
+      return compilationUnit.Children.SelectMany(Visit).ToArray();
     }
 
-    public DocumentSymbol Visit(ModuleSymbol moduleSymbol) {
-      _cancellationToken.ThrowIfCancellationRequested();
-      return WithLocation(
-        moduleSymbol,
-        new DocumentSymbol {
-          Name = moduleSymbol.Name,
-          Kind = SymbolKind.Module,
-          Detail = moduleSymbol.GetDetailText(_cancellationToken),
-          Children = moduleSymbol.Children.Select(child => child.Accept(this)).ToArray()
-        }
-      );
+    public IEnumerable<DocumentSymbol> Visit(ModuleSymbol moduleSymbol) {
+      return CreateSymbols(moduleSymbol, moduleSymbol.Declaration.tok, SymbolKind.Module);
     }
 
-    public DocumentSymbol Visit(ClassSymbol classSymbol) {
-      _cancellationToken.ThrowIfCancellationRequested();
-      return WithLocation(
-        classSymbol,
-        new DocumentSymbol {
-          Name = classSymbol.Name,
-          Kind = SymbolKind.Class,
-          Detail = classSymbol.GetDetailText(_cancellationToken),
-          Children = classSymbol.Children.Select(child => child.Accept(this)).ToArray()
-        }
-      );
+    public IEnumerable<DocumentSymbol> Visit(ClassSymbol classSymbol) {
+      return CreateSymbols(classSymbol, classSymbol.Declaration.tok, SymbolKind.Class);
     }
 
-    public DocumentSymbol Visit(ValueTypeSymbol valueTypeSymbol) {
-      return WithLocation(
-        valueTypeSymbol,
-        new DocumentSymbol {
-          Name = valueTypeSymbol.Name,
-          Kind = SymbolKind.Struct,
-          Detail = valueTypeSymbol.GetDetailText(_cancellationToken),
-          Children = valueTypeSymbol.Children.Select(child => child.Accept(this)).ToArray()
-        }
-      );
+    public IEnumerable<DocumentSymbol> Visit(ValueTypeSymbol valueTypeSymbol) {
+      return CreateSymbols(valueTypeSymbol, valueTypeSymbol.Declaration.tok, SymbolKind.Struct);
     }
 
-    public DocumentSymbol Visit(FieldSymbol fieldSymbol) {
-      _cancellationToken.ThrowIfCancellationRequested();
-      return WithLocation(
-        fieldSymbol,
-        new DocumentSymbol {
-          Name = fieldSymbol.Name,
-          Kind = SymbolKind.Field,
-          Detail = fieldSymbol.GetDetailText(_cancellationToken)
-        }
-      );
+    public IEnumerable<DocumentSymbol> Visit(FieldSymbol fieldSymbol) {
+      return CreateSymbols(fieldSymbol, fieldSymbol.Declaration.tok, SymbolKind.Field);
     }
 
-    public DocumentSymbol Visit(FunctionSymbol functionSymbol) {
-      _cancellationToken.ThrowIfCancellationRequested();
-      return WithLocation(
-        functionSymbol,
-        new DocumentSymbol {
-          Name = functionSymbol.Name,
-          Kind = SymbolKind.Function,
-          Detail = functionSymbol.GetDetailText(_cancellationToken),
-          Children = functionSymbol.Children.Select(child => child.Accept(this)).ToArray()
-        }
-      );
+    public IEnumerable<DocumentSymbol> Visit(FunctionSymbol functionSymbol) {
+      return CreateSymbols(functionSymbol, functionSymbol.Declaration.tok, SymbolKind.Function);
     }
 
-    public DocumentSymbol Visit(MethodSymbol methodSymbol) {
-      _cancellationToken.ThrowIfCancellationRequested();
-      return WithLocation(
-        methodSymbol,
-        new DocumentSymbol {
-          Name = methodSymbol.Name,
-          Kind = SymbolKind.Method,
-          Detail = methodSymbol.GetDetailText(_cancellationToken),
-          Children = methodSymbol.Children.Select(child => child.Accept(this)).ToArray()
-        }
-      );
+    public IEnumerable<DocumentSymbol> Visit(MethodSymbol methodSymbol) {
+      return CreateSymbols(methodSymbol, methodSymbol.Declaration.tok, SymbolKind.Method);
     }
 
-    public DocumentSymbol Visit(VariableSymbol variableSymbol) {
-      _cancellationToken.ThrowIfCancellationRequested();
-      return WithLocation(
-        variableSymbol,
-        new DocumentSymbol {
-          Name = variableSymbol.Name,
-          Kind = SymbolKind.Variable,
-          Detail = variableSymbol.GetDetailText(_cancellationToken)
-        }
-      );
+    public IEnumerable<DocumentSymbol> Visit(VariableSymbol variableSymbol) {
+      return CreateSymbols(variableSymbol, variableSymbol.Declaration.Tok, SymbolKind.Variable);
     }
 
-    public DocumentSymbol Visit(ScopeSymbol scopeSymbol) {
-      return new DocumentSymbol();
+    public IEnumerable<DocumentSymbol> Visit(ScopeSymbol scopeSymbol) {
+      return scopeSymbol.Children.SelectMany(Visit);
     }
 
-    private DocumentSymbol WithLocation(ISymbol symbol, DocumentSymbol documentSymbol) {
+    private IEnumerable<DocumentSymbol> CreateSymbols(ILocalizableSymbol symbol, Boogie.IToken token, SymbolKind kind) {
+      var children = symbol.Children.SelectMany(Visit);
+      if(!IsPartOfEntryDocument(token)) {
+        return children;
+      }
+      var documentSymbol = new DocumentSymbol {
+        Name = symbol.Name,
+        Kind = kind,
+        Detail = symbol.GetDetailText(_cancellationToken),
+        Children = children.ToArray()
+      };
       if(_symbolTable.TryGetLocationOf(symbol, out var location)) {
         documentSymbol.Range = location.Declaration;
         documentSymbol.SelectionRange = location.Name;
       }
-      return documentSymbol;
+      return new[] { documentSymbol };
     }
   }
 }
