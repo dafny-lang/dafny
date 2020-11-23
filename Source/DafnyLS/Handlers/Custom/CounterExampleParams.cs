ae7ca6fdeba82fe846ef5076e1da8e5ec481678c
Christoph Amrein <camrein@users.noreply.github.com>
2020-11-23 09:43:20 +0100

    Added the request definition to make the class usable for clients.

--- Source/DafnyLS/Handlers/Custom/CounterExampleParams.cs
+++ Source/DafnyLS/Handlers/Custom/CounterExampleParams.cs
@@ -1,7 +1,9 @@
 ﻿using MediatR;
+using OmniSharp.Extensions.JsonRpc;
 using System.Diagnostics.CodeAnalysis;
 
 namespace Microsoft.Dafny.LanguageServer.Handlers.Custom {
+  [Method(DafnyRequestNames.CounterExample, Direction.ClientToServer)]
   public class CounterExampleParams : IRequest<CounterExampleList> {
     // TODO Use LSP's DocumentUri.
     [AllowNull]
