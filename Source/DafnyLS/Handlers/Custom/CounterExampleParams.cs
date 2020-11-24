cd064ef7f4a53d2d7f70813ce7541fd16ef2fb09
Christoph Amrein <camrein@users.noreply.github.com>
2020-11-24 12:56:23 +0100

    Disabled nullability for the single property since AllowNullAttribute does not work in the CI.

--- Source/DafnyLS/Handlers/Custom/CounterExampleParams.cs
+++ Source/DafnyLS/Handlers/Custom/CounterExampleParams.cs
@@ -1,12 +1,13 @@
 ﻿using MediatR;
 using OmniSharp.Extensions.JsonRpc;
 using OmniSharp.Extensions.LanguageServer.Protocol.Models;
-using System.Diagnostics.CodeAnalysis;
 
 namespace Microsoft.Dafny.LanguageServer.Handlers.Custom {
   [Method(DafnyRequestNames.CounterExample, Direction.ClientToServer)]
   public class CounterExampleParams : ITextDocumentIdentifierParams, IRequest<CounterExampleList> {
-    [AllowNull]
+    // TODO Refine nullability. AllowNull is currently ignored in the CI and leads to build warnings.
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor.
     public TextDocumentIdentifier TextDocument { get; set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor.
   }
 }
