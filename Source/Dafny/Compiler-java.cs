9a6ca12a962cfcb2a5d92f1a6474471493901542
Rustan Leino <leino@amazon.com>
2021-01-21 13:35:20 -0800

    Java runtime clean-up (#1057)
    * Remove mutable map methods for Java
    
    The previous DafnyMap class in the runtime for Java implemented Java’s Map interface. However, the mutable methods of the map interface don’t go with the immutable nature of DafnyMap. Therefore, the ties with Map are hereby severed.
    
    * Remove “dafny” prefix from method names
    
    * Suppress unchecked warnings
    
    Also, fix some deprecated syntax.
    
    Leave -Xlint:unchecked on in gradle.build.
    
    * Fix construction of maps

--- Source/Dafny/Compiler-java.cs
+++ Source/Dafny/Compiler-java.cs
@@ -1212,16 +1212,16 @@ namespace Microsoft.Dafny{
           postString = ")";
           break;
         case SpecialField.ID.Keys:
-          compiledName = "dafnyKeySet()";
+          compiledName = "keySet()";
           break;
         case SpecialField.ID.Values:
-          compiledName = "dafnyValues()";
+          compiledName = "valueSet()";
           break;
         case SpecialField.ID.Items:
           var mapType = receiverType.AsMapType;
           Contract.Assert(mapType != null);
           var errorWr = new TargetWriter();
-          compiledName = $"<{BoxedTypeName(mapType.Domain, errorWr, Bpl.Token.NoToken)}, {BoxedTypeName(mapType.Range, errorWr, Bpl.Token.NoToken)}>dafnyEntrySet()";
+          compiledName = $"<{BoxedTypeName(mapType.Domain, errorWr, Bpl.Token.NoToken)}, {BoxedTypeName(mapType.Range, errorWr, Bpl.Token.NoToken)}>entrySet()";
           break;
         case SpecialField.ID.Reads:
           compiledName = "_reads";
@@ -2495,7 +2495,7 @@ namespace Microsoft.Dafny{
       var mt = e.Type.AsMapType;
       var domType = mt.Domain;
       var ranType = mt.Range;
-      wr.WriteLine($"{DafnyMapClass}<{BoxedTypeName(domType, wr, e.tok)}, {BoxedTypeName(ranType, wr, e.tok)}> {collectionName} = new {DafnyMapClass}<>();");
+      wr.WriteLine($"java.util.HashMap<{BoxedTypeName(domType, wr, e.tok)}, {BoxedTypeName(ranType, wr, e.tok)}> {collectionName} = new java.util.HashMap<>();");
     }
 
     protected override void OrganizeModules(Program program, out List<ModuleDefinition> modules){
