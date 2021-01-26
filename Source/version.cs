f601a6aaf3b6ee9259e4736a6eea1e63cf0398ae
davidcok <davidcok@github.com>
2021-01-25 19:17:29 -0500

    New version

--- Source/version.cs
+++ Source/version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
-// Version 3.0.0 pre-release 2, year 2018+3 month 01 day 07
-[assembly: AssemblyVersion("3.0.0.30107")]
-[assembly: AssemblyFileVersion("3.0.0.30107")]
+// Version 3.0.0 pre-release 3, year 2018+3 month 01 day 25
+[assembly: AssemblyVersion("3.0.0.30125")]
+[assembly: AssemblyFileVersion("3.0.0.30125")]
