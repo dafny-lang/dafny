ebdeecd32c0252afedd4f57463add3f418538343
davidcok <davidcok@github.com>
2021-01-30 18:26:12 -0500

    Bumping version number

--- Source/version.cs
+++ Source/version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
-// Version 3.0.0 pre-release 3, year 2018+3 month 01 day 25
-[assembly: AssemblyVersion("3.0.0.30125")]
-[assembly: AssemblyFileVersion("3.0.0.30125")]
+// Version 3.0.0 pre-release 4, year 2018+3 month 01 day 30
+[assembly: AssemblyVersion("3.0.0.30130")]
+[assembly: AssemblyFileVersion("3.0.0.30130")]
