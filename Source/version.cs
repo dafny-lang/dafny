b857089d9214d0cbf7e1cc452d3a37424e4780b8
Rustan Leino <leino@amazon.com>
2021-02-04 15:11:20 -0800

    Bump internal version number
    This makes the detailed version number different from the 3.0.0 release. Although this change by itself won’t let us distinguish different future build-from-source versions, it will let us distinguish between “Dafny 3.0.0 release” and “built from source after the Dafny 3.0.0 release”.

--- Source/version.cs
+++ Source/version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
-// Version 3.0.0, year 2018+3 month 02 day 03
-[assembly: AssemblyVersion("3.0.0.30203")]
-[assembly: AssemblyFileVersion("3.0.0.30203")]
+// Version 3.0.0, year 2018+3 month 02 day 04
+[assembly: AssemblyVersion("3.0.0.30204")]
+[assembly: AssemblyFileVersion("3.0.0.30204")]
