be9313ebb47d3ccd1edfb072440d4f0ada2d9f33
Rustan Leino <leino@amazon.com>
2021-04-21 14:00:41 -0700

    Update version to 3.1.0.30421

--- Source/version.cs
+++ Source/version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
-// Version 3.0.0, year 2018+3 month 02 day 04
-[assembly: AssemblyVersion("3.0.0.30204")]
-[assembly: AssemblyFileVersion("3.0.0.30204")]
+// Version 3.1.0, year 2018+3 month 04 day 21
+[assembly: AssemblyVersion("3.1.0.30421")]
+[assembly: AssemblyFileVersion("3.1.0.30421")]
