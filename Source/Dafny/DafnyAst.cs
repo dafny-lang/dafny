3e64d65e4beb8fcae67306ec2fcb0bce484c18ee
Remy Willems <rgv.willems@gmail.com>
2021-07-05 14:29:49 +0200

    Move Substitutor and AlphaConverting_Substitutor to separate files

--- Source/Dafny/DafnyAst.cs
+++ Source/Dafny/DafnyAst.cs
@@ -9639,7 +9639,7 @@ namespace Microsoft.Dafny {
         substMap.Add(oldVars[i], id);
       }
 
-      Translator.Substituter sub = new Translator.Substituter(null, substMap, typeMap);
+      Substituter sub = new Substituter(null, substMap, typeMap);
       return sub.Substitute(e);
     }
 
