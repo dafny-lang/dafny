d0f3502c5070e077707b9c960bcf65847cf5b070
Robin Salkeld <salkeldr@amazon.com>
2021-08-09 12:40:53 -0700

    Add LitTestConvertor.Test package

---
+++ Source/LitTestConvertor.Test/ConvertingFromLitToXunit.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace LitTestConvertor.Test
+{
+  public class ConvertingFromLitToXunit
+  {
+    [Fact]
+    public void HelloWorld() {
+      var convertor = new LitTestConvertor();
+      // TODO
+    }
+  }
+}
