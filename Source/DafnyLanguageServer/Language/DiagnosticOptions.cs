cb167a07d0cbf2948e751c08ead3a05e30cb2ca8
Christoph Amrein <camrein@users.noreply.github.com>
2021-09-03 15:17:23 +0200

    Added the missing options class.

---
+++ Source/DafnyLanguageServer/Language/DiagnosticOptions.cs
@@ -0,0 +1,18 @@
+﻿using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+
+namespace Microsoft.Dafny.LanguageServer.Language {
+  /// <summary>
+  /// Options for diagnostics reported to the LSP client.
+  /// </summary>
+  public class DiagnosticOptions {
+    /// <summary>
+    /// The IConfiguration section of the document options.
+    /// </summary>
+    public const string Section = "Diagnostic";
+
+    /// <summary>
+    /// Gets or sets the severity of info messages produced by the Dafny compiler.
+    /// </summary>
+    public DiagnosticSeverity InfoSeverity { get; set; } = DiagnosticSeverity.Information;
+  }
+}
