2c146d8a4f8c729e8edc171f8b22d492cdf21f77
Parva <parva0shah@gmail.com>
2021-09-15 12:36:24 -0400

    Issue #1384

--- Source/Dafny/AST/DafnyAst.cs
+++ Source/Dafny/AST/DafnyAst.cs
@@ -2508,8 +2508,9 @@ namespace Microsoft.Dafny {
       } else {
         // if the domain type consists of a single tuple type, then an extra set of parentheses is needed
         // Note, we do NOT call .AsDatatype or .AsIndDatatype here, because those calls will do a NormalizeExpand().  Instead, we do the check manually.
-        var udt = typeArgs[0].Normalize() as UserDefinedType;  // note, we do Normalize(), not NormalizeExpand(), since the TypeName will use any synonym
-        if (udt != null && udt.ResolvedClass is TupleTypeDecl) {
+        var udt = typeArgs[0].Normalize() as UserDefinedType;  // note, we do Normalize(), not NormalizeExpand(), since the TypeName will use any synony
+        
+        if (udt != null && BuiltIns.IsTupleTypeName(udt.FullName))  {
           domainNeedsParens = true;
         }
       }
