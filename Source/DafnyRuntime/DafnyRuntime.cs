c8af69e11b93d24cf952714cc3ee4034754e3ed0
Parva <parva0shah@gmail.com>
2021-10-15 09:40:03 -0400

    Revert "working on Unchanged files with check annotations."
    This reverts commit 20e7f1ebc0fd031a4c492aa87c4c58de4cb51fb4.

--- Source/DafnyRuntime/DafnyRuntime.cs
+++ Source/DafnyRuntime/DafnyRuntime.cs
@@ -1,16 +1,12 @@
 // Copyright by the contributors to the Dafny Project
 // SPDX-License-Identifier: MIT
 
-// #if ISDAFNYRUNTIMELIB
-// using System; // for Func
-// using System.Numerics;
-// #endif
+#if ISDAFNYRUNTIMELIB
+using System; // for Func
+using System.Numerics;
+#endif
 
 namespace DafnyAssembly {
-#if ISDAFNYRUNTIMELIB
-  using System; // for Func
-  using System.Numerics;
-#endif
   [AttributeUsage(AttributeTargets.Assembly)]
   public class DafnySourceAttribute : Attribute {
     public readonly string dafnySourceText;
@@ -40,7 +36,6 @@ namespace Dafny {
       this.setImpl = d;
       this.containsNull = containsNull;
     }
-
     public static readonly ISet<T> Empty = new Set<T>(ImmutableHashSet<T>.Empty, false);
 
     private static readonly TypeDescriptor<ISet<T>> _TYPE = new Dafny.TypeDescriptor<ISet<T>>(Empty);
@@ -55,7 +50,6 @@ namespace Dafny {
     public static Set<T> FromISet(ISet<T> s) {
       return s as Set<T> ?? FromCollection(s.Elements);
     }
-
     public static Set<T> FromCollection(IEnumerable<T> values) {
       var d = ImmutableHashSet<T>.Empty.ToBuilder();
       var containsNull = false;
@@ -66,7 +60,6 @@ namespace Dafny {
           d.Add(t);
         }
       }
-
       return new Set<T>(d.ToImmutable(), containsNull);
     }
     public static ISet<T> FromCollectionPlusOne(IEnumerable<T> values, T oneMoreValue) {
