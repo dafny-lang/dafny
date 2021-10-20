fe37a19d6c5d732280a004e2638b0459d6ccac67
Robin Salkeld <salkeldr@amazon.com>
2021-10-20 15:42:56 -0700

    chore: Fix version number in attributes (#1525)

--- Source/version.cs
+++ Source/version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 // Version 3.3.0, year 2018+3 month 10 day 20
-[assembly: AssemblyVersion("3.2.0.31020")]
-[assembly: AssemblyFileVersion("3.2.0.31020")]
+[assembly: AssemblyVersion("3.3.0.31020")]
+[assembly: AssemblyFileVersion("3.3.0.31020")]
