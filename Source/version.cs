94c03819fe65a169c47467f91089a90c67ed7f79
Robin Salkeld <salkeldr@amazon.com>
2021-11-04 12:42:16 -0700

    chore: Bump for full 3.3 release

--- Source/version.cs
+++ Source/version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
-// Version 3.3.0, year 2018+3 month 10 day 20
-[assembly: AssemblyVersion("3.3.0.31020")]
-[assembly: AssemblyFileVersion("3.3.0.31020")]
+// Version 3.3.0, year 2018+3 month 11 day 4
+[assembly: AssemblyVersion("3.3.0.31104")]
+[assembly: AssemblyFileVersion("3.3.0.31104")]
