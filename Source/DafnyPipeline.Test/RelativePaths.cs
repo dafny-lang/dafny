cd2c9df770af8f706eb78ea7abbed615bf956a60
Fabio Madge <fmadge@amazon.com>
2021-11-23 13:54:01 +0100

    move testfile instead of working directory

--- Source/DafnyPipeline.Test/RelativePaths.cs
+++ Source/DafnyPipeline.Test/RelativePaths.cs
@@ -8,9 +8,7 @@ namespace DafnyPipeline.Test {
   public class RelativePaths {
     [Fact]
     public void Test() {
-      Directory.SetCurrentDirectory(Path.Join("TestFiles", "LitTests", "LitTest"));
       var code = DafnyDriver.ThreadMain(new string[] { "/spillTargetCode:3", "warnings-as-errors.dfy" });
-      Directory.SetCurrentDirectory(Path.Join("..", "..", ".."));
       Assert.Equal(0, code);
     }
   }
