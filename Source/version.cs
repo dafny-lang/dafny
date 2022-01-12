b9891db0e32b12f93bb40452dc980d134793429d
prvshah51 <89794159+prvshah51@users.noreply.github.com>
2022-01-12 15:30:13 -0500

    pre release 3.4.0

--- Source/version.cs
+++ Source/version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
-// Version 3.3.0, year 2018+3 month 11 day 4
-[assembly: AssemblyVersion("3.3.0.31104")]
-[assembly: AssemblyFileVersion("3.3.0.31104")]
+// Version 3.4.0, year 2018+3 month 11 day 4
+[assembly: AssemblyVersion("3.4.0.31104")]
+[assembly: AssemblyFileVersion("3.4.0.31104")]
