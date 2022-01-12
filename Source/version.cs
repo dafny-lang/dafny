8ba289dfd7650399c248cf102188cf4913cf8ea2
prvshah51 <89794159+prvshah51@users.noreply.github.com>
2022-01-12 15:51:27 -0500

    made changes.

--- Source/version.cs
+++ Source/version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
-// Version 3.4.0, year 2018+3 month 11 day 4
-[assembly: AssemblyVersion("3.4.0.31104")]
-[assembly: AssemblyFileVersion("3.4.0.31104")]
+// Version 3.4.0, year 2018+4 month 1 day 12
+[assembly: AssemblyVersion("3.4.0.40112")]
+[assembly: AssemblyFileVersion("3.4.0.40112")]
