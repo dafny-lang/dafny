fc06a99e3568720bcdb4f78f183d5d61eca9de59
Clément Pit-Claudel <cpitcla@amazon.com>
2022-01-18 20:00:49 -0500

    Improve auto-generator; use nesting

--- Source/Dafny/AST/ASTExport/Program.cs
+++ Source/Dafny/AST/ASTExport/Program.cs
@@ -1,13 +1,26 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace ASTExport;
+
+public static class EnumerableExtensions {
+  public static IEnumerable<T> NonNull<T>(this IEnumerable<T?> items) {
+    foreach (var item in items) {
+      if (item != null) {
+        yield return item;
+      }
+    }
+  }
+}
 
 abstract class PrettyPrintable {
   protected virtual string ChildIndent => "  ";
@@ -17,8 +30,7 @@ abstract class PrettyPrintable {
     wr.WriteLine($"{indent}{ChildIndent}{child}");
   }
 
-  protected void PpChildren<T>(TextWriter wr, string indent, IEnumerable<T> children)
-    where T : PrettyPrintable
+  protected void PpChildren(TextWriter wr, string indent, IEnumerable<PrettyPrintable> children)
   {
     indent += ChildIndent;
     var childrenList = children.ToList();
@@ -30,26 +42,36 @@ abstract class PrettyPrintable {
     }
   }
 
-  public string FmtAttrs(IDictionary<string, string?> attrs) {
+  private string FmtAttrs(IDictionary<string, string?> attrs) {
     return String.Join(" ", attrs.Select((kv) =>
       String.IsNullOrEmpty(kv.Value) ? $"{{:{kv.Key}}}" : $"{{:{kv.Key} {kv.Value}}}"));
   }
 
-  protected void PpBlockOpen(TextWriter wr, string indent, string kind, string name,
-    Dictionary<string, string?>? attrs, IEnumerable<string>? inheritance) {
-    var parts = new List<string>() {kind};
+  protected void PpBlockOpen(TextWriter wr, string indent, object? kind, object? name,
+    Dictionary<string, string?>? attrs, IEnumerable<Type>? inheritance) {
+    var parts = new List<string>();
+    parts.Add($"{kind}");
     if (attrs != null) {
       parts.Add(FmtAttrs(attrs));
     }
-    parts.Add(name);
+    parts.Add($"{name}");
     if (inheritance != null) {
-      parts.Add($"extends {String.Join(", ", inheritance)}");
+      parts.Add($"extends {String.Join(", ", inheritance.Select(t => t.ToString()))}");
     }
     wr.WriteLine($"{indent}{String.Join(" ", parts)} {{");
   }
 
   protected void PpBlockClose(TextWriter wr, string indent) {
     wr.WriteLine($"{indent}}}");
+  }
+
+  public static IEnumerable<PrettyPrintable> Dispatch(SyntaxNode node) {
+    return node switch {
+      NamespaceDeclarationSyntax s => s.ChildNodes().SelectMany(Dispatch),
+      TypeDeclarationSyntax s => new[] { new TypeDecl(s) },
+      FieldDeclarationSyntax s => new[] { new Field(s) },
+      _ => Enumerable.Empty<PrettyPrintable>()
+    };
   }
 
   public abstract void Pp(TextWriter wr, string indent);
@@ -62,43 +84,70 @@ class AST : PrettyPrintable {
     this.syntax = syntax;
   }
 
-  public static AST FromFile(string fname) {
-    using var reader = new StreamReader(fname);
+  public static AST FromFile(string fileName) {
+    using var reader = new StreamReader(fileName);
     return new AST(CSharpSyntaxTree.ParseText(reader.ReadToEnd()));
   }
 
-  public IEnumerable<Class> Classes =>
-    syntax.GetCompilationUnitRoot().DescendantNodes().OfType<ClassDeclarationSyntax>()
-      .Select(c => new Class(c));
+  private IEnumerable<PrettyPrintable> Children =>
+    syntax.GetCompilationUnitRoot().ChildNodes().SelectMany(Dispatch);
 
   public override void Pp(TextWriter wr, string indent) {
     wr.WriteLine("include \"CSharpCompat.dfy\"");
     wr.WriteLine();
+
     PpBlockOpen(wr, indent, "module", "CSharp",
       new Dictionary<string, string?> {{"extern", "\"SelfHosting.CSharp\""}}, null);
+
     PpChild(wr, indent, "import opened CSharpGenerics");
+    PpChild(wr, indent, "import opened CSharpSystem");
+    PpChild(wr, indent, "import opened Boogie");
+    PpChild(wr, indent, "import opened Dafny");
     wr.WriteLine();
-    PpChildren(wr, indent, Classes);
+
+    PpChildren(wr, indent, Children);
+
     PpBlockClose(wr, indent);
   }
 }
 
-class Class : PrettyPrintable {
-  private readonly ClassDeclarationSyntax syntax;
+class TypeDecl : PrettyPrintable {
+  private readonly TypeDeclarationSyntax syntax;
 
-  public Class(ClassDeclarationSyntax syntax) {
+  protected override string ChildSeparator => "";
+
+  public TypeDecl(TypeDeclarationSyntax syntax) {
     this.syntax = syntax;
   }
 
-  public IEnumerable<Field> Fields =>
-    syntax.DescendantNodes().OfType<FieldDeclarationSyntax>().Select(f => new Field(f));
+  private IEnumerable<PrettyPrintable> Children =>
+    syntax.ChildNodes().SelectMany(Dispatch);
 
   public override void Pp(TextWriter wr, string indent) {
-    PpBlockOpen(wr, indent, "trait", syntax.Identifier.Text,
+    PpBlockOpen(wr, indent, "trait", new Identifier(syntax.Identifier).WithAttrs(),
       new Dictionary<string, string?> {{"compile", "false"}, {"extern", null}},
-      syntax.BaseList?.Types.Select(t => t.Type.GetText().ToString().Trim()));
-    PpChildren(wr, indent, Fields);
+      syntax.BaseList?.Types.Select(t => new Type(t.Type)));
+    PpChildren(wr, indent, Children);
     PpBlockClose(wr, indent);
+  }
+}
+
+class Enum : PrettyPrintable {
+  private readonly EnumDeclarationSyntax syntax;
+
+  public Enum(EnumDeclarationSyntax syntax) {
+    this.syntax = syntax;
+  }
+
+  private IEnumerable<Identifier> Members =>
+    syntax.Members.Select(m => new Identifier(m.Identifier));
+
+  public override void Pp(TextWriter wr, string indent) {
+    var decl = new Identifier(syntax.Identifier);
+    wr.WriteLine($"{indent}datatype {decl.WithAttrs()} =");
+    foreach (var m in Members) {
+      PpChild(wr, indent, $"| {m.WithAttrs()}");
+    }
   }
 }
 
@@ -112,7 +161,7 @@ class Field : PrettyPrintable {
     this.syntax = syntax;
   }
 
-  public IEnumerable<Variable> Variables =>
+  private IEnumerable<Variable> Variables =>
     syntax.Declaration.Variables.Select(v => new Variable(syntax.Declaration.Type, v));
 
   public override void Pp(TextWriter wr, string indent) {
@@ -120,17 +169,57 @@ class Field : PrettyPrintable {
   }
 }
 
-internal class Variable : PrettyPrintable {
-  private readonly TypeSyntax type;
-  private readonly VariableDeclaratorSyntax syntax;
+internal class Type {
+  private readonly TypeSyntax syntax;
 
-  public Variable(TypeSyntax type, VariableDeclaratorSyntax syntax) {
-    this.type = type;
+  public Type(TypeSyntax syntax) {
     this.syntax = syntax;
   }
 
+  public override string ToString() {
+    if (syntax is ArrayTypeSyntax arr) {
+      return $"array<{arr.ElementType.GetText().ToString().Trim()}>";
+    }
+    return syntax.GetText().ToString().Trim();
+  }
+}
+
+internal class Identifier {
+  private const string EscapePrefix = "CSharp_";
+  private static readonly Regex DisallowedNameRe = new Regex("^(type$|_)");
+
+  private readonly SyntaxToken token;
+
+  public Identifier(SyntaxToken token) {
+    this.token = token;
+  }
+
+  private string Id => token.Text;
+  private string EscapedId => Id.StartsWith(EscapePrefix) || DisallowedNameRe.IsMatch(Id) ?
+    EscapePrefix + Id : Id;
+
+  public string WithAttrs() {
+    string id = Id, eId = EscapedId;
+    var attr = id != eId ? $"{{:extern \"{id}\"}} " : "";
+    return $"{attr}{eId}";
+  }
+
+  public override string ToString() {
+    return EscapedId;
+  }
+}
+
+internal class Variable : PrettyPrintable {
+  private readonly Type type;
+  private readonly Identifier identifier;
+
+  public Variable(TypeSyntax type, VariableDeclaratorSyntax syntax) {
+    this.type = new Type(type);
+    this.identifier = new Identifier(syntax.Identifier);
+  }
+
   public override void Pp(TextWriter wr, string indent) {
-    wr.WriteLine($"{indent}var {syntax.Identifier.Text}: {type.GetText().ToString().Trim()}");
+    wr.WriteLine($"{indent}var {identifier.WithAttrs()}: {type}");
   }
 }
 
