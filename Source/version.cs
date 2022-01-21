b8626755601b97951bbf1de948e194cae70c3b0f
prvshah51 <parva0shah@gmail.com>
2022-01-21 10:44:58 -0500

    instruction removed from version.cs and added in RELEASE.txt

--- Source/version.cs
+++ Source/version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
-// Version 3.4.0, year 2018+4, month 1 day 12 as for date January 12th
+// Version 3.4.0, year 2018+4, month 1 day 12
 [assembly: AssemblyVersion("3.4.0.40112")]
 [assembly: AssemblyFileVersion("3.4.0.40112")]
