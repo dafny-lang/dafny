a496826f948395b5802ed50d465cc46cba07d6f1
Aaron Tomb <aarotomb@amazon.com>
2022-02-07 14:13:27 -0800

    Update version number for release

--- Source/version.cs
+++ Source/version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 // Version 3.4.0, year 2018+4 month 1 day 12
-[assembly: AssemblyVersion("3.4.0.40112")]
-[assembly: AssemblyFileVersion("3.4.0.40112")]
+[assembly: AssemblyVersion("3.4.0.40207")]
+[assembly: AssemblyFileVersion("3.4.0.40207")]
