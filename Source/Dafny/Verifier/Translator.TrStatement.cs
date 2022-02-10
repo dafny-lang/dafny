c14e26320938923cf7b735bfc01c87fdfa5bb40a
Rustan Leino <leino@amazon.com>
2022-02-10 11:48:49 -0800

    fix: Include necessary antecedents for CanCall assumptions for loop invariants

--- Source/Dafny/Verifier/Translator.TrStatement.cs
+++ Source/Dafny/Verifier/Translator.TrStatement.cs
@@ -1910,10 +1910,14 @@ namespace Microsoft.Dafny {
         loopBodyBuilder.Add(Bpl.Cmd.SimpleAssign(s.Tok, w, Bpl.Expr.False));
       }
       // Finally, assume the well-formedness of the invariant (which has been checked once and for all above), so that the check
-      // of invariant-maintenance can use the appropriate canCall predicates.
+      // of invariant-maintenance can use the appropriate canCall predicates. Note, it is important (see Test/git-issues/git-issue-1812.dfy)
+      // that each CanCall assumption uses the preceding invariants as antecedents--this is achieved by treating all "invariant"
+      // declarations as one big conjunction, because then CanCallAssumption will add the needed antecedents.
+      Expression allInvariants = Expression.CreateBoolLiteral(s.Tok, true);
       foreach (AttributedExpression loopInv in s.Invariants) {
-        loopBodyBuilder.Add(TrAssumeCmd(loopInv.E.tok, CanCallAssumption(loopInv.E, etran)));
+        allInvariants = Expression.CreateAnd(allInvariants, loopInv.E);
       }
+      loopBodyBuilder.Add(TrAssumeCmd(s.Tok, CanCallAssumption(allInvariants, etran)));
       Bpl.StmtList body = loopBodyBuilder.Collect(s.Tok);
 
       builder.Add(new Bpl.WhileCmd(s.Tok, Bpl.Expr.True, invariants, body));
