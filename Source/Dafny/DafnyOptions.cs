bd35d681c71fb84d9c5634da8b12e0a23e2ec6a6
Rustan Leino <leino@amazon.com>
2022-02-15 09:15:09 -0800

    Added two clarifications, per PR comments

--- Source/Dafny/DafnyOptions.cs
+++ Source/Dafny/DafnyOptions.cs
@@ -703,9 +703,29 @@ namespace Microsoft.Dafny {
       that dynamically dispatch to implementations in other modules terminate,
       then the trait `T` can be marked with `{:termination false}`. This will
       allow `T` to be extended by types declared in modules outside `T`'s module.
+
       Caution: This loophole is unsound; that is, if a cross-module dynamic
       dispatch fails to terminate, then this and other errors in the program
       may have been overlooked by the verifier.       
+
+      The meaning of `{:termination false}` is defined only on trait declarations.
+      It has no meaning if applied to other declarations.
+
+      Applying `{:termination false}` to a trait is similar to the
+      effect of declaring each of its methods with `decreases *`, but
+      there are several differences.  The biggest difference is that
+      `decreases *` is sound, whereas the attribute is not. As such,
+      `decreases *` cannot be used with functions, lemmas, or ghost
+      methods, and callers of a `decreases *` method must themselves
+      be declared with `decreases *`. In contrast, `{:termination false}`
+      applies to all functions, lemmas, and methods of the trait, and
+      callers do not have to indicate that they are using such a
+      trait. Another difference is that `{:termination false}` does
+      not change checking for intra-module calls. That is, even if a
+      trait is declared with `{:termination false}`, calls to its
+      functions, lemmas, and methods from within the module where the
+      trait is declared are checked for termination in the usual
+      manner.
 
     {:warnShadowing}
       TODO
