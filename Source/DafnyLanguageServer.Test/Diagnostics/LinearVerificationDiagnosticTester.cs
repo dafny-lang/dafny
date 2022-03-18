cce0b70c8cba462d64ba97e9734f590aad5b2351
Mikael Mayer <mimayere@amazon.com>
2022-03-18 13:55:48 -0500

    WIP tests

--- Source/DafnyLanguageServer.Test/Diagnostics/LinearVerificationDiagnosticTester.cs
+++ Source/DafnyLanguageServer.Test/Diagnostics/LinearVerificationDiagnosticTester.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Dafny.LanguageServer.IntegrationTest.Extensions;
 using Microsoft.Dafny.LanguageServer.IntegrationTest.Util;
 using Microsoft.Dafny.LanguageServer.Workspace.Notifications;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -10,6 +12,7 @@ using OmniSharp.Extensions.JsonRpc;
 using OmniSharp.Extensions.LanguageServer.Protocol.Client;
 using OmniSharp.Extensions.LanguageServer.Protocol.Document;
 using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+using Range = OmniSharp.Extensions.LanguageServer.Protocol.Models.Range;
 
 namespace Microsoft.Dafny.LanguageServer.IntegrationTest.Diagnostics;
 
@@ -81,8 +84,12 @@ public abstract class LinearVerificationDiagnosticTester : ClientBasedLanguageSe
   /// <returns></returns>
   public static string ExtractCode(string tracesAndCode) {
     var i = 0;
-    while (tracesAndCode[i] != ':') {
+    while (tracesAndCode[i] != ':' && tracesAndCode[i] != '\n') {
       i++;
+    }
+
+    if (tracesAndCode[i] == '\n') {
+      return tracesAndCode;
     }
     var pattern = $"(?<newline>^|\r?\n).{{{i}}}:(?<line>.*)";
     var regexRemoveTrace = new Regex(pattern);
@@ -128,11 +135,86 @@ public abstract class LinearVerificationDiagnosticTester : ClientBasedLanguageSe
            verificationDiagnosticReport.PerLineDiagnostic.All(IsNotIndicatingProgress) ||
            verificationDiagnosticReport.PerLineDiagnostic.All(status => status == LineVerificationStatus.Unknown);
   }
+
+  /// <summary>
+  /// Given some code, will emit the edit corresponding to remove the regular expression
+  /// .*?//NextN?:
+  /// </summary>
+  /// <param name="code">The original code with the //Next: comments or //NextN:</param>
+  /// <returns></returns>
+  public System.Tuple<string, List<System.Tuple<Range, string>>> ExtractCodeAndChanges(string code) {
+    var lineMatcher = new Regex(@"\r?\n");
+    var matcher = new Regex(@"(?<previousNewline>^|\r?\n)(?<toRemove>.*?//Next(?<id>\d*):)");
+    var originalCode = code;
+    var matches = matcher.Matches(code);
+    var changes = new List<System.Tuple<Range, string>>() { };
+    while (matches.Count > 0) {
+      var firstChange = 0;
+      Match? firstChangeMatch = null;
+      for (var i = 0; i < matches.Count; i++) {
+        if (matches[i].Groups["id"].Value != "") {
+          var intValue = Int32.Parse(matches[i].Groups["id"].Value);
+          if (firstChange == 0 || intValue < firstChange) {
+            firstChange = intValue;
+            firstChangeMatch = matches[i];
+          }
+        }
+      }
+
+      if (firstChangeMatch == null) {
+        break;
+      }
+
+      var startRemove = firstChangeMatch.Groups["toRemove"].Index;
+      var endRemove = startRemove + firstChangeMatch.Groups["toRemove"].Value.Length;
+
+      Position IndexToPosition(int index) {
+        var before = code.Substring(0, index);
+        var newlines = lineMatcher.Matches(before);
+        var line = newlines.Count;
+        var character = index;
+        if (newlines.Count > 0) {
+          var lastNewline = newlines[newlines.Count - 1];
+          character = index - (lastNewline.Index + lastNewline.Value.Length);
+        }
+
+        return new Position(line, character);
+      }
+
+      // For now, simple: Remove the line
+      changes.Add(new Tuple<Range, string>(
+        new Range(IndexToPosition(startRemove), IndexToPosition(endRemove)), ""));
+      code = code.Substring(0, startRemove) + code.Substring(endRemove);
+      matches = matcher.Matches(code);
+    }
+
+    return new Tuple<string, List<Tuple<Range, string>>>(originalCode, changes);
+  }
+
+  public async Task VerifyTrace(string codeAndTrace) {
+    codeAndTrace = codeAndTrace.StripMargin();
+    var codeAndChanges = ExtractCode(codeAndTrace);
+    var (code, changes) = ExtractCodeAndChanges(codeAndChanges);
+    var documentItem = CreateTestDocument(code);
+    await client.OpenDocumentAndWaitAsync(documentItem, CancellationToken);
+    var traces = new List<LineVerificationStatus[]>();
+    traces.AddRange(await GetAllLineVerificationDiagnostics(documentItem));
+    foreach (var (range, inserted) in changes) {
+      ApplyChange(ref documentItem, range, inserted);
+      traces.AddRange(await GetAllLineVerificationDiagnostics(documentItem));
+    }
+    var expected = RenderTrace(traces, code);
+    AssertWithDiff.Equal(codeAndTrace, expected);
+
+  }
 }
 
 public static class StringExtensions {
   public static string StripMargin(this string s) {
-    return Regex.Replace(s, @"(?:(?<init>^\r?\n?)|(?<newline>\r?\n))[ \t]+:", match =>
+    if (!Regex.Match(s, @"(?:^\r?\n?)[ \t]*:").Success) {
+      return s;
+    }
+    return Regex.Replace(s, @"(?:(?<init>^\r?\n?)|(?<newline>\r?\n))[ \t]*:", match =>
       match.Groups["init"].Success ? "" : "\n"
     );
   }
--- Source/DafnyLanguageServer.Test/Diagnostics/SimpleLinearVerificationDiagnosticTester.cs
+++ Source/DafnyLanguageServer.Test/Diagnostics/SimpleLinearVerificationDiagnosticTester.cs
@@ -16,67 +16,13 @@ namespace Microsoft.Dafny.LanguageServer.IntegrationTest.Diagnostics;
 public class SimpleLinearVerificationDiagnosticTester : LinearVerificationDiagnosticTester {
   private const int MaxTestExecutionTimeMs = 10000;
 
-  [TestInitialize]
-  public override Task SetUp() => base.SetUp();
+  // To add a new test, just call VerifyTrace on a given program,
+  // the test will fail and give the correct output that can be use for the test
+  // Add '//Next<n>:' to edit a line multiple times
 
-  /// <summary>
-  /// Given some code, will emit the edit corresponding to remove the regular expression
-  /// .*?//NextN?:
-  /// </summary>
-  /// <param name="code">The original code with the //Next: comments or //NextN:</param>
-  /// <returns></returns>
-  public Tuple<string, List<Tuple<Range, string>>> ExtractCodeAndChanges(string code) {
-    var lineMatcher = new Regex(@"\r?\n");
-    var matcher = new Regex(@"(?<previousNewline>^|\r?\n)(?<toRemove>.*?//Next(?<id>\d*):)");
-    var originalCode = code;
-    var matches = matcher.Matches(code);
-    var changes = new List<Tuple<Range, string>>() { };
-    while (matches.Count > 0) {
-      var firstChange = 0;
-      Match? firstChangeMatch = null;
-      for (var i = 0; i < matches.Count; i++) {
-        if (matches[i].Groups["id"].Value != "") {
-          var intValue = Int32.Parse(matches[i].Groups["id"].Value);
-          if (firstChange == 0 || intValue < firstChange) {
-            firstChange = intValue;
-            firstChangeMatch = matches[i];
-          }
-        }
-      }
-
-      if (firstChangeMatch == null) {
-        break;
-      }
-
-      var startRemove = firstChangeMatch.Groups["toRemove"].Index;
-      var endRemove = startRemove + firstChangeMatch.Groups["toRemove"].Value.Length;
-
-      Position IndexToPosition(int index) {
-        var before = code.Substring(0, index);
-        var newlines = lineMatcher.Matches(before);
-        var line = newlines.Count;
-        var character = index;
-        if (newlines.Count > 0) {
-          var lastNewline = newlines[newlines.Count - 1];
-          character = index - (lastNewline.Index + lastNewline.Value.Length);
-        }
-
-        return new Position(line, character);
-      }
-
-      // For now, simple: Remove the line
-      changes.Add(new Tuple<Range, string>(
-        new Range(IndexToPosition(startRemove), IndexToPosition(endRemove)), ""));
-      code = code.Substring(0, startRemove) + code.Substring(endRemove);
-      matches = matcher.Matches(code);
-    }
-
-    return new Tuple<string, List<Tuple<Range, string>>>(originalCode, changes);
-  }
-
-  [TestMethod/*, Timeout(MaxTestExecutionTimeMs)*/]
+  [TestMethod, Timeout(MaxTestExecutionTimeMs)]
   public async Task EnsureVerificationDiagnosticsAreWorking() {
-    var codeAndTrace = @"
+    await VerifyTrace(@"
     : s  |  |  |  I  I  |  | :predicate Ok() {
     : s  |  |  |  I  I  |  | :  true
     : s  |  |  |  I  I  |  | :}
@@ -93,19 +39,19 @@ public class SimpleLinearVerificationDiagnosticTester : LinearVerificationDiagno
     :    |  |  |  I  I  |  | :    
     : s  |  |  |  I  I  |  | :predicate OkBis() {
     : s  |  |  |  I  I  |  | :  false
-    : s  |  |  |  I  I  |  | :}".StripMargin();
-    var codeAndChanges = ExtractCode(codeAndTrace);
-    var (code, changes) = ExtractCodeAndChanges(codeAndChanges);
-    var documentItem = CreateTestDocument(code);
-    await client.OpenDocumentAndWaitAsync(documentItem, CancellationToken);
-    var traces = new List<LineVerificationStatus[]>();
-    traces.AddRange(await GetAllLineVerificationDiagnostics(documentItem));
-    foreach (var (range, inserted) in changes) {
-      ApplyChange(ref documentItem, range, inserted);
-      traces.AddRange(await GetAllLineVerificationDiagnostics(documentItem));
-    }
-    var expected = RenderTrace(traces, code);
-    AssertWithDiff.Equal(codeAndTrace, expected);
-
+    : s  |  |  |  I  I  |  | :}");
+  }
+  [TestMethod]
+  public async Task EnsuresItWorksForSubsetTypes() {
+    await VerifyTrace(@"
+:    |  | :
+:    |  | :ghost const maxId := 200;
+:    |  | :
+: s  |  | :predicate isIssueIdValid(issueId: int) {
+: s  |  | :  101 <= issueId < maxId
+: s  |  | :}
+:    |  | :
+: s  S  | :type IssueId = i : int | isIssueIdValid(i)
+: s  S  | :  witness 101 //Next1:   witness 99 //Next2:   witness 101 ");
   }
 }
