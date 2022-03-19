d0f42b09363d4906b270d747fb851d55371a9943
Remy Willems <rwillems@amazon.com>
2022-03-19 12:30:46 +0100

    Fix compilation

--- Source/DafnyLanguageServer/Language/DafnyProgramVerifier.cs
+++ Source/DafnyLanguageServer/Language/DafnyProgramVerifier.cs
@@ -146,7 +146,7 @@ namespace Microsoft.Dafny.LanguageServer.Language {
       public void AdvisoryWriteLine(TextWriter writer, string format, params object[] args) {
       }
 
-      public ExecutionEngineOptions Options { get; set; }
+      public ExecutionEngineOptions? Options { get; set; }
 
       public void ErrorWriteLine(TextWriter tw, string s) {
         logger.LogError(s);
