a686cc6e2705d42163c8a31efc3bc2c845495ba3
Fabio Madge <fmadge@amazon.com>
2022-04-05 01:50:31 +0200

    first cut

--- Source/DafnyRuntime/DafnyRuntime.py
+++ Source/DafnyRuntime/DafnyRuntime.py
@@ -1,4 +1,6 @@
 import builtins
+from dataclasses import dataclass
+from contextlib import contextmanager
 
 def print(value):
   if type(value) == bool:
@@ -7,3 +9,43 @@ def print(value):
     builtins.print(value.fget(), end="")
   else:
     builtins.print(value, end="")
+
+@dataclass
+class Break(Exception):
+  target: str
+
+@contextmanager
+def label(name: str):
+  try:
+    yield
+  except Break as g:
+    if g.target != name:
+      raise g
+
+def _break(name):
+  raise Break(target=name)
+
+class Seq(list):
+  @property
+  def Elements(self):
+    return self
+
+class Set(set):
+  @property
+  def Elements(self):
+    return self
+
+  def union(self, other):
+    return Set(set.union(self, other))
+
+  def __or__(self, other):
+    return Set(set.union(self, other))
+
+class Map(dict):
+  @property
+  def Elements(self):
+    return self
+
+  @property
+  def Keys(self):
+    return Seq(self.keys())
