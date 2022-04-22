7388f481923f31ce2b2aaa1ea045b7992d9b791f
Remy Willems <rwillems@amazon.com>
2022-04-22 15:46:44 +0200

    Upgrade to Boogie 2.14.0

--- Source/DafnyLanguageServer/Language/DiagnosticErrorReporter.cs
+++ Source/DafnyLanguageServer/Language/DiagnosticErrorReporter.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
+using VCGeneration;
 
 namespace Microsoft.Dafny.LanguageServer.Language {
   public class DiagnosticErrorReporter : ErrorReporter {
