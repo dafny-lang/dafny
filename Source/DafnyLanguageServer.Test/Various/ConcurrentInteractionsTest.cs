07dd1bd58a8226bef96ef9b7c1e9a45192e512af
Remy Willems <rwillems@amazon.com>
2022-05-03 19:59:40 +0200

    Reduce load count for windows

--- Source/DafnyLanguageServer.Test/Various/ConcurrentInteractionsTest.cs
+++ Source/DafnyLanguageServer.Test/Various/ConcurrentInteractionsTest.cs
@@ -188,7 +188,7 @@ method Multiply(x: bv10, y: bv10) returns (product: bv10)
       // exclusive to themselves. This "stress test" ensures that loading multiple documents at once is possible.
       // To be more specific, this test should ensure that there is no state discarded/overriden between the three steps within
       // the Dafny Compiler itself.
-      int documentsToLoadConcurrently = 100;
+      int documentsToLoadConcurrently = 50;
       var source = @"
 method Multiply(x: int, y: int) returns (product: int)
   requires y >= 0 && x >= 0
