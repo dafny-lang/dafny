7401cc2ce8b6b66a8ac0634ed3e992cb06ad2fb0
Remy Willems <rwillems@amazon.com>
2022-05-06 09:54:28 +0200

    Remove debug console write line

--- Source/DafnyLanguageServer/Workspace/DiagnosticPublisher.cs
+++ Source/DafnyLanguageServer/Workspace/DiagnosticPublisher.cs
@@ -26,7 +26,6 @@ namespace Microsoft.Dafny.LanguageServer.Workspace {
     }
 
     private void PublishDocumentDiagnostics(DafnyDocument document) {
-      Console.WriteLine($"Publishing document version {document.Version} with {document.Diagnostics.Count()} diagnostics.");
       languageServer.TextDocument.PublishDiagnostics(new PublishDiagnosticsParams {
         Uri = document.Uri,
         Version = document.Version,
