034f7bed424ee55d01c3a2b5fa14e3a8c634410c
Mikaël Mayer <MikaelMayer@users.noreply.github.com>
2022-05-16 15:05:55 -0500

    feat: Verification diagnostics reordering (#1944)

--- Source/DafnyLanguageServer/Workspace/DafnyDocument.cs
+++ Source/DafnyLanguageServer/Workspace/DafnyDocument.cs
@@ -1,6 +1,7 @@
 ﻿using OmniSharp.Extensions.LanguageServer.Protocol;
 using OmniSharp.Extensions.LanguageServer.Protocol.Models;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using Microsoft.Boogie;
 using SymbolTable = Microsoft.Dafny.LanguageServer.Language.Symbols.SymbolTable;
@@ -46,6 +47,12 @@ namespace Microsoft.Dafny.LanguageServer.Workspace {
       TextDocumentItem.Uri.ToString(),
       TextDocumentItem.Text.Count(c => c == '\n') + 1
     );
+
+    // List of a few last touched method positions
+    public ImmutableList<Position> LastTouchedMethodPositions { get; set; } = new List<Position>() { }.ToImmutableList();
+
+    // Used to prioritize verification to one method and its dependencies
+    public Range? LastChange { get; init; } = null;
 
     /// <summary>
     /// Checks if the given document uri is pointing to this dafny document.
