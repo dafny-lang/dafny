77233e8c00185415858baf92537be22725cf887a
Mikael Mayer <mimayere@amazon.com>
2022-05-18 12:27:18 -0500

    Added previously failing test to reproduce what is happening with VSCode

--- Source/DafnyLanguageServer.Test/Various/DiagnosticMigrationTest.cs
+++ Source/DafnyLanguageServer.Test/Various/DiagnosticMigrationTest.cs
@@ -17,7 +17,7 @@ public class DiagnosticMigrationTest : ClientBasedLanguageServerTest {
 
   [TestMethod]
   public async Task ResolutionDiagnosticsContainPreviousVerificationResultsWhenCodeIsInsertedAfter() {
-    var documentItem = CreateTestDocument(FastToFailVerification);
+    var documentItem = CreateTestDocument(FastToFailVerification, "untitled:Untitled-1");
     client.OpenDocument(documentItem);
     var verificationDiagnostics = await GetLastVerificationDiagnostics(documentItem, CancellationToken);
     Assert.AreEqual(1, verificationDiagnostics.Length);
