3b6101e01fe545ddba79b6098aaed504030252bc
Remy Willems <rwillems@amazon.com>
2022-05-25 14:48:39 +0200

    Update ignore file and skip index in verification status

--- Source/DafnyLanguageServer/Workspace/FileVerificationStatus.cs
+++ Source/DafnyLanguageServer/Workspace/FileVerificationStatus.cs
@@ -42,6 +42,6 @@ public enum PublishedVerificationStatus {
   Stale = 0,    // Not scheduled to be run
   Queued = 1,   // Scheduled to be run but waiting for resources
   Running = 2,  // Currently running
-  Error = 3,    // Finished and had errors
-  Correct = 4,  // Finished and was correct
+  Error = 4,    // Finished and had errors
+  Correct = 5,  // Finished and was correct
 }
