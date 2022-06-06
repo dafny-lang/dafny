52f93a1640c28128f5a66c87d0cf616f45ad9338
Robin Salkeld <salkeldr@amazon.com>
2022-06-06 09:49:20 -0700

    Starting on arg parsing

--- Source/TestDafny/Program.cs
+++
@@ -1,3 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
---
+++ Source/TestDafny/TestDafny.cs
@@ -0,0 +1,46 @@
+﻿using CommandLine;
+using Microsoft.Dafny;
+using Parser = CommandLine.Parser;
+
+public class TestDafnyOptions {
+
+  [Value(0)]
+  public string? TestFile { get; set; } = null;
+
+  [Option("exit-code")]
+  public int? ExpectedExitCode { get; set; } = 0;
+  
+  [Value(1)]
+  public IEnumerable<string> OtherArgs { get; set; }
+}
+
+public class TestDafny {
+
+  public static int Main(string[] args) {
+    TestDafnyOptions? options = null;
+    var parser = new Parser(with => with.EnableDashDash = true);
+    parser.ParseArguments<TestDafnyOptions>(args).WithParsed(o => {
+      options = o;
+    });
+    return RunTest(options!);
+  }
+
+  private static int RunTest(TestDafnyOptions testDafnyOptions) {
+    var dafnyOptions = new DafnyOptions();
+    var success = dafnyOptions.Parse(testDafnyOptions.OtherArgs.ToArray());
+    if (!success) {
+      return -1;
+    }
+
+    foreach(var plugin in dafnyOptions.Plugins) {
+      foreach (var compiler in plugin.GetCompilers()) {
+        var dafnyArgs = new List<string>(testDafnyOptions.OtherArgs);
+        dafnyArgs.Add($"/compileTarget:{compiler.TargetId}");
+        Console.Out.WriteLine(string.Join(" ", dafnyArgs));
+        // TODO: Run test, compare to sourcefile.expect
+      }  
+    }
+    
+    return 0;
+  }
+}
