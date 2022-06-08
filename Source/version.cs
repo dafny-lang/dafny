5a0b2c92d23dc325bc1b9b5085dafcaefd50afce
Aaron Tomb <aarotomb@amazon.com>
2022-06-08 09:22:44 -0700

    Do package DafnyPipeline

--- Source/version.cs
+++ Source/version.cs
@@ -2,6 +2,6 @@ using System.Reflection;
 // Version 3.6.0, year 2018+4, month 5, day 11
 // When changing this, also be sure to change it in the following files:
 // * Source/DafnyDriver/DafnyDriver.csproj
-// * Source/DafnyLanguageServer/DafnyLanguageServer.csproj
+// * Source/Dafny/DafnyPipeline.csproj
 [assembly: AssemblyVersion("3.6.0.40511")]
 [assembly: AssemblyFileVersion("3.6.0.40511")]
