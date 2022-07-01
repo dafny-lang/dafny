c507a570571474f25301df592f6d149d79a9ad54
Mikael Mayer <mimayere@amazon.com>
2022-07-01 15:00:21 -0500

    Fixed the test suite

--- Source/DafnyLanguageServer.Test/Various/SlowVerifier.cs
+++ Source/DafnyLanguageServer.Test/Various/SlowVerifier.cs
@@ -37,6 +37,7 @@ class SlowVerifier : IProgramVerifier {
   }
 
   public IObservable<AssertionBatchResult> BatchCompletions => verifier.BatchCompletions;
+  public IObservable<Implementation> StartedImplementations => verifier.StartedImplementations;
 
   class NeverVerifiesImplementationTask : IImplementationTask {
     private readonly IImplementationTask original;
