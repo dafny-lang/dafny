dafny-lang/dafny

commit 53765c53768b5c398d12bd6ee196f3521a2efb24

Add structured proof obligation descriptions (#1915)
* Use the ProofObligationDescription class from Boogie in Dafny
* Change “may” to “might” in proof obligation failure messages
* Update other messages slightly for consistency and understandability

Co-authored-by: Remy Willems <rwillems@amazon.com>
Co-authored-by: Mikaël Mayer <MikaelMayer@users.noreply.github.com>

Source/Dafny/Verifier/Translator.ClassMembers.cs
@@ -6,6 +6,7 @@ using System.Text;
 using Microsoft.Boogie;
 using Bpl = Microsoft.Boogie;
 using static Microsoft.Dafny.Util;
+using PODesc = Microsoft.Dafny.ProofObligationDescription;
 
 namespace Microsoft.Dafny {
   public partial class Translator {
@@ -660,7 +661,8 @@ namespace Microsoft.Dafny {
           if (formal.IsOld) {
             Boogie.Expr wh = GetWhereClause(e.tok, etran.TrExpr(e), e.Type, etran.Old, ISALLOC, true);
             if (wh != null) {
-              builder.Add(Assert(e.tok, wh, "default value must be allocated in the two-state lemma's previous state"));
+              var desc = new PODesc.IsAllocated("default value", "in the two-state lemma's previous state");
+              builder.Add(Assert(e.tok, wh, desc));
             }
           }
         }
@@ -1189,7 +1191,7 @@ namespace Microsoft.Dafny {
         bool splitHappened;  // we actually don't care
         foreach (var s in TrSplitExpr(postcond, etran, false, out splitHappened)) {
           if (s.IsChecked) {
-            builder.Add(Assert(m.tok, s.E, "the method must provide an equal or more detailed postcondition than in its parent trait"));
+            builder.Add(Assert(m.tok, s.E, new PODesc.EnsuresStronger()));
           }
         }
       }
@@ -1210,7 +1212,7 @@ namespace Microsoft.Dafny {
         bool splitHappened;  // we actually don't care
         foreach (var s in TrSplitExpr(req.E, etran, false, out splitHappened)) {
           if (s.IsChecked) {
-            builder.Add(Assert(m.tok, s.E, "the method must provide an equal or more permissive precondition than in its parent trait"));
+            builder.Add(Assert(m.tok, s.E, new PODesc.RequiresWeaker()));
           }
         }
       }
@@ -1278,7 +1280,7 @@ namespace Microsoft.Dafny {
       //   as "false".
       bool allowNoChange = N == decrCountT && decrCountT <= decrCountC;
       var decrChk = DecreasesCheck(toks, types0, types1, callee, caller, null, null, allowNoChange, false);
-      builder.Add(Assert(original.Tok, decrChk, string.Format("{0}'s decreases clause must be below or equal to that in the trait", original.WhatKind)));
+      builder.Add(Assert(original.Tok, decrChk, new PODesc.TraitDecreases(original.WhatKind)));
     }
 
     private void AddMethodOverrideSubsetChk(Method m, BoogieStmtListBuilder builder, ExpressionTranslator etran, List<Variable> localVariables, Dictionary<IVariable, Expression> substMap) {
@@ -1321,7 +1323,7 @@ namespace Microsoft.Dafny {
       Boogie.Expr consequent2 = InRWClause(tok, o, f, traitFrameExps, etran, null, null);
       Boogie.Expr q = new Boogie.ForallExpr(tok, new List<TypeVariable> { alpha }, new List<Variable> { oVar, fVar },
         Boogie.Expr.Imp(Boogie.Expr.And(ante, oInCallee), consequent2));
-      builder.Add(Assert(tok, q, "expression may modify an object not in the parent trait context's modifies clause", kv));
+      builder.Add(Assert(tok, q, new PODesc.TraitFrame(true), kv));
     }
 
     /// <summary>
@@ -1382,9 +1384,12 @@ namespace Microsoft.Dafny {
         foreach (var formal in m.Ins) {
           if (formal.IsOld) {
             var dafnyFormalIdExpr = new IdentifierExpr(formal.tok, formal);
-            req.Add(Requires(formal.tok, false, MkIsAlloc(etran.TrExpr(dafnyFormalIdExpr), formal.Type, prevHeap),
-              string.Format("parameter{0} ('{1}') must be allocated in the two-state lemma's previous state",
-                m.Ins.Count == 1 ? "" : " " + index, formal.Name), null));
+            var pIdx = m.Ins.Count == 1 ? "" : " at index " + index;
+            var desc = new PODesc.IsAllocated($"parameter{pIdx} ('{formal.Name}')", "in the two-state lemma's previous state");
+            var require = Requires(formal.tok, false, MkIsAlloc(etran.TrExpr(dafnyFormalIdExpr), formal.Type, prevHeap),
+              desc.FailureDescription, null);
+            require.Description = desc;
+            req.Add(require);
           }
           index++;
         }

commit 08b62b80300fbd587d6f1a0bc930c1f58cacc4af

fix: Incorrect error message for modifies clause (closes #2384)
The bug was introduced by a typo in 53765c537.  This PR further improves the
message by not mentioning "expressions".

Source/Dafny/Verifier/ProofObligationDescription.cs
@@ -378,20 +378,22 @@ public class YieldEnsures : ProofObligationDescription {
 public class TraitFrame : ProofObligationDescription {
   public override string SuccessDescription =>
     isModify
-      ? "expression abides by trait context's modifies clause"
-      : "expression abides by trait context's reads clause";
+      ? $"{whatKind} abides by trait context's modifies clause"
+      : $"{whatKind} abides by trait context's reads clause";
 
   public override string FailureDescription =>
     isModify
-      ? "expression might read an object not in the parent trait context's reads clause"
-      : "expression might modify an object not in the parent trait context's modifies clause";
+      ? $"{whatKind} might modify an object not in the parent trait context's modifies clause"
+      : $"{whatKind} might read an object not in the parent trait context's reads clause";
 
   public override string ShortDescription =>
     isModify ? "trait modifies" : "trait reads";
 
+  private readonly string whatKind;
   private bool isModify;
 
-  public TraitFrame(bool isModify) {
+  public TraitFrame(string whatKind, bool isModify) {
+    this.whatKind = whatKind;
     this.isModify = isModify;
   }
 }

Source/Dafny/Verifier/Translator.ClassMembers.cs
@@ -1323,7 +1323,7 @@ namespace Microsoft.Dafny {
       Boogie.Expr consequent2 = InRWClause(tok, o, f, traitFrameExps, etran, null, null);
       Boogie.Expr q = new Boogie.ForallExpr(tok, new List<TypeVariable> { alpha }, new List<Variable> { oVar, fVar },
         Boogie.Expr.Imp(Boogie.Expr.And(ante, oInCallee), consequent2));
-      builder.Add(Assert(tok, q, new PODesc.TraitFrame(true), kv));
+      builder.Add(Assert(tok, q, new PODesc.TraitFrame(m.WhatKind, true), kv));
     }
 
     /// <summary>

Source/Dafny/Verifier/Translator.cs
@@ -3611,7 +3611,7 @@ namespace Microsoft.Dafny {
       Bpl.Expr consequent2 = InRWClause(tok, o, f, traitFrameExps, etran, null, null);
       Bpl.Expr q = new Bpl.ForallExpr(tok, new List<TypeVariable> { alpha }, new List<Variable> { oVar, fVar },
                                       Bpl.Expr.Imp(Bpl.Expr.And(ante, oInCallee), consequent2));
-      builder.Add(Assert(tok, q, new PODesc.TraitFrame(false), kv));
+      builder.Add(Assert(tok, q, new PODesc.TraitFrame(func.WhatKind, false), kv));
     }
 
     private void AddFunctionOverrideReqsChk(Function f, BoogieStmtListBuilder builder, ExpressionTranslator etran, Dictionary<IVariable, Expression> substMap) {

Test/git-issues/git-issue-2384.dfy
@@ -1,0 +1,19 @@
+// RUN: %dafny_0 "%s" > "%t"
+// RUN: %diff "%s.expect" "%t"
+
+trait T {
+  method M()
+  predicate P()
+  function F(): int
+}
+
+class C extends T {
+  method M() // method might modify an object not in the parent trait context's modifies clause
+    modifies this
+
+  predicate P() // predicate might read an object not in the parent trait context's reads clause
+    reads this
+
+  function F(): int // function might read an object not in the parent trait context's reads clause
+    reads this
+}

Test/git-issues/git-issue-2384.dfy.expect
@@ -1,0 +1,5 @@
+git-issue-2384.dfy(11,9): Error: method might modify an object not in the parent trait context's modifies clause
+git-issue-2384.dfy(14,12): Error: predicate might read an object not in the parent trait context's reads clause
+git-issue-2384.dfy(17,11): Error: function might read an object not in the parent trait context's reads clause
+
+Dafny program verifier finished with 0 verified, 3 errors