9121f5cdea17abec4cf0cc2993918b6ddc556eed
Aaron Tomb <aarotomb@amazon.com>
2022-07-26 16:41:02 -0700

    Remove obsolete changes

--- Source/Dafny/Resolver/BoundsDiscovery.cs
+++ Source/Dafny/Resolver/BoundsDiscovery.cs
@@ -5,11 +5,9 @@
 //
 //-----------------------------------------------------------------------------
 
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Diagnostics.Contracts;
-using System.Numerics;
 using Microsoft.Boogie;
 
 namespace Microsoft.Dafny {
