65b152450a2564b78cfde20d9c0e4a65615bafe7
Remy Willems <rwillems@amazon.com>
2022-07-27 11:44:21 +0200

    Add debug message

--- Source/DafnyLanguageServer.Test/Util/ClientBasedLanguageServerTest.cs
+++ Source/DafnyLanguageServer.Test/Util/ClientBasedLanguageServerTest.cs
@@ -159,13 +159,14 @@ public class ClientBasedLanguageServerTest : DafnyLanguageServerTestBase {
     var resolutionReport = await diagnosticsReceiver.AwaitNextNotificationAsync(cancellationToken);
     Assert.AreEqual(verificationDocumentItem.Uri, resolutionReport.Uri,
       "Unexpected diagnostics were received whereas none were expected:\n" +
-      string.Join(",", resolutionReport.Diagnostics.Select(diagnostic =>
-        diagnostic.ToString())));
+      string.Join(",", resolutionReport.Diagnostics.Select(diagnostic => diagnostic.ToString())));
     client.DidCloseTextDocument(new DidCloseTextDocumentParams {
       TextDocument = verificationDocumentItem
     });
     var hideReport = await diagnosticsReceiver.AwaitNextNotificationAsync(cancellationToken);
-    Assert.AreEqual(verificationDocumentItem.Uri, hideReport.Uri);
+    Assert.AreEqual(verificationDocumentItem.Uri, hideReport.Uri,
+      "Unexpected diagnostics were received whereas none were expected:\n" +
+      string.Join(",", hideReport.Diagnostics.Select(diagnostic => diagnostic.ToString())));
   }
 
   protected async Task AssertNoResolutionErrors(TextDocumentItem documentItem) {
