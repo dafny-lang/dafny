a556fcc14aa724a02afd12cf00cc5ce65aa8e1b8
Remy Willems <rwillems@amazon.com>
2022-08-11 14:21:21 +0200

    Do not publish gutter status notification when the option is turned off

--- Source/DafnyLanguageServer/Workspace/NotificationPublisher.cs
+++ Source/DafnyLanguageServer/Workspace/NotificationPublisher.cs
@@ -5,14 +5,18 @@ using OmniSharp.Extensions.LanguageServer.Protocol.Document;
 using OmniSharp.Extensions.LanguageServer.Protocol.Models;
 using OmniSharp.Extensions.LanguageServer.Protocol.Server;
 using System.Linq;
+using Microsoft.Dafny.LanguageServer.Language;
+using Microsoft.Extensions.Options;
 
 namespace Microsoft.Dafny.LanguageServer.Workspace {
 
   public class NotificationPublisher : INotificationPublisher {
     private readonly ILanguageServerFacade languageServer;
+    private readonly VerifierOptions verifierOptions;
 
-    public NotificationPublisher(ILanguageServerFacade languageServer) {
+    public NotificationPublisher(ILanguageServerFacade languageServer, IOptions<VerifierOptions> verifierOptions) {
       this.languageServer = languageServer;
+      this.verifierOptions = verifierOptions.Value;
     }
 
     public void PublishNotifications(DafnyDocument previous, DafnyDocument document) {
@@ -82,6 +86,10 @@ namespace Microsoft.Dafny.LanguageServer.Workspace {
     }
 
     public void PublishGutterIcons(DafnyDocument document, bool verificationStarted) {
+      if (!verifierOptions.GutterStatus) {
+        return;
+      }
+
       if (document.LoadCanceled) {
         // We leave the responsibility to shift the error locations to the LSP clients.
         // Therefore, we do not republish the errors when the document (re-)load was canceled.
