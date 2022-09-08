9ef5edbd72a2cab0a8524de65ff31ff808081b2b
Remy Willems <rwillems@amazon.com>
2022-09-08 18:34:55 +0200

    Improve documentation

--- Scripts/git-cp.sh
+++ Scripts/git-cp.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+
+# This file enables splitting code off from an existing file while keeping the history of the split off code intact
+# Using this script, a history preserving copy can be made of the original file
+# The user must then manually prune both the existing and new file so no duplicate code is left.
 if [ $# -lt 2 ]; then
     echo "Usage: git cp <src> <dst>"
     exit 1
