4e2e6bd75897668b08acb915765a4d54c5117d68
Aaron Tomb <aarotomb@amazon.com>
2022-09-19 14:43:03 -0700

    Add previously-omitted C# files for contract-wrappers tests

---
+++ Test/contract-wrappers/CheckExtern.dfy.externs.cs
@@ -0,0 +1,13 @@
+using System.Numerics;
+
+namespace _module {
+
+  public partial class __default {
+    public static BigInteger Foo(BigInteger x) {
+      return BigInteger.Zero;
+    }
+    public static BigInteger Bar(BigInteger x) {
+      return BigInteger.Zero;
+    }
+  }
+}
