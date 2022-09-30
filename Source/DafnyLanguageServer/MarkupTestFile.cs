6ceb633101c151de802f21346cdaa879353a8d45
Remy Willems <rgv.willems@gmail.com>
2022-09-30 10:26:09 +0200

    Update Source/DafnyLanguageServer/MarkupTestFile.cs
    Co-authored-by: Mikaël Mayer <MikaelMayer@users.noreply.github.com>

--- Source/DafnyLanguageServer/MarkupTestFile.cs
+++ Source/DafnyLanguageServer/MarkupTestFile.cs
@@ -23,7 +23,7 @@ namespace Microsoft.Dafny.LanguageServer {
   /// information in it.  This prevents a test writer from having to carry around multiple sources
   /// of information that must be reconstituted.  For example, instead of having to keep around the
   /// contents of a file *and* and the location of the cursor, the tester can just provide a
-  /// string with the "$" character in it.  This allows for easy creation of "FIT" tests where all
+  /// string with the "><" string in it.  This allows for easy creation of "FIT" tests where all
   /// that needs to be provided are strings that encode every bit of state necessary in the string
   /// itself.
   /// 
