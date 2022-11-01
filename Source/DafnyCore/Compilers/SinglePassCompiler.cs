6dede64eff21970c051d1bab5e52407b04958163
Mikael Mayer <mimayere@amazon.com>
2022-11-01 16:35:33 -0500

    Fix: Tail recursion not activated if cannot

--- Source/DafnyCore/Compilers/SinglePassCompiler.cs
+++ Source/DafnyCore/Compilers/SinglePassCompiler.cs
@@ -2650,10 +2650,11 @@ namespace Microsoft.Dafny.Compilers {
         // finally, the jump back to the head of the function
         EmitJumpToTailCallStart(wr);
 
-      } else if (expr is BinaryExpr bin && bin.AccumulatesForTailRecursion != BinaryExpr.AccumulationOperand.None) {
-        Contract.Assert(accumulatorVar != null);
-        Contract.Assert(enclosingFunction != null);
-        Contract.Assert(enclosingFunction.IsAccumulatorTailRecursive);
+      } else if (expr is BinaryExpr bin
+                 && bin.AccumulatesForTailRecursion != BinaryExpr.AccumulationOperand.None
+                 && accumulatorVar != null
+                 && enclosingFunction != null
+                 && enclosingFunction.IsAccumulatorTailRecursive) {
         Expression tailTerm;
         Expression rhs;
         var acc = new IdentifierExpr(expr.tok, accumulatorVar);
