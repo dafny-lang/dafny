9d538b74720aecf1a350c3e6a91b21a89d008379
Mikael Mayer <mimayere@amazon.com>
2022-11-02 15:07:48 -0500

    Fixed the build

--- Source/DafnyCore/AST/Expressions.cs
+++ Source/DafnyCore/AST/Expressions.cs
@@ -1034,10 +1034,8 @@ public class DatatypeValue : Expression, IHasUsages {
     Bindings.AcceptArgumentExpressionsAsExactParameterList();
   }
 
-  public override IEnumerable<Expression> SubExpressions {
-    get {
-      return Arguments ?? Enumerable.Empty<Expression>();
-  }
+  public override IEnumerable<Expression> SubExpressions =>
+    Arguments ?? Enumerable.Empty<Expression>();
 
   public IEnumerable<IDeclarationOrUsage> GetResolvedDeclarations() {
     return Enumerable.Repeat(Ctor, 1);
