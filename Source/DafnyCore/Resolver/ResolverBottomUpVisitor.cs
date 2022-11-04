6f3e5cbcf94de3b4c81a4b7aac1704ab19cdf6ad
Remy Willems <rwillems@amazon.com>
2022-11-04 17:34:16 +0100

    Refactored out CheckTypeInferenceVisitor

---
+++ Source/DafnyCore/Resolver/ResolverBottomUpVisitor.cs
@@ -0,0 +1,9 @@
+namespace Microsoft.Dafny;
+
+class ResolverBottomUpVisitor : BottomUpVisitor {
+  protected Resolver resolver;
+  public ResolverBottomUpVisitor(Resolver resolver) {
+    Contract.Requires(resolver != null);
+    this.resolver = resolver;
+  }
+}
