8d89c99d63e488f656efab0b415758b2a7ddf64a
davidcok <davidcok@github.com>
2022-11-18 17:49:46 -0500

    Removing userexamples

--- Source/IntegrationTests/LitTests.cs
+++ Source/IntegrationTests/LitTests.cs
@@ -45,7 +45,6 @@ namespace IntegrationTests {
         { "%binaryDir", "." },
         { "%z3", Path.Join("z3", "bin", "z3") },
         { "%repositoryRoot", repositoryRoot.Replace(@"\", "/") },
-        { "%userexamples", Path.Join("TestFiles", "LitTests", "LitTest", "examples") },
         { "%refmanexamples", Path.Join("TestFiles", "LitTests", "LitTest", "refman", "examples") }
       };
 
