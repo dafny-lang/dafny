ea72d0eaafa267a34ae14ce6d3221723a5643e17
Remy Willems <rwillems@amazon.com>
2022-11-22 12:28:25 +0100

    Rename option

--- Source/DafnyCore/Options/VerifyIncludesOption.cs
+++ Source/DafnyCore/Options/VerifyIncludesOption.cs
@@ -2,7 +2,7 @@ namespace Microsoft.Dafny;
 
 public class VerifyIncludesOption : BooleanOption {
   public static readonly VerifyIncludesOption Instance = new();
-  public override string LongName => "verify-includes";
+  public override string LongName => "verify-included-files";
 
   public override string Description => @"Verify code in included files.";
 
