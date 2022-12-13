0108c43c8bdc704b0a74394b9fe7e293ea114b62
Mikael Mayer <mimayere@amazon.com>
2022-12-13 13:48:46 -0600

    Transposed atomp:fix-pr-2783 on top of ownedTokens

--- Source/DafnyCore/PrecedenceLinter.cs
+++ Source/DafnyCore/PrecedenceLinter.cs
@@ -92,6 +92,8 @@ namespace Microsoft.Dafny {
           // a SeqSelectExpr is the open-bracket). To avoid looking at that internal structure, we instead
           // use the .StartToken for these expressions.
           column = expr.StartToken.col;
+        } else if (expr is BinaryExpr binaryExpr && binaryExpr.OwnedTokens.Count() > 1) {
+          column = binaryExpr.OwnedTokens.FirstOrDefault()!.col;
         } else {
           column = expr.tok.col;
         }
