dafny-lang/dafny

commit 06c52ecc4c3dbe6428d2e397556633c215726114

Add warnings about possibly confused operator precedence (#2783)
A common mistake is to write an expression like

``` dafny
&& A
&& B ==> C
&& D
```

with the intention of writing

``` dafny
&& A
&& (B ==> C)
&& D
```

This PR emits warnings when certain operator arguments seem to go on for
too long, where "seem to" is judged based on identation.

The basic idea is to look where the right-hand operand of `==>` starts
and then insist that all tokens in the RHS are aligned at, or to the
right of, the column of the RHS start. This same idea is used for other
kinds of "right-hand sides", too, like the `else` branch of an
`if-then-else` expression and the body ("term") of logical quantifiers.

When looking at the RHS, certain parts of the RHS expression are ignored
(or, rather, are treated separately). This happens when an expression
has a "middle" part, like the `then` branch of an `if-then-else`
expression. For example, no warning is produced for

``` dafny
  if x < 0 then
-x
    else x
```

If an expression of interest `e` is found in the RHS, then `e`'s
internal structure is analyzed for `e` but not for the enclosing
expression. For example, no warning is produced for

``` dafny
  forall x :: P(x) ==>
Q(x)
```

The left-hand side of `==>` is also analyzed. Some more details are
found in the `VisitOneExpr` method in `PrecedenceLinter.cs`.

The binary operators `==>`, `<==`, and `<==>` are treated as mentioned
above, since these operators have relatively low binding powers. The
linter does not do this processing for any other binary operators, like
`&&`, `||`, `+`, and `*`.

Fixes #1311

<small>By submitting this pull request, I confirm that my contribution
is made under the terms of the [MIT
license](https://github.com/dafny-lang/dafny/blob/master/LICENSE.txt).</small>

Source/DafnyCore/AST/DafnyAst.cs
@@ -904,7 +904,7 @@ namespace Microsoft.Dafny {
       // recursively visit all subexpressions and all substatements
       expr.SubExpressions.Iter(Visit);
       if (expr is StmtExpr) {
-        // a StmtExpr also has a sub-statement
+        // a StmtExpr also has a substatement
         var e = (StmtExpr)expr;
         Visit(e.S);
       }
@@ -933,7 +933,7 @@ namespace Microsoft.Dafny {
         // recursively visit all subexpressions and all substatements
         expr.SubExpressions.Iter(e => Visit(e, st));
         if (expr is StmtExpr) {
-          // a StmtExpr also has a sub-statement
+          // a StmtExpr also has a substatement
           var e = (StmtExpr)expr;
           Visit(e.S, st);
         }
@@ -991,7 +991,7 @@ namespace Microsoft.Dafny {
     }
     /// <summary>
     /// Visit one expression proper.  This method is invoked before it is invoked on the
-    /// sub-parts (sub-expressions and sub-statements).  A return value of "true" says to
+    /// sub-parts (subexpressions and substatements).  A return value of "true" says to
     /// continue invoking the method on sub-parts, whereas "false" says not to do so.
     /// The on-return value of "st" is the value that is passed to sub-parts.
     /// </summary>

Source/DafnyCore/Cloner.cs
@@ -842,6 +842,14 @@ namespace Microsoft.Dafny {
     }
   }
 
+  public class ClonerKeepParensExpressions : Cloner {
+    public override Expression CloneExpr(Expression expr) {
+      if (expr is ParensExpression parensExpression) {
+        return new ParensExpression(Tok(parensExpression.tok), CloneExpr(parensExpression.E));
+      }
+      return base.CloneExpr(expr);
+    }
+  }
 
   /// <summary>
   /// This cloner copies the origin module signatures to their cloned declarations

Source/DafnyCore/Dafny.atg
@@ -3805,35 +3805,30 @@ ImpliesExpression<out Expression e0, bool allowLemma, bool allowLambda, bool all
 /*------------------------------------------------------------------------*/
 LogicalExpression<out Expression e0, bool allowLemma, bool allowLambda, bool allowBitwiseOps>
 = (. Contract.Ensures(Contract.ValueAtReturn(out e0) != null); IToken/*!*/ x;  Expression/*!*/ e1;
-     Expression first;
      e0 = dummyExpr; /* mute the warning */
   .)
   ( AndOp                                                       (. x = t; .)
-    RelationalExpression<out e0, allowLemma, allowLambda, allowBitwiseOps>        (. first = e0; .)
+    RelationalExpression<out e0, allowLemma, allowLambda, allowBitwiseOps>
+    // We conjoin "true" to the left, for two reasons. One reason is to keep track of the source
+    // location of the initial AndOp operator. The other reason is to make, when there is only
+    // one conjunct, that the type checker still checks it to be a boolean.
+    (. e0 = new BinaryExpr(x, BinaryExpr.Opcode.And, new LiteralExpr(new AutoGeneratedToken(x), true), e0); .)
     { IF(IsAndOp())  /* read a conjunction as far as possible */
       AndOp                                                     (. x = t; .)
       RelationalExpression<out e1, allowLemma, allowLambda, allowBitwiseOps>      (. e0 = new BinaryExpr(x, BinaryExpr.Opcode.And, e0, e1); .)
     }
     [ IF(IsOrOp()) OrOp (. SemErr(t, "Ambiguous use of && and ||. Use parentheses to disambiguate."); .) ]
-    (. if (e0 == first) {
-         // There was only one conjunct. To make sure that the type checker still checks it to
-         // be a boolean, we conjoin "true" to its left.
-         e0 = new BinaryExpr(x, BinaryExpr.Opcode.And, new LiteralExpr(x, true), e0);
-       }
-    .)
   | OrOp                                                        (. x = t; .)
-    RelationalExpression<out e0, allowLemma, allowLambda, allowBitwiseOps>        (. first = e0; .)
+    RelationalExpression<out e0, allowLemma, allowLambda, allowBitwiseOps>
+    // We disjoin [sic] "false" to the left, for two reasons. One reason is to keep track of the source
+    // location of the initial OrOp operator. The other reason is to make, when there is only
+    // one disjunct, that the type checker still checks it to be a boolean.
+    (. e0 = new BinaryExpr(x, BinaryExpr.Opcode.Or, new LiteralExpr(new AutoGeneratedToken(x), false), e0); .)
     { IF(IsOrOp())  /* read a disjunction as far as possible */
       OrOp                                                      (. x = t; .)
       RelationalExpression<out e1, allowLemma, allowLambda, allowBitwiseOps>      (. e0 = new BinaryExpr(x, BinaryExpr.Opcode.Or, e0, e1); .)
     }
     [ IF(IsAndOp()) AndOp (. SemErr(t, "Ambiguous use of && and ||. Use parentheses to disambiguate."); .) ]
-    (. if (e0 == first) {
-         // There was only one disjunct. To make sure that the type checker still checks it to
-         // be a boolean, we disjoin [sic] "false" to its left.
-         e0 = new BinaryExpr(x, BinaryExpr.Opcode.Or, new LiteralExpr(x, false), e0);
-       }
-    .)
   | RelationalExpression<out e0, allowLemma, allowLambda, allowBitwiseOps>
     [ IF(IsAndOp() || IsOrOp())  /* read a LogicalExpression as far as possible */
       ( AndOp                                                   (. x = t; .)
@@ -4916,7 +4911,7 @@ SetComprehensionExpr<out Expression q, bool allowLemma, bool allowLambda, bool a
        // range was not optional, so we map null to "true" here so that the rest of the
        // logic doesn't hit exceptions.
        if (range == null) {
-         range = LiteralExpr.CreateBoolLiteral(Token.NoToken, true);
+         range = LiteralExpr.CreateBoolLiteral(new AutoGeneratedToken(t), true);
        }
        q = new SetComprehension(setToken, t, finite, bvars, range, body, attrs);
      }

Source/DafnyCore/AST/Expressions.cs
@@ -104,17 +104,23 @@ public abstract class Expression : INode {
         } else {
           var startTok = tok;
           var endTok = tok;
-          foreach (var e in SubExpressions) {
-            if (e.tok.Filename != tok.Filename || e.IsImplicit) {
-              // Ignore auto-generated expressions, if any.
-              continue;
-            }
 
-            if (e.StartToken.pos < startTok.pos) {
-              startTok = e.StartToken;
-            } else if (e.EndToken.pos > endTok.pos) {
-              endTok = e.EndToken;
+          void updateStartEndTok(Expression expression) {
+            if (expression.tok.Filename != tok.Filename || expression.IsImplicit || expression is DefaultValueExpression) {
+              // Ignore any auto-generated expressions.
+            } else {
+              if (expression.StartToken.pos < startTok.pos) {
+                startTok = expression.StartToken;
+              }
+              if (endTok.pos < expression.EndToken.pos) {
+                endTok = expression.EndToken;
+              }
             }
+          }
+
+          SubExpressions.Iter(updateStartEndTok);
+          if (this is StmtExpr stmtExpr) {
+            stmtExpr.S.SubStatements.Iter(s => s.SubExpressions.Iter(updateStartEndTok));
           }
 
           if (FormatTokens != null) {
@@ -146,7 +152,7 @@ public abstract class Expression : INode {
 
   /// <summary>
   /// Returns the list of types that appear in this expression proper (that is, not including types that
-  /// may appear in subexpressions). Types occurring in sub-statements of the expression are not included.
+  /// may appear in subexpressions). Types occurring in substatements of the expression are not included.
   /// To be called after the expression has been resolved.
   /// </summary>
   public virtual IEnumerable<Type> ComponentTypes {
@@ -498,6 +504,13 @@ public abstract class Expression : INode {
     expr = StripParens(expr);
     return (expr is SetDisplayExpr && ((SetDisplayExpr)expr).Elements.Count == 0) ||
            (expr is MultiSetDisplayExpr && ((MultiSetDisplayExpr)expr).Elements.Count == 0);
+  }
+
+  /// <summary>
+  /// Create a resolved ParensExpression around a given resolved expression "e".
+  /// </summary>
+  public static Expression CreateParensExpression(IToken tok, Expression e) {
+    return new ParensExpression(tok, e) { Type = e.Type, ResolvedExpression = e };
   }
 
   public static Expression CreateNot(IToken tok, Expression e) {
@@ -912,12 +925,7 @@ public class LiteralExpr : Expression {
   [Pure]
   public static bool IsTrue(Expression e) {
     Contract.Requires(e != null);
-    if (e is LiteralExpr) {
-      LiteralExpr le = (LiteralExpr)e;
-      return le.Value is bool && (bool)le.Value;
-    } else {
-      return false;
-    }
+    return Expression.IsBoolLiteral(e, out var value) && value;
   }
 
   public static bool IsEmptySet(Expression e) {

Source/DafnyCore/AST/Statements.cs
@@ -486,7 +486,7 @@ public abstract class AssignmentRhs : INode {
     }
   }
   /// <summary>
-  /// Returns the non-null sub-statements of the AssignmentRhs.
+  /// Returns the non-null substatements of the AssignmentRhs.
   /// </summary>
   public virtual IEnumerable<Statement> SubStatements {
     get { yield break; }

commit 7d7490e97440551e0acccddf8b6be9de528a7472

Undo the extra expressions added by PR #2783 (#3179)
This uses `FormatTokens` to keep track of the beginning of a sequence of
expressions prefixed by `&&` or `||` instead of a synthetic identity
expression.

Fixes a performance regression caused by #2783.

By submitting this pull request, I confirm that my contribution
is made under the terms of the [MIT
license](https://github.com/dafny-lang/dafny/blob/master/LICENSE.txt).

Source/DafnyCore/AST/DafnyAst.cs
@@ -69,6 +69,10 @@ namespace Microsoft.Dafny {
             }
 
             void updateStartEndTokRecursive(INode node) {
+              if (node is null) {
+                return;
+              }
+
               if (node.tok.Filename != tok.Filename || node is Expression { IsImplicit: true } ||
                   node is DefaultValueExpression) {
                 // Ignore any auto-generated expressions.

Source/DafnyCore/AST/Expressions/Expressions.cs
@@ -1651,6 +1651,7 @@ public class BinaryExpr : Expression {
     BitwiseXor
   }
   public readonly Opcode Op;
+  public readonly IToken PrefixOp;
   public enum ResolvedOpcode {
     YetUndetermined,  // the value before resolution has determined the value; .ResolvedOp should never be read in this state
 
@@ -1928,7 +1929,7 @@ public class BinaryExpr : Expression {
     Contract.Invariant(E1 != null);
   }
 
-  public BinaryExpr(IToken tok, Opcode op, Expression e0, Expression e1)
+  public BinaryExpr(IToken tok, Opcode op, Expression e0, Expression e1, IToken prefixOp = null)
     : base(tok) {
     Contract.Requires(tok != null);
     Contract.Requires(e0 != null);
@@ -1936,13 +1937,17 @@ public class BinaryExpr : Expression {
     this.Op = op;
     this.E0 = e0;
     this.E1 = e1;
+    this.PrefixOp = prefixOp;
+    if (prefixOp != null) {
+      FormatTokens = new[] { prefixOp };
+    }
   }
 
   /// <summary>
   /// Returns a resolved binary expression
   /// </summary>
-  public BinaryExpr(IToken tok, BinaryExpr.ResolvedOpcode rop, Expression e0, Expression e1)
-    : this(tok, BinaryExpr.ResolvedOp2SyntacticOp(rop), e0, e1) {
+  public BinaryExpr(IToken tok, BinaryExpr.ResolvedOpcode rop, Expression e0, Expression e1, IToken prefixOp = null)
+    : this(tok, BinaryExpr.ResolvedOp2SyntacticOp(rop), e0, e1, prefixOp) {
     ResolvedOp = rop;
     switch (rop) {
       case ResolvedOpcode.EqCommon:

Source/DafnyCore/Cloner.cs
@@ -399,8 +399,8 @@ namespace Microsoft.Dafny {
 
       } else if (expr is BinaryExpr) {
         var e = (BinaryExpr)expr;
-        return new BinaryExpr(Tok(e.tok), e.Op, CloneExpr(e.E0), CloneExpr(e.E1));
-
+        var prefixOp = e.PrefixOp is null ? null : Tok(e.PrefixOp);
+        return new BinaryExpr(Tok(e.tok), e.Op, CloneExpr(e.E0), CloneExpr(e.E1), prefixOp);
       } else if (expr is TernaryExpr) {
         var e = (TernaryExpr)expr;
         return new TernaryExpr(Tok(e.tok), e.Op, CloneExpr(e.E0), CloneExpr(e.E1), CloneExpr(e.E2));

Source/DafnyCore/Dafny.atg
@@ -3922,45 +3922,47 @@ ImpliesExpression<out Expression e0, bool allowLemma, bool allowLambda, bool all
 /*------------------------------------------------------------------------*/
 LogicalExpression<out Expression e0, bool allowLemma, bool allowLambda, bool allowBitwiseOps>
 = (. Contract.Ensures(Contract.ValueAtReturn(out e0) != null); IToken/*!*/ x;  Expression/*!*/ e1;
+     Expression first;
      IToken startToken = null;
      IToken/*!*/ firstOp;
      e0 = dummyExpr; /* mute the warning */
   .)
     // This extra AndOp will belong to the outermost binary expression
   ( AndOp                                                       (. x = t; firstOp = x; .)
-    RelationalExpression<out e0, allowLemma, allowLambda, allowBitwiseOps>
-    // We conjoin "true" to the left, for two reasons. One reason is to keep track of the source
-    // location of the initial AndOp operator. The other reason is to make, when there is only
-    // one conjunct, that the type checker still checks it to be a boolean.
-    (. startToken = e0.StartToken;
-       e0 = new BinaryExpr(x, BinaryExpr.Opcode.And, new LiteralExpr(new AutoGeneratedToken(x), true), e0);
-       e0.RangeToken = new RangeToken(startToken, t);
-    .)
+    RelationalExpression<out e0, allowLemma, allowLambda, allowBitwiseOps> (. first = e0; .)
     { IF(IsAndOp())  /* read a conjunction as far as possible */
       AndOp                                                     (. x = t; .)
       RelationalExpression<out e1, allowLemma, allowLambda, allowBitwiseOps>
          (. startToken = e0.StartToken;
-            e0 = new BinaryExpr(x, BinaryExpr.Opcode.And, e0, e1);
+            e0 = new BinaryExpr(x, BinaryExpr.Opcode.And, e0, e1, firstOp);
             e0.RangeToken = new RangeToken(startToken, t); .)
     }
     [ IF(IsOrOp()) OrOp (. SemErr(t, "Ambiguous use of && and ||. Use parentheses to disambiguate."); .) ]
-    (. e0.RangeToken = new RangeToken(firstOp, t); .)
+    (. e0.RangeToken = new RangeToken(firstOp, t);
+       if (e0 == first) {
+         // There was only one conjunct. To make sure that the type checker still checks it to
+         // be a boolean, we conjoin "true" to its left.
+         e0 = new BinaryExpr(x, BinaryExpr.Opcode.And, new LiteralExpr(new AutoGeneratedToken(x), true), e0, firstOp);
+         e0.RangeToken = new RangeToken(firstOp, t);
+       }
+    .)
   | OrOp                                                        (. x = t; firstOp = x; .)
-    RelationalExpression<out e0, allowLemma, allowLambda, allowBitwiseOps>
-    // We disjoin [sic] "false" to the left, for two reasons. One reason is to keep track of the source
-    // location of the initial OrOp operator. The other reason is to make, when there is only
-    // one disjunct, that the type checker still checks it to be a boolean.
-    (. startToken = e0.StartToken;
-       e0 = new BinaryExpr(x, BinaryExpr.Opcode.Or, new LiteralExpr(new AutoGeneratedToken(x), false), e0);
-       e0.RangeToken = new RangeToken(startToken, t); .)
+    RelationalExpression<out e0, allowLemma, allowLambda, allowBitwiseOps> (. first = e0; .)
     { IF(IsOrOp())  /* read a disjunction as far as possible */
       OrOp                                                      (. x = t; .)
       RelationalExpression<out e1, allowLemma, allowLambda, allowBitwiseOps>
          (. startToken = e0.StartToken;
-            e0 = new BinaryExpr(x, BinaryExpr.Opcode.Or, e0, e1);
+            e0 = new BinaryExpr(x, BinaryExpr.Opcode.Or, e0, e1, firstOp);
             e0.RangeToken = new RangeToken(startToken, t); .)    }
     [ IF(IsAndOp()) AndOp (. SemErr(t, "Ambiguous use of && and ||. Use parentheses to disambiguate."); .) ]
-    (. e0.RangeToken = new RangeToken(firstOp, t); .)
+    (. e0.RangeToken = new RangeToken(firstOp, t);
+       if (e0 == first) {
+         // There was only one disjunct. To make sure that the type checker still checks it to
+         // be a boolean, we disjoin [sic] "false" to its left.
+         e0 = new BinaryExpr(x, BinaryExpr.Opcode.Or, new LiteralExpr(new AutoGeneratedToken(x), false), e0, firstOp);
+         e0.RangeToken = new RangeToken(firstOp, t);
+       }
+    .)
   | RelationalExpression<out e0, allowLemma, allowLambda, allowBitwiseOps>
     [ IF(IsAndOp() || IsOrOp())  /* read a LogicalExpression as far as possible */
       ( AndOp                                                   (. x = t; .)

Source/DafnyCore/Resolver/BoundsDiscovery.cs
@@ -390,7 +390,7 @@ namespace Microsoft.Dafny {
             goto JUST_RETURN_IT;
         }
         if (newROp != b.ResolvedOp || swapOperands) {
-          b = new BinaryExpr(b.tok, newOp, swapOperands ? b.E1 : b.E0, swapOperands ? b.E0 : b.E1);
+          b = new BinaryExpr(b.tok, newOp, swapOperands ? b.E1 : b.E0, swapOperands ? b.E0 : b.E1, b.PrefixOp);
           b.ResolvedOp = newROp;
           b.Type = Type.Bool;
           yield return b;

Source/DafnyCore/Substituter.cs
@@ -264,7 +264,7 @@ namespace Microsoft.Dafny {
         Expression e0 = Substitute(e.E0);
         Expression e1 = Substitute(e.E1);
         if (e0 != e.E0 || e1 != e.E1) {
-          BinaryExpr newBin = new BinaryExpr(expr.tok, e.Op, e0, e1);
+          BinaryExpr newBin = new BinaryExpr(expr.tok, e.Op, e0, e1, e.PrefixOp);
           newBin.ResolvedOp = e.ResolvedOp;  // part of what needs to be done to resolve on the fly (newBin.Type is set below, at end)
           newExpr = newBin;
         }