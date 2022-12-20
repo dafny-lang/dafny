c441e58036d47215b7e00b0144d10a3f4f144ab0
Remy Willems <rwillems@amazon.com>
2022-12-20 13:24:48 +0100

    Fix help for --quantifier-syntax option

--- Source/DafnyCore/Options/CommonOptionBag.cs
+++ Source/DafnyCore/Options/CommonOptionBag.cs
@@ -136,6 +136,7 @@ true - The char type represents any Unicode scalar value.".TrimStart());
 Functionality is still being expanded. Currently only checks contracts on every call to a function or method marked with the {:extern} attribute.".TrimStart());
 
   static CommonOptionBag() {
+    QuantifierSyntax = QuantifierSyntax.FromAmong("3", "4");
     DafnyOptions.RegisterLegacyBinding(SolverPath, (options, value) => {
       if (!string.IsNullOrEmpty(value)) {
         options.ProverOptions.Add($"PROVER_PATH={value}");
