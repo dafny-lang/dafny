b7c5bbf2b4f2b882b3f66e9a42e489ada4d225bc
Remy Willems <rwillems@amazon.com>
2022-12-20 14:31:17 +0100

    Let target be an argument instead of an option for translate

--- Source/DafnyDriver/Commands/TranslateCommand.cs
+++ Source/DafnyDriver/Commands/TranslateCommand.cs
@@ -11,12 +11,26 @@ class TranslateCommand : ICommandSpec {
       CommonOptionBag.Output,
       CommonOptionBag.CompileVerbose,
       CommonOptionBag.IncludeRuntime,
-    }.Concat(ICommandSpec.ExecutionOptions).
+    }.Concat(ICommandSpec.TranslationOptions).
       Concat(ICommandSpec.ConsoleOutputOptions).
       Concat(ICommandSpec.CommonOptions);
 
+  private static readonly Argument<string> Target = new("target",@"
+cs - Translate to C#.
+go - Translate to Go.
+js - Translate to JavaScript.
+java - Translate to Java.
+py - Translate to Python.
+cpp - Translate to C++.
+
+Note that the C++ backend has various limitations (see Docs/Compilation/Cpp.md). This includes lack of support for BigIntegers (aka int), most higher order functions, and advanced features like traits or co-inductive types.".TrimStart()
+  ) {
+    HelpName = "language"
+  };
+
   public Command Create() {
     var result = new Command("translate", "Generate source and build files in a specified target language.");
+    result.AddArgument(Target);
     result.AddArgument(ICommandSpec.FilesArgument);
     return result;
   }
@@ -24,6 +38,7 @@ class TranslateCommand : ICommandSpec {
   public void PostProcess(DafnyOptions dafnyOptions, Options options, InvocationContext context) {
     dafnyOptions.Compile = false;
     var noVerify = dafnyOptions.Get(BoogieOptionBag.NoVerify);
+    dafnyOptions.CompilerName = context.ParseResult.GetValueForArgument(Target);
     dafnyOptions.SpillTargetCode = noVerify ? 3U : 2U;
   }
 }
