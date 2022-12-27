c6fc6b917db424ae29984224dee62151b82ac237
Fabio Madge <fmadge@amazon.com>
2022-12-27 22:30:26 +0100

    remove assertion

--- Source/DafnyRuntime/DafnyRuntimeJava/src/main/java/dafny/Array.java
+++ Source/DafnyRuntime/DafnyRuntimeJava/src/main/java/dafny/Array.java
@@ -26,8 +26,6 @@ public final class Array<T> implements Cloneable {
     private final Object array;
 
     private Array(TypeDescriptor<T> eltType, Object array) {
-        assert eltType.arrayType().isInstance(array);
-
         this.eltType = eltType;
         this.array = array;
     }
