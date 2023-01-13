080a46170327a5d7db83886f34b14fe80ec6ac55
Remy Willems <rwillems@amazon.com>
2023-01-13 12:12:27 +0100

    Do not fail the deep test check on in progress runs

--- .github/workflows/check-for-workflow-run.js
+++ .github/workflows/check-for-workflow-run.js
@@ -15,9 +15,10 @@ module.exports = async ({github, context, core, workflow_id, sha, ...config}) =>
   // run for this SHA we see.
   const runFilterDesc = sha ? `${workflow_id} on ${sha}` : workflow_id
   for (const run of result.data.workflow_runs) {
-    if (!sha || run.head_sha == sha) {
-      if (run.conclusion != "success") {
+    if (!sha || run.head_sha === sha) {
+      if (run.conclusion !== "success" && run.conclusion !== "in_progress") {
         core.setFailed(`Last run of ${runFilterDesc} did not succeed: ${run.html_url}`)
+        return
       } else {
         // The SHA is fully tested, exit with success
         console.log(`Last run of ${runFilterDesc} succeeded: ${run.html_url}`)
