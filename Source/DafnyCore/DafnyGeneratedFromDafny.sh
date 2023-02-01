275cba59b702a7330b85e567bfe8820a70f9915f
Mikael Mayer <mimayere@amazon.com>
2023-02-01 14:57:56 -0600

    Removed dependency on Dafny temporary

---
+++ Source/DafnyCore/DafnyGeneratedFromDafny.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+# Until we get proper dependency to previous Dafny, you have to generate the file GeneratedFromDafny.cs
+# To remove this manual build process, when it will be appropriate:
+# 1. Delete the file GeneratedFromDafny.cs
+# 2. Add a dependcy to 
+#      <PackageReference Include="dafny.msbuild" Version="1.0.0" />
+# That's it! The same file will now be automatically generated as obj/Debug/net6.0/GeneratedFromDafny.cs
+./Binaries/Dafny.exe translate --target cs --out GeneratedFromDafny.cs AST/Formatting.dfy
