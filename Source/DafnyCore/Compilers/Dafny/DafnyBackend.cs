d9efbf9b2453616a914d4e69333d04a95d7bced4
John Tristan <trjohnb@amazon.com>
2023-02-02 09:30:25 -0500

    Catching output from second compiler call

--- Source/DafnyCore/Compilers/Dafny/DafnyBackend.cs
+++ Source/DafnyCore/Compilers/Dafny/DafnyBackend.cs
@@ -1,8 +1,11 @@
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Diagnostics.Contracts;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace Microsoft.Dafny.Compilers;
@@ -47,7 +50,44 @@ public class DafnyBackend : ExecutableBackend {
     string targetFilename, ReadOnlyCollection<string> otherFileNames, object compilationResult, TextWriter outputWriter) {
     Contract.Requires(targetFilename != null || otherFileNames.Count == 0);
     var opt = DafnyOptions.O;
-    var psi = PrepareProcessStartInfo("dafny", opt.MainArgs.Prepend("/compileTarget:cs").Prepend("/compile:3").Prepend(targetFilename));
-    return 0 == RunProcess(psi, outputWriter);
+    var psi = PrepareProcessStartInfo("dafny", opt.MainArgs.Prepend("/compileTarget:cs").Prepend("/compile:3").Prepend("/compileVerbose:0").Prepend("/printVerifiedProceduresCount:0").Prepend(targetFilename));
+
+    /*
+     * When this code was written, the Dafny compiler cannot be made completely silent.
+     * This is a problem for this specific compiler and the integration tests because the second
+     * call to the compiler makes unexpected writes to the output.
+     * The following code is simply catching the output from the second compiler call (the one that executes the code)
+     * and stripping out the first two lines and the last line.
+     */
+
+    psi.RedirectStandardOutput = true;
+
+    var process = new Process();
+    process.StartInfo = psi;
+    var outputBuilder = new List<string>();
+    process.OutputDataReceived += (sender, args) => outputBuilder.Add(args.Data); // Console.WriteLine("received output: {0}", args.Data);
+    try {
+      process.Start();
+      process.BeginOutputReadLine();
+      process.WaitForExit();
+      process.CancelOutputRead();
+
+      for (int i = 2; i < outputBuilder.Count - 1; i++) {
+        Console.WriteLine(outputBuilder[i]);
+      }
+
+      if (process.ExitCode != 0) {
+        outputWriter.WriteLine("{0} Process exited with exit code {1}", "", process.ExitCode);
+        return false;
+      }
+
+    } catch (System.ComponentModel.Win32Exception e) {
+      string additionalInfo = $": {e.Message}";
+      outputWriter.WriteLine($"Error: Unable to start {psi.FileName}{additionalInfo}");
+      return false;
+    }
+
+    return true;
+
   }
 }
