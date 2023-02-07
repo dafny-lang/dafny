c50a444e25b0545a0ed1147923ec53f856358132
Remy Willems <rwillems@amazon.com>
2023-02-07 15:20:20 +0100

    Update Dafny.atg

--- Source/DafnyCore/AST/DafnyAst.cs
+++ Source/DafnyCore/AST/DafnyAst.cs
@@ -381,9 +381,6 @@ namespace Microsoft.Dafny {
       get;
     }
     void MakeGhost();
-    IToken Tok {
-      get;
-    }
   }
   [ContractClassFor(typeof(IVariable))]
   public abstract class IVariableContracts : TokenNode, IVariable {
