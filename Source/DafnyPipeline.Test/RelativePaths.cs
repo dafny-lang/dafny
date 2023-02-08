9d41df089463ecf8c32cb2a25ca32d4b50133824
Mikael Mayer <mimayere@amazon.com>
2023-02-08 14:49:26 -0600

    Fix canonical paths for paths with spaces After testing today's version of Dafny in VSCode, I realized that a path with name triggered an error when attempting to resolve includes. Hence, I added a unit test to catch this regression for next time and fixed the method that trasnforms file paths (which can be gross URI) to actual file locations

--- Source/DafnyPipeline.Test/RelativePaths.cs
+++ Source/DafnyPipeline.Test/RelativePaths.cs
@@ -10,5 +10,14 @@ namespace DafnyPipeline.Test {
     public void Test() {
       Assert.Equal(0, DafnyDriver.ThreadMain(new string[] { "/spillTargetCode:3", "warnings-as-errors.dfy" }));
     }
+
+    [Fact]
+    public void TestFileConversion() {
+      var filePath = @"file:\c:\Users\xxx\Documents\with%20space\test.dfy";
+      var filePath1 = @"file:c:\Users\xxx\Documents\with%20space\test.dfy";
+      var expected = @"c:\Users\xxx\Documents\with space\test.dfy";
+      Assert.Equal(expected, DafnyFile.Canonicalize(filePath));
+      Assert.Equal(expected, DafnyFile.Canonicalize(filePath1));
+    }
   }
 }
