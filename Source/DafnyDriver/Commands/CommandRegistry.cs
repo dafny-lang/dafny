fb4d1e6da03ef2738d128b5e2d12159b8ec54a5f
Remy Willems <rwillems@amazon.com>
2023-02-21 14:04:35 +0100

    Add comment

--- Source/DafnyDriver/Commands/CommandRegistry.cs
+++ Source/DafnyDriver/Commands/CommandRegistry.cs
@@ -171,13 +171,15 @@ static class CommandRegistry {
   }
 }
 
+/// <summary>
+/// The class HelpResult is internal to System.CommandLine so we have to include it as source.
+/// It seems System.CommandLine didn't consider having more than one help option as a use-case.
+/// </summary>
 internal class HelpResult : IInvocationResult
 {
-  /// <inheritdoc />
   public void Apply(InvocationContext context)
   {
     var output = context.Console.Out.CreateTextWriter();
-
     var helpBuilder = ((HelpBuilder)context.BindingContext.GetService(typeof(HelpBuilder)))!;
     var helpContext = new HelpContext(helpBuilder,
       context.ParseResult.CommandResult.Command,
