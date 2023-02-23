68f7be88c3d47a9b4b3afa54881dbb471f010307
Aaron Tomb <aarotomb@amazon.com>
2023-02-22 17:03:07 -0800

    Undo changes that broke resource use test

--- Source/DafnyLanguageServer.Test/Various/ResourceUsageTest.cs
+++ Source/DafnyLanguageServer.Test/Various/ResourceUsageTest.cs
@@ -24,10 +24,10 @@ method Foo()
 
     await GetLastDiagnostics(documentItem, CancellationToken);
     var processes2 = Process.GetProcessesByName(solverProcessName);
-    Assert.IsTrue(processes2.Length <= processes1.Length);
+    Assert.AreEqual(processes2.Length - 1, processes1.Length);
     ApplyChange(ref documentItem, new Range(0, 0, 0, 0), "\n");
     await GetLastDiagnostics(documentItem, CancellationToken);
     var processes3 = Process.GetProcessesByName(solverProcessName);
-    Assert.IsTrue(processes3.Length <= processes2.Length);
+    Assert.AreEqual(processes3.Length, processes2.Length);
   }
 }
