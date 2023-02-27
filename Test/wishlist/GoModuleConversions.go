96961f919da31995f261b686f4a685452d6a20d7
Robin Salkeld <salkeldr@amazon.com>
2023-02-27 15:27:37 -0800

    More whitespace

--- Test/wishlist/GoModuleConversions.go
+++ Test/wishlist/GoModuleConversions.go
@@ -6,6 +6,6 @@ import (
 )
 
 func ParseURL(dafnyAddress dafny.Sequence) (*url.URL, error) {
-	var address = dafny.SequenceVerbatimString(dafnyAddress, false);
+  var address = dafny.SequenceVerbatimString(dafnyAddress, false);
   return url.Parse(address)
 }
