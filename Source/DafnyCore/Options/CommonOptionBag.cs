b4b8a3f7c7d04942ab18d925a6620660b070db63
Aaron Tomb <aarotomb@amazon.com>
2023-02-27 11:23:36 -0800

    Revert "WIP"
    This reverts commit 9a332844ca521db44d94544facaeb1931301ae33.

--- Source/DafnyCore/Options/CommonOptionBag.cs
+++ Source/DafnyCore/Options/CommonOptionBag.cs
@@ -60,7 +60,7 @@ The `text` format also includes a more detailed breakdown of what assertions app
     ArgumentHelpName = "configuration"
   };
 
-  public static readonly Option<uint> SolverResourceLimit = new("--resource-limit", @"Specify the maximum resource limit (rlimit) value to pass to Z3. A resource limit is a deterministic alternative to a time limit. The output produced by `--log-format csv` includes the resource use of each proof effort, which you can use to determine an appropriate limit for your program. Multiplied by 1000 before sending to Z3.");
+  public static readonly Option<uint> SolverResourceLimit = new("--resource-limit", @"Specify the maximum resource limit (rlimit) value to pass to Z3. Multiplied by 1000 before sending to Z3.");
   public static readonly Option<string> SolverPlugin = new("--solver-plugin", @"Specify a plugin to use to solve verification conditions (instead of an external Z3 process).");
   public static readonly Option<string> SolverLog = new("--solver-log", @"Specify a file to use to log the SMT-Lib text sent to the solver.");
 
