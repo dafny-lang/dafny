779352536ea8508c5235c3a268bcbd82b6511e80
Aaron Tomb <aarotomb@amazon.com>
2023-03-02 10:41:10 -0800

    Fix allow_on_max.sh for Z3 updates

--- Scripts/allow_on_mac.sh
+++ Scripts/allow_on_mac.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 cd "$( dirname "${BASH_SOURCE[0]}" )" || exit 1
-xattr -d com.apple.quarantine ./*.dylib dafny DafnyServer ./*.dll z3/bin/z3
+xattr -d com.apple.quarantine ./*.dylib dafny DafnyServer ./*.dll z3/bin/z3*
