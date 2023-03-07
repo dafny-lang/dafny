f89c96b3412e8cf663eaaf2b74b6ebe88f7ba9b2
davidcok <davidcok@github.com>
2023-03-07 16:31:06 -0500

    Using new CLI in smoke tests

--- Scripts/quicktest.sh
+++ Scripts/quicktest.sh
@@ -6,17 +6,33 @@ echo "method Main() { print (42,131), '\n'; }" > c.dfy
 
 DIR="$(dirname ${BASH_SOURCE[0]})"
 echo Should succeed
-$DIR/dafny /compile:0 a.dfy
+$DIR/dafny verify a.dfy
 echo Should fail
-$DIR/dafny /compile:0 -useBaseNameForFileName b.dfy
+$DIR/dafny verify --use-basename-for-filename  b.dfy
 echo Compiling on C#
-$DIR/dafny /compile:3 /compileVerbose:0 /compileTarget:cs c.dfy
+$DIR/dafny run -t:cs c.dfy
 echo Compiling to Javascript
-$DIR/dafny /compile:3 /compileVerbose:0 /compileTarget:js c.dfy
+$DIR/dafny run -t:js c.dfy
 echo Compiling to Java
-$DIR/dafny /compile:3 /compileVerbose:0 /compileTarget:java c.dfy
+$DIR/dafny run -t:java c.dfy
 echo Compiling to Go
-$DIR/dafny /compile:3 /compileVerbose:0 /compileTarget:go c.dfy
+$DIR/dafny run -t:go c.dfy
 echo Compiling to Python
-$DIR/dafny /compile:3 /compileVerbose:0 /compileTarget:py c.dfy
-rm -rf a.dfy b.dfy c.dfy c-go c-java c-py c.jar c
+$DIR/dafny run -t:py c.dfy
+echo Building on C#
+$DIR/dafny build -t:cs c.dfy
+dotnet c.dll
+echo Building to Javascript
+$DIR/dafny build -t:js c.dfy
+node c.js
+echo Building to Java
+$DIR/dafny build -t:java c.dfy
+java -jar c.jar
+echo Building to Go
+$DIR/dafny build -t:go c.dfy
+(cd c-go; GO111MODULE=auto GOPATH=`pwd` go run src/c.go)
+echo Building to Python
+$DIR/dafny build -t:py c.dfy
+python c-py/c.py
+
+#rm -rf a.dfy b.dfy c.dfy c-go c-java c-py c.jar c
