5f9db2e5ab739eb73b00295908c58f457f80484c
Remy Willems <rwillems@amazon.com>
2023-03-10 18:08:15 +0100

    Use XUnit for all automated tests (#3722)
    ### Changes
    
    - Use XUnit for all unit tests. Previously MSTest was used by the
    language server tests and by the test generation tests
    - Use the package `<PackageReference Include="Xunit.AssertMessages"
    Version="2.4.0" />` to allow specifying messages when using XUnit
    assertions. If you want to specify an assertion message you have to use
    `AssertM`, like this:
    ```c#
    Assert.NotNull(hover);
    AssertM.NotNull(hover, $"No hover message found at {hoverPosition}");
    ```
    
    - XUnit runs tests in parallel by default, unlike MSTest. Some edits
    were made to the `DafnyTestGeneration` project to remove usage of a
    static variable that caused issues when running tests in parallel.
    - Three tests were annotated with `[Collection("Sequential
    Collection")]`, because they are either too slow to be run in parallel
    with other tests, or measure some resource usage with is affected when
    other tests are running.
    
    ### Testing
    Since this is a refactoring, no additional tests are needed
    
    <small>By submitting this pull request, I confirm that my contribution
    is made under the terms of the [MIT
    license](https://github.com/dafny-lang/dafny/blob/master/LICENSE.txt).</small>

--- Source/DafnyLanguageServer.Test/Completion/DotCompletionTest.cs
+++ Source/DafnyLanguageServer.Test/Completion/DotCompletionTest.cs
@@ -1,14 +1,13 @@
 ﻿using Microsoft.Dafny.LanguageServer.IntegrationTest.Extensions;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OmniSharp.Extensions.LanguageServer.Protocol.Document;
 using OmniSharp.Extensions.LanguageServer.Protocol.Models;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Dafny.LanguageServer.IntegrationTest.Util;
+using Xunit;
 
 namespace Microsoft.Dafny.LanguageServer.IntegrationTest.Completion {
-  [TestClass]
   public class DotCompletionTest : ClientBasedLanguageServerTest {
 
     private async Task<List<CompletionItem>> RequestCompletionAsync(TextDocumentItem documentItem, Position position) {
@@ -23,7 +22,7 @@ namespace Microsoft.Dafny.LanguageServer.IntegrationTest.Completion {
       return completionList.OrderBy(completion => completion.Label).ToList();
     }
 
-    [TestMethod]
+    [Fact]
     public async Task CompleteOnThisReturnsClassMembers() {
       var source = @"
 class A {
@@ -38,14 +37,14 @@ class A {
       ApplyChange(ref documentItem, new Range(4, 10, 4, 10), " + this.");
 
       var completionList = await RequestCompletionAsync(documentItem, (4, 18));
-      Assert.AreEqual(2, completionList.Count);
-      Assert.AreEqual(CompletionItemKind.Function, completionList[0].Kind);
-      Assert.AreEqual("GetX", completionList[0].Label);
-      Assert.AreEqual(CompletionItemKind.Field, completionList[1].Kind);
-      Assert.AreEqual("x", completionList[1].Label);
+      Assert.Equal(2, completionList.Count);
+      Assert.Equal(CompletionItemKind.Function, completionList[0].Kind);
+      Assert.Equal("GetX", completionList[0].Label);
+      Assert.Equal(CompletionItemKind.Field, completionList[1].Kind);
+      Assert.Equal("x", completionList[1].Label);
     }
 
-    [TestMethod]
+    [Fact]
     public async Task CompleteOnNothingReturnsEmptyList() {
       var source = @"
 class A {
@@ -60,10 +59,10 @@ class A {
       ApplyChange(ref documentItem, new Range((4, 4), (4, 10)), ".");
 
       var completionList = await RequestCompletionAsync(documentItem, (4, 5));
-      Assert.AreEqual(0, completionList.Count);
+      Assert.Empty(completionList);
     }
 
-    [TestMethod]
+    [Fact]
     public async Task CompleteOnNothingAtLineStartReturnsEmptyList() {
       var source = @"
 class A {
@@ -78,10 +77,10 @@ class A {
       ApplyChange(ref documentItem, new Range((4, 0), (4, 10)), ".");
 
       var completionList = await RequestCompletionAsync(documentItem, (4, 1));
-      Assert.AreEqual(0, completionList.Count);
+      Assert.Empty(completionList);
     }
 
-    [TestMethod]
+    [Fact]
     public async Task CompleteOnTypeWithoutMembersReturnsEmptyList() {
       var source = @"
 class A {
@@ -96,10 +95,10 @@ class A {
       ApplyChange(ref documentItem, new Range((4, 10), (4, 10)), ".");
 
       var completionList = await RequestCompletionAsync(documentItem, (4, 11));
-      Assert.AreEqual(0, completionList.Count);
+      Assert.Empty(completionList);
     }
 
-    [TestMethod]
+    [Fact]
     public async Task CompleteOnLocalArrayReturnsLength() {
       var source = @"
 method DoIt() {
@@ -111,12 +110,12 @@ method DoIt() {
   var y := x.");
 
       var completionList = await RequestCompletionAsync(documentItem, (2, 13));
-      Assert.AreEqual(1, completionList.Count);
-      Assert.AreEqual(CompletionItemKind.Field, completionList[0].Kind);
-      Assert.AreEqual("Length", completionList[0].Label);
+      Assert.Single(completionList);
+      Assert.Equal(CompletionItemKind.Field, completionList[0].Kind);
+      Assert.Equal("Length", completionList[0].Label);
     }
 
-    [TestMethod]
+    [Fact]
     public async Task CompleteOnShadowedVariableReturnsCompletionOfClosestSymbol() {
       var source = @"
 class A {
@@ -138,12 +137,12 @@ class B {
     var y := x.");
 
       var completionList = await RequestCompletionAsync(documentItem, (5, 15));
-      Assert.AreEqual(1, completionList.Count);
-      Assert.AreEqual(CompletionItemKind.Field, completionList[0].Kind);
-      Assert.AreEqual("b", completionList[0].Label);
+      Assert.Single(completionList);
+      Assert.Equal(CompletionItemKind.Field, completionList[0].Kind);
+      Assert.Equal("b", completionList[0].Label);
     }
 
-    [TestMethod]
+    [Fact]
     public async Task CompleteOnShadowedVariableReturnsCompletionOfClassIfPrefixedWithThis() {
       var source = @"
 class A {
@@ -165,12 +164,12 @@ class B {
     var y := this.x.");
 
       var completionList = await RequestCompletionAsync(documentItem, (5, 20));
-      Assert.AreEqual(1, completionList.Count);
-      Assert.AreEqual(CompletionItemKind.Field, completionList[0].Kind);
-      Assert.AreEqual("Length", completionList[0].Label);
+      Assert.Single(completionList);
+      Assert.Equal(CompletionItemKind.Field, completionList[0].Kind);
+      Assert.Equal("Length", completionList[0].Label);
     }
 
-    [TestMethod]
+    [Fact]
     public async Task CompleteOnMemberAccessChainRespectsCompleteChain() {
       var source = @"
 class A {
@@ -214,9 +213,9 @@ class X {
       ApplyChange(ref documentItem, new Range((8, 0), (8, 0)), "    var l := b.c.x.");
 
       var completionList = await RequestCompletionAsync(documentItem, (8, 19));
-      Assert.AreEqual(1, completionList.Count);
-      Assert.AreEqual(CompletionItemKind.Method, completionList[0].Kind);
-      Assert.AreEqual("GetConstant", completionList[0].Label);
+      Assert.Single(completionList);
+      Assert.Equal(CompletionItemKind.Method, completionList[0].Kind);
+      Assert.Equal("GetConstant", completionList[0].Label);
     }
   }
 }
