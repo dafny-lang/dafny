fe9d93ead6e9c2bd2a3a540be96ec63b5fb81708
Mikael Mayer <mimayere@amazon.com>
2023-03-16 15:14:35 -0500

    Added remaining failing tests

--- Source/DafnyPipeline.Test/DocstringTest.cs
+++ Source/DafnyPipeline.Test/DocstringTest.cs
@@ -47,10 +47,10 @@ function Test2(i: int): int
 
 // Test3 computes an int
 // It takes an int and adds 3 to it
-function Test3(i: int): int
+ghost function Test3(i: int): int
 { i + 3 }
 
-// TODO: MAke this function recursive and this is not a docstring
+// this is not a docstring but can be used as a TODO
 function Test4(i: int): int
   // Test4 computes an int
   // It takes an int and adds 4 to it
@@ -62,11 +62,10 @@ function Test5(i: int): int
 { i + 5 }
 
 function Test6(i: int): int
-  /** Test6 computes an int
-    * It takes an int and adds 6 to it */
-{ i + 6 }
+/** Test6 computes an int
+  * It takes an int and adds 6 to it */
 
-function Test7(i: int): int
+function Test7(i: int): (j: int)
   /* Test7 computes an int
   It takes an int and adds 7 to it */
 { i + 7 }
@@ -98,6 +97,202 @@ function Test11(i: int): int
           ($"Test11", $"Test11 computes an int\n It takes an int and adds 11 to it")
         }
         ).ToList());
+    }
+
+    [Fact]
+    public void DocstringWorksForPredicate() {
+      DocstringWorksFor(@"
+class X {
+  static predicate Test1(i: int)
+    // Test1 checks if an int
+    // is equal to 1
+  { i == 1 }
+  // Unrelated trailing comment
+  
+  // Test2 checks if an int
+  // is equal to 2
+  static predicate Test2(i: int)
+  { i == 2 }
+}
+", new List<(string nodeTokenValue, string expectedDocstring)>() {
+        ("Test1", "Test1 checks if an int\nis equal to 1"),
+        ("Test2", "Test2 checks if an int\nis equal to 2")});
+    }
+
+    [Fact]
+    public void DocstringWorksForMethodsAndLemmas() {
+      DocstringWorksFor(@"
+// ComputeThing prints something to the screen
+method ComputeThing(i: int) returns (j: int)
+{ print i; }
+// Unattached comment
+
+// Unattached comment
+method ComputeThing2(i: int) returns (j: int)
+  // ComputeThing2 prints something to the screen
+  requires i == 2
+{ print i; }
+
+// Unattached comment
+method ComputeThing3(i: int) returns (j: int)
+  // ComputeThing3 prints something to the screen
+
+// Unattached comment
+method ComputeThing4(i: int)
+  // ComputeThing4 prints something to the screen
+  requires i == 2
+", new List<(string nodeTokenValue, string expectedDocstring)> {
+        ("ComputeThing", "ComputeThing prints something to the screen"),
+        ("ComputeThing2", "ComputeThing2 prints something to the screen"),
+        ("ComputeThing3", "ComputeThing3 prints something to the screen"),
+        ("ComputeThing4", "ComputeThing4 prints something to the screen")
+      });
+    }
+    [Fact]
+    public void DocstringWorksForConst() {
+      DocstringWorksFor(@"
+class X {
+  const x2 := 29
+  // The biggest prime number less than 30 
+
+  // The biggest prime number less than 20 
+  const x1 := 19
+
+  // Unrelated todo 
+  const x3 := 37
+  // The biggest prime number less than 40
+}
+", new List<(string nodeTokenValue, string expectedDocstring)> {
+        ("x1", "The biggest prime number less than 20"),
+        ("x2", "The biggest prime number less than 30"),
+        ("x3", "The biggest prime number less than 40")});
+    }
+
+    [Fact]
+    public void DocstringWorksForSubsetType() {
+      DocstringWorksFor(@"
+class X {
+  type Odd := x: int | x % 2 == 1 witness 1
+  // Type of number that are not divisible by 2 
+
+  // Type of numbers divisible by 2
+  type Even := x: int | x % 2 == 1 witness 1
+
+  // Unrelated comment
+  type Weird := x: int | x % 2 == x % 3 witness 0
+  // Type of numbers whose remainder modulo 2 or 3 is the same
+}", new List<(string nodeTokenValue, string expectedDocstring)> {
+        ("Odd", "Type of number that are not divisible by 2"),
+        ("Even", "Type of numbers divisible by 2"),
+        ("Weird", "Type of numbers whose remainder modulo 2 or 3 is the same")});
+    }
+
+    [Fact]
+    public void DocstringWorksForDatatypes() {
+      DocstringWorksFor(@"
+// Unrelated comment
+datatype State =
+  // A typical log message from a process monitoring
+  | // Unrelated comment
+    Begin(time: int)
+    // The beginning of the process
+  | // Unrelated comment
+    End(time: int)
+    // The end of the process
+
+// Another typical log message from a process monitoring
+datatype State2 =
+  | // The start of the process
+    Start(time: int)
+  | // The finishing state of the process
+    Finish(time: int)
+", new List<(string nodeTokenValue, string expectedDocstring)> {
+        ("State", "A typical log message from a process monitoring"),
+        ("Begin", "The beginning of the process"),
+        ("End", "The end of the process"),
+        ("State2", "Another typical log message from a process monitoring"),
+        ("Start", "The start of the process"),
+        ("Finish", "The finishing state of the process"),
+      });
+    }
+
+
+    [Fact]
+    public void DocstringWorksForClassAndTraits() {
+      DocstringWorksFor(@"
+trait X
+// A typical base class
+{}
+
+// Unattached comment
+trait T1 extends X
+// A typical extending trait
+{}
+
+// A typical extending trait with an even number
+trait T2 extends X
+{}
+
+// Unrelated comment
+class A extends T
+  // A typical example of a class extending a trait
+{}
+
+// Another typical example of a class extending a trait
+class A2 extends T
+{}
+", new List<(string nodeTokenValue, string expectedDocstring)> {
+        ("X", "A typical base class"),
+        ("T1", "A typical extending trait"),
+        ("T2", "A typical extending trait with an even number"),
+        ("A", "A typical example of a class extending a trait"),
+        ("A2", "Another typical example of a class extending a trait")
+      });
+    }
+
+    [Fact]
+    public void DocstringWorksForModules() {
+      DocstringWorksFor(@"
+// Unattached comment
+module A
+  // A is the most interesting module
+{}
+
+// Unattached comment
+module B refines A
+  // But it can be refined
+{}
+// Unattached comment
+
+// Clearly, modules can be abstract as well
+abstract module C
+{}
+", new List<(string nodeTokenValue, string expectedDocstring)> {
+        ("A", "A is the most interesting module"),
+        ("B", "But it can be refined"),
+        ("C", "Clearly, modules can be abstract as well")
+      });
+    }
+
+    [Fact]
+    public void DocstringWorksForIterators() {
+      DocstringWorksFor(@"
+// Iter is interesting
+iterator Iter(x: int) yields (y: int)
+  requires A: 0 <= x
+{
+}
+
+// Unattached comment
+iterator Iter2(x: int) yields (y: int)
+  // Iter2 is interesting
+  requires A: 0 <= x
+{
+}
+", new List<(string nodeTokenValue, string expectedDocstring)> {
+        ("Iter", "Iter is interesting"),
+        ("Iter2", "Iter2 is interesting")
+      });
     }
 
     protected Node? FindNode(Node? node, Func<Node, bool> nodeFinder) {
