c0c070588ae16f7fa72df93ee3cb879beede2c07
Remy Willems <rwillems@amazon.com>
2023-04-06 16:09:22 +0200

    Add language server test for project files

---
+++ Source/DafnyLanguageServer.Test/ProjectFiles/ProjectFiles.cs
@@ -0,0 +1,21 @@
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Dafny.LanguageServer.IntegrationTest.Extensions;
+using Microsoft.Dafny.LanguageServer.IntegrationTest.Util;
+using Xunit;
+
+namespace Microsoft.Dafny.LanguageServer.IntegrationTest; 
+
+public class ProjectFiles : ClientBasedLanguageServerTest {
+  [Fact]
+  public async Task ProjectFileIsFound() {
+    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "ProjectFiles/TestFiles/src/foo.dfy");
+    var source = await File.ReadAllTextAsync(filePath);
+    var documentItem = CreateTestDocument(source, filePath);
+    await client.OpenDocumentAndWaitAsync(documentItem, CancellationToken);
+    var diagnostics = await GetLastDiagnostics(documentItem, CancellationToken);
+    
+    Assert.Single(diagnostics);
+    Assert.Contains("shadows", diagnostics[0].Message);
+  }
+}
