8657732a5944a4da5683a923f861072739ad22d3
Rustan Leino <leino@amazon.com>
2023-05-16 17:12:36 -0700

    Correct whitespace

--- Source/DafnyCore/Resolver/Resolver.cs
+++ Source/DafnyCore/Resolver/Resolver.cs
@@ -2871,7 +2871,7 @@ namespace Microsoft.Dafny {
                     }
                   }
                 }
-                DONE_DT: ;
+              DONE_DT:;
               }
             }
           } else if (d is TypeSynonymDecl) {
