b536ca51f1b359d8c45e28b23d307b2ae33b968c
Remy Willems <rwillems@amazon.com>
2023-05-26 10:55:52 +0200

    Moved some classes into their own files

--- Source/DafnyCore/AST/DafnyAst.cs
+++ Source/DafnyCore/AST/DafnyAst.cs
@@ -137,32 +137,6 @@ namespace Microsoft.Dafny {
     }
   }
 
-  public class Include : TokenNode, IComparable {
-    public Uri IncluderFilename { get; }
-    public Uri IncludedFilename { get; }
-    public string CanonicalPath { get; }
-    public bool ErrorReported;
-
-    public Include(IToken tok, Uri includer, Uri theFilename) {
-      this.tok = tok;
-      this.IncluderFilename = includer;
-      this.IncludedFilename = theFilename;
-      this.CanonicalPath = DafnyFile.Canonicalize(theFilename.LocalPath).LocalPath;
-      this.ErrorReported = false;
-    }
-
-    public int CompareTo(object obj) {
-      if (obj is Include include) {
-        return CanonicalPath.CompareTo(include.CanonicalPath);
-      } else {
-        throw new NotImplementedException();
-      }
-    }
-
-    public override IEnumerable<Node> Children => Enumerable.Empty<Node>();
-    public override IEnumerable<Node> PreResolveChildren => Enumerable.Empty<Node>();
-  }
-
   /// <summary>
   /// An expression introducting bound variables
   /// </summary>
---
+++ Source/DafnyLanguageServer/Workspace/Documents/DocumentAfterParsing.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Microsoft.Dafny.LanguageServer.Workspace;
+
+public class DocumentAfterParsing : Document {
+  private readonly IReadOnlyList<DafnyDiagnostic> parseDiagnostics;
+
+  public DocumentAfterParsing(DocumentTextBuffer textDocumentItem,
+    Dafny.Program program,
+    IReadOnlyList<DafnyDiagnostic> parseDiagnostics) : base(textDocumentItem) {
+    this.parseDiagnostics = parseDiagnostics;
+    Program = program;
+  }
+
+  public override IEnumerable<DafnyDiagnostic> Diagnostics => parseDiagnostics;
+
+  public Dafny.Program Program { get; }
+
+  public override IdeState ToIdeState(IdeState previousState) {
+    return previousState with {
+      TextDocumentItem = TextDocumentItem,
+      ResolutionDiagnostics = Diagnostics.Select(d => Util.ToLspDiagnostic(d)),
+      ImplementationsWereUpdated = false,
+    };
+  }
+}
