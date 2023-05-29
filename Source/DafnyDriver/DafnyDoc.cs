cc030d7d5b5cb6cd62e930944d5363a2dfc0d9b4
davidcok <davidcok@github.com>
2023-05-28 22:54:10 -0400

    Merge resolution

--- Source/DafnyDriver/DafnyDoc.cs
+++ Source/DafnyDriver/DafnyDoc.cs
@@ -672,7 +672,7 @@ class DafnyDoc {
       decl.Append(" = ");
       // datatype constructors are written out several lines down
     } else {
-      Reporter.Warning(MessageSource.Documentation, null, t.Tok, "Kind of type not handled in dafny doc");
+      Reporter.Warning(MessageSource.Documentation, (string)null, t.Tok, "Kind of type not handled in dafny doc");
     }
     decl.Append(br).Append(eol);
     details.Append(AttrString(t.Attributes));
@@ -972,7 +972,7 @@ class DafnyDoc {
         return Code(s);
       }
     }
-    Reporter.Warning(MessageSource.Documentation, null, t.Tok, "Implementation missing for type " + t.GetType() + " " + t.ToString());
+    Reporter.Warning(MessageSource.Documentation, (string)null, t.Tok, "Implementation missing for type " + t.GetType() + " " + t.ToString());
     return Code(t.ToString());
   }
 
