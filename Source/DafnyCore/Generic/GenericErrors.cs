592be431537267cef31dd63b725b27fac563b8bf
David Cok <david.r.cok@gmail.com>
2023-06-01 18:10:57 -0400

    Cleanup of errorids and information (#4109)
    <small>By submitting this pull request, I confirm that my contribution
    is made under the terms of the [MIT
    license](https://github.com/dafny-lang/dafny/blob/master/LICENSE.txt).</small>
    
    ---------
    
    Co-authored-by: davidcok <davidcok@github.com>

--- Source/DafnyCore/Generic/GenericErrors.cs
+++ Source/DafnyCore/Generic/GenericErrors.cs
@@ -2,20 +2,34 @@
 // SPDX-License-Identifier: MIT
 
 namespace Microsoft.Dafny;
+using static Microsoft.Dafny.ErrorRegistry;
 
 public class GenericErrors {
 
   public enum ErrorId {
-    g_cli_option_error,
-    g_include_has_errors, // In Reporting.cs
-
-    g_fuel_must_increase,
+    g_deprecated_this_in_constructor_modifies_clause,
     g_no_old_unicode_char,
     g_unicode_escape_must_have_six_digits,
     g_unicode_escape_is_too_large,
-    g_unicode_escape_may_not_be_surogate,
+    g_unicode_escape_may_not_be_surrogate,
     g_U_unicode_chars_are_disallowed,
-    g_deprecated_this_in_constructor_modifies_clause
+    g_cli_option_error,
+    g_feature_unsupported,
+
+  }
+
+  static GenericErrors() {
+
+    Add(ErrorId.g_deprecated_this_in_constructor_modifies_clause,
+     @"
+The purpose of a constructor is to initialize a newly allocated instance of a class.
+Hence it always modifies the `this` object.
+Previously it was required to list `this` in the modifies clause of the
+constructor to specify this property, but now `this` is always implicitly 
+a part of the modifies clause. 
+If the constructor only modifies its own object (as is the very common case)
+then no explicit modifies clause is needed at all.
+"); // TODO _ add a quick fix to remove the this, or maybe the whole clause
 
   }
 }
--- Source/DafnyDriver/DafnyDriver.cs
+++ Source/DafnyDriver/DafnyDriver.cs
@@ -442,7 +442,7 @@ namespace Microsoft.Dafny {
           if (!options.Backend.UnsupportedFeatures.Contains(e.Feature)) {
             throw new Exception($"'{e.Feature}' is not an element of the {options.Backend.TargetId} compiler's UnsupportedFeatures set");
           }
-          dafnyProgram.Reporter.Error(MessageSource.Compiler, e.Token, e.Message);
+          dafnyProgram.Reporter.Error(MessageSource.Compiler, Compilers.CompilerErrors.ErrorId.f_unsupported_feature, e.Token, e.Message);
           compiled = false;
         }
 
