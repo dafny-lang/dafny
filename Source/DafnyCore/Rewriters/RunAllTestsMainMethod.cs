12eedcd493ec5783df77f925604d7ce83050e7d2
Remy Willems <rwillems@amazon.com>
2023-06-06 12:32:31 +0200

    Run RunAllTestsMainMethod after resolution, don't verify try/recover statements, and fix expect file

--- Source/DafnyCore/Rewriters/RunAllTestsMainMethod.cs
+++ Source/DafnyCore/Rewriters/RunAllTestsMainMethod.cs
@@ -84,7 +84,7 @@ public class RunAllTestsMainMethod : IRewriter {
   ///   print "Test failures occurred: see above.\n";
   /// }
   /// </summary>
-  public override void PostVerification(Program program) {
+  internal override void PostResolve(Program program) {
     var tok = program.GetFirstTopLevelToken();
     List<Statement> mainMethodStatements = new();
     var idGenerator = new FreshIdGenerator();
