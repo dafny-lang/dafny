95fde0852ef3c58d86b466e373f1237bebea2102
Remy Willems <rwillems@amazon.com>
2023-06-12 17:09:54 +0200

    IndirectlyIncludedFileFailsSemantic now passes

--- Source/DafnyLanguageServer/Workspace/Documents/DocumentAfterParsing.cs
+++ Source/DafnyLanguageServer/Workspace/Documents/DocumentAfterParsing.cs
@@ -37,13 +37,22 @@ public class DocumentAfterParsing : Document {
   }
 
   private IEnumerable<DafnyDiagnostic> GetIncludeErrorDiagnostics() {
-    foreach (var include in Program.Includes.Select(i => i.IncludedFilename).Distinct()) {
+
+    var graph = new Graph<Uri>();
+    foreach (var edgesForUri in Program.Includes.GroupBy(i => i.IncluderFilename)) {
+      foreach (var edge in edgesForUri) {
+        graph.AddEdge(edge.IncluderFilename, edge.IncludedFilename);
+      }
+    }
+
+    foreach (var include in graph.TopologicallySortedComponents()) {
       var messageForIncludedFile =
         ResolutionDiagnostics.GetOrDefault(include, Enumerable.Empty<DafnyDiagnostic>);
       if (messageForIncludedFile.Any(m => m.Level == ErrorLevel.Error)) {
         var diagnostic = new DafnyDiagnostic(null, Program.GetFirstTopLevelToken(), "the included file " + include.LocalPath + " contains error(s)",
           MessageSource.Parser, ErrorLevel.Error, new DafnyRelatedInformation[] { });
         yield return diagnostic;
+        break;
       }
     }
   }
