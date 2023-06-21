1ad70924d98732fd2c179303130af4f0fdee513a
Remy Willems <rwillems@amazon.com>
2023-06-21 19:13:28 +0200

    Refactoring

--- Source/DafnyCore/AST/Grammar/ProgramParser.cs
+++ Source/DafnyCore/AST/Grammar/ProgramParser.cs
@@ -12,17 +12,6 @@ using static Microsoft.Dafny.ParseErrors;
 
 namespace Microsoft.Dafny;
 
-public interface IFileSystem {
-  TextReader ReadFile(Uri uri);
-}
-
-public class OnDiskFileSystem : IFileSystem {
-  public static readonly IFileSystem Instance = new OnDiskFileSystem();
-  public TextReader ReadFile(Uri uri) {
-    return new StreamReader(File.Open(uri.LocalPath, FileMode.Open));
-  }
-}
-
 public record DfyParseResult(
   BatchErrorReporter ErrorReporter,
   FileModuleDefinition Module,
