42acd36a8e97ce9632cc0b57f488683cb93c7d10
Robin Salkeld <salkeldr@amazon.com>
2023-06-28 15:49:50 -0700

    Working .NET benchmark project
    Reproduces bug if I insert ANY sleep at least

---
+++ Test/benchmarks/net/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+namespace MyBenchmarks
+{
+    public class SequenceRace
+    {
+        private readonly Dafny.ISequence<int> s;
+        
+
+        public SequenceRace() {
+          s = Dafny.Sequence<int>.FromArray(Array.Empty<int>());
+          for (var i = 0; i <= 1000; i++) {
+            s = Dafny.Sequence<int>.Concat(s, Dafny.Sequence<int>.FromArray(new int[]{ i }));
+          }
+        }
+
+        [Benchmark]
+        public void LazyRaceParallel() {
+          Parallel.For(0, 10, _ => {
+            LazyRace();
+          });
+        }
+        
+        private void LazyRace() {
+          s.Select(0);
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<SequenceRace>(
+              DefaultConfig.Instance.AddJob(
+                Job.Default.WithAffinity((IntPtr)(2 ^ Environment.ProcessorCount - 1))
+                  .WithIterationTime()));
+        }
+    }
+}
