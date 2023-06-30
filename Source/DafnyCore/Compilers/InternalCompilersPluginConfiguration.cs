f915c2de1bc7c4b9982c019f9957535812af5ff2
Shadaj Laddad <shadaj@users.noreply.github.com>
2023-06-30 16:24:27 -0400

    Update DafnyRef

--- Source/DafnyCore/Compilers/InternalCompilersPluginConfiguration.cs
+++ Source/DafnyCore/Compilers/InternalCompilersPluginConfiguration.cs
@@ -14,7 +14,6 @@ internal class InternalCompilersPluginConfiguration : Plugins.PluginConfiguratio
       new PythonBackend(options),
       new CppCompilerBackend(options),
       new LibraryBackend(options),
-      new DafnyBackend(options),
       new RustBackend(options),
     };
   }
