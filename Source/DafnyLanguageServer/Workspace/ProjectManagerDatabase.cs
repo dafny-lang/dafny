abec36973e2a904c05034a17da27debda400d050
Remy Willems <rwillems@amazon.com>
2023-07-04 15:16:29 +0200

    Add comments

--- Source/DafnyLanguageServer/Workspace/ProjectManagerDatabase.cs
+++ Source/DafnyLanguageServer/Workspace/ProjectManagerDatabase.cs
@@ -153,8 +153,19 @@ namespace Microsoft.Dafny.LanguageServer.Workspace {
       if (projectManagerForFile != null) {
         if (!projectManagerForFile.Project.Equals(project)) {
           if (projectManagerForFile.Project.Uri == project.Uri) {
+            /* It's nice and simple if we only need to think about each file that the IDE interacts with.
+             
+               If a project changes, how do we know for which files to reset diagnostics?
+               If we have a global IdeState, then we don't need to publish reset diagnostics, 
+               
+               When a file is closed, how do you know whether to reset diagnostics?
+               Currently each ProjectManager has its own IdeState LastPublishedState
+               Which assumes that each project has a separate IdeState
+               However, different projects may relate to the same IdeState
+             */
             // TODO hack until we have a global IdeState.
             await projectManagerForFile.CloseAsync();
+            // await projectManagerForFile.CloseDocument();
           } else {
             await projectManagerForFile.CloseDocument();
           }
