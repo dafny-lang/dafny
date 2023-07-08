21987c87bdc108fe99f5c8b216b8be7c77a6f0c7
Robin Salkeld <salkeldr@amazon.com>
2023-07-08 14:12:42 -0700

    Whitespace

--- Source/DafnyCore/Compilers/Java/Compiler-java.cs
+++ Source/DafnyCore/Compilers/Java/Compiler-java.cs
@@ -338,7 +338,7 @@ namespace Microsoft.Dafny.Compilers {
       var addCompileSuffix = Options.Get(CommonOptionBag.AddCompileSuffix);
       var defaultModuleCompileName = addCompileSuffix ? "_module_Compile" : "_module";
       var enclosingModuleCompileNAme = mainMethod.EnclosingClass.EnclosingModuleDefinition.GetCompileName(Options);
-      var modName = enclosingModuleCompileNAme == defaultModuleCompileName 
+      var modName = enclosingModuleCompileNAme == defaultModuleCompileName
         ? (addCompileSuffix ? "_System_Compile." : "_System.")
         : "";
       companion = modName + companion;
