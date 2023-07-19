29de74cd59b1fee04923010438196a790173c732
Remy Willems <rwillems@amazon.com>
2023-07-19 12:03:41 +0200

    Properly await document open so that opening a document and then waiting for hover on it always works

--- Source/DafnyLanguageServer/Workspace/LanguageServerExtensions.cs
+++ Source/DafnyLanguageServer/Workspace/LanguageServerExtensions.cs
@@ -57,7 +57,7 @@ namespace Microsoft.Dafny.LanguageServer.Workspace {
         services.GetRequiredService<ISymbolTableFactory>(),
         services.GetRequiredService<IGhostStateDiagnosticCollector>(),
         services.GetRequiredService<ICompilationStatusNotificationPublisher>(),
-        services.GetRequiredService<ILoggerFactory>()
+        services.GetRequiredService<ILogger<ITextDocumentLoader>>()
       );
     }
   }
