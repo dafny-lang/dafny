c2cf9b111cda7a9f8c135c1d4d4786f0553b8f9a
Aaron Tomb <aarotomb@amazon.com>
2023-07-20 15:17:15 -0700

    Leave Rust testing out of quicktest.sh until a release supports it
    The current setup breaks Homebrew testing that we run in every PR

--- Scripts/quicktest.sh
+++ Scripts/quicktest.sh
@@ -41,8 +41,8 @@ echo Running with Go
 $DAFNY run -t:go c.dfy | diff - $tmp || exit 1
 echo Running with Python
 $DAFNY run -t:py c.dfy | diff - $tmp || exit 1
-echo Running with Rust
-$DAFNY run -t:rs c.dfy | diff - $tmp || exit 1
+#echo Running with Rust
+#$DAFNY run -t:rs c.dfy | diff - $tmp || exit 1
 
 rm -rf c-go c-java c-py c.jar c c.dll c.exe c.js c.runtimeconfig.json
 echo "" > $tmp
@@ -65,8 +65,8 @@ $DAFNY build -t:go c.dfy | diff - $tmp || exit 1
 echo Building with Python
 $DAFNY build -t:py c.dfy | diff - $tmp || exit 1
 python c-py/c.py | diff - $tmpx || exit 1
-echo Building with Rust
-$DAFNY build -t:rs c.dfy | diff - $tmp || exit 1
+#echo Building with Rust
+#$DAFNY build -t:rs c.dfy | diff - $tmp || exit 1
 ./c.exe | diff - $tmpx || exit 1
 
 echo Quicktest script succeeded
