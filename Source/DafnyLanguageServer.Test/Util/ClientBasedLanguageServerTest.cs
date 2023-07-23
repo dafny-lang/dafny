a61994bb82b5941e4f0b7c72fbe4fed90d73936c
Remy Willems <rwillems@amazon.com>
2023-07-23 22:46:51 +0200

    Fix comp error

--- Source/DafnyLanguageServer.Test/Util/ClientBasedLanguageServerTest.cs
+++ Source/DafnyLanguageServer.Test/Util/ClientBasedLanguageServerTest.cs
@@ -100,7 +100,7 @@ public class ClientBasedLanguageServerTest : DafnyLanguageServerTestBase, IAsync
           return namedVerifiableStatus;
         }
       } catch (OperationCanceledException) {
-        await WriteVerificationHistory();
+        WriteVerificationHistory();
       }
     }
   }
