6eb6e85f5c6fba2feec8d6e68eb197a4b307a8e0
Remy Willems <rwillems@amazon.com>
2023-08-01 17:49:30 +0200

    Bring back comment

--- Source/DafnyCore/AST/IHasUsages.cs
+++ Source/DafnyCore/AST/IHasUsages.cs
@@ -44,6 +44,11 @@ public interface IHasSymbolChildren : ISymbol {
   IEnumerable<ISymbol> ChildSymbols { get; }
 }
 
+/// <summary>
+/// This is a copy of OmniSharp.Extensions.LanguageServer.Protocol.Models.SymbolKind
+/// In the future, we'll include that package in this project, and then this copy can be removed.
+/// For now, adding that package reference is not worth its weight.
+/// </summary>
 public enum DafnySymbolKind {
   File = 1,
   Module = 2,
