848b5a964e0d9e5a1a9d3ce6d5ad33bfe70f9346
Remy Willems <rwillems@amazon.com>
2023-08-09 14:43:41 +0200

    Add workaround for faulty dafny format errors

--- Source/DafnyCore.Test/NodeTests.cs
+++ Source/DafnyCore.Test/NodeTests.cs
@@ -25,8 +25,8 @@ public class NodeTests {
   public void FindNodeWithTokenLessIntermediate() {
     var uri = new Uri(Directory.GetCurrentDirectory());
     var child = new ConcreteNode(CreateRange(uri, 0, 1, 0, 2));
-    var parent = new ConcreteNode(RangeToken.NoToken, new INode[] { child });
-    var grandParent = new ConcreteNode(CreateRange(uri, 0, 0, 0, 3), new INode[] { parent });
+    var parent = new ConcreteNode(RangeToken.NoToken, (IEnumerable<INode>) new INode[] { child });
+    var grandParent = new ConcreteNode(CreateRange(uri, 0, 0, 0, 3), (IEnumerable<INode>) new INode[] { parent });
 
     var shouldBeChild = grandParent.FindNode(uri, new DafnyPosition(0, 1));
     Assert.Equal(child, shouldBeChild);
@@ -38,7 +38,7 @@ public class NodeTests {
     var child1 = new ConcreteNode(RangeToken.NoToken);
     var child2 = new ConcreteNode(CreateRange(uri, 0, 1, 0, 2));
 
-    var parent = new ConcreteNode(CreateRange(uri, 0, 0, 0, 3), new INode[] { child1, child2 });
+    var parent = new ConcreteNode(CreateRange(uri, 0, 0, 0, 3), (IEnumerable<INode>) new [] { child1, child2 });
 
     var shouldBeChild = parent.FindNode(uri, new DafnyPosition(0, 1));
     Assert.Equal(child2, shouldBeChild);
