5c6deff1688b01a8b2518f784062c9a37e1e01fd
Aaron Tomb <aarotomb@amazon.com>
2023-08-30 09:36:14 -0700

    Clean up ProofDependency

--- Source/DafnyCore/Verifier/ProofDependency.cs
+++ Source/DafnyCore/Verifier/ProofDependency.cs
@@ -1,45 +1,59 @@
-using JetBrains.Annotations;
+// Copyright (C) Microsoft Corporation.  All Rights Reserved.
+// Copyright by the contributors to the Dafny Project
+// SPDX-License-Identifier: MIT
+//
+//-----------------------------------------------------------------------------
 using Microsoft.Dafny;
 using IToken = Microsoft.Dafny.IToken;
 using PODesc = Microsoft.Dafny.ProofObligationDescription;
-using ResolutionContext = Microsoft.Boogie.ResolutionContext;
 
 namespace DafnyCore.Verifier;
 
+// A proof dependency represents a particular part of a Dafny program
+// that may be used in the process of proving its correctness. When
+// Boogie proves a particular verification condition, it returns a
+// list of strings, returned by the SMT solver, indicating which
+// program elements were used in completing the proof. After this, the
+// ProofDependencyManager can map each string to a more structured
+// ProofDependency.
 public abstract class ProofDependency {
   public abstract string Description { get; }
 
-  public abstract RangeToken RangeToken { get; }
+  public abstract RangeToken Range { get; }
 
   public string LocationString() {
-    if (RangeToken?.StartToken is null) {
+    if (Range?.StartToken is null) {
       return "<no location>";
     }
-    var fn = RangeToken.StartToken.filename;
-    var sl = RangeToken.StartToken.line;
-    var sc = RangeToken.StartToken.col - 1;
+    var fn = Range.StartToken.filename;
+    var sl = Range.StartToken.line;
+    var sc = Range.StartToken.col - 1;
     return $"{fn}({sl},{sc})";
   }
 
   public string RangeString() {
-    if (RangeToken?.StartToken is null) {
+    if (Range?.StartToken is null) {
       return "<no range>";
     }
-    var fn = RangeToken.StartToken.filename;
-    var sl = RangeToken.StartToken.line;
-    var sc = RangeToken.StartToken.col - 1;
-    var el = RangeToken.EndToken.line;
-    var ec = RangeToken.EndToken.col - 1;
+    var fn = Range.StartToken.filename;
+    var sl = Range.StartToken.line;
+    var sc = Range.StartToken.col - 1;
+    var el = Range.EndToken.line;
+    var ec = Range.EndToken.col - 1;
     return $"{fn}({sl},{sc})-({el},{ec})";
   }
 
   public string OriginalString() {
-    return RangeToken?.PrintOriginal();
+    return Range?.PrintOriginal();
   }
 }
 
+// Represents the portion of a Dafny program corresponding to a proof
+// obligation. This is particularly important to track because if a particular
+// assertion batch can be proved without proving one of the assertions that is
+// a proof obligation within it, that assertion must have been proved vacuously.
 public class ProofObligationDependency : ProofDependency {
-  public override RangeToken RangeToken { get; }
+  public override RangeToken Range { get; }
 
   public PODesc.ProofObligationDescription ProofObligation { get; }
 
@@ -47,15 +61,17 @@ public class ProofObligationDependency : ProofDependency {
       $"{ProofObligation.SuccessDescription}";
 
   public ProofObligationDependency(IToken tok, PODesc.ProofObligationDescription proofObligation) {
-    RangeToken = tok as RangeToken ?? new RangeToken(tok, tok);
+    Range = tok as RangeToken ?? new RangeToken(tok, tok);
     ProofObligation = proofObligation;
   }
 }
 
+// Represents the assumption of a requires clause in the process of
+// proving a Dafny definition.
 public class RequiresDependency : ProofDependency {
   private Expression requires;
 
-  public override RangeToken RangeToken =>
+  public override RangeToken Range =>
     requires.RangeToken;
 
   public override string Description =>
@@ -66,10 +82,11 @@ public class RequiresDependency : ProofDependency {
   }
 }
 
+// Represents the goal of proving an ensures clause of a Dafny definition.
 public class EnsuresDependency : ProofDependency {
   private Expression ensures;
 
-  public override RangeToken RangeToken =>
+  public override RangeToken Range =>
     ensures.RangeToken;
 
   public override string Description =>
@@ -80,12 +97,14 @@ public class EnsuresDependency : ProofDependency {
   }
 }
 
+// Represents the goal of proving a specific requires clause of a specific
+// call.
 public class CallRequiresDependency : ProofDependency {
   private CallDependency call;
   private RequiresDependency requires;
 
-  public override RangeToken RangeToken =>
-    call.RangeToken;
+  public override RangeToken Range =>
+    call.Range;
 
   public override string Description =>
     $"requires clause at {requires.RangeString()} from call";
@@ -96,12 +115,14 @@ public class CallRequiresDependency : ProofDependency {
   }
 }
 
+// Represents the assumption of a specific ensures clause of a specific
+// call.
 public class CallEnsuresDependency : ProofDependency {
   private CallDependency call;
   private EnsuresDependency ensures;
 
-  public override RangeToken RangeToken =>
-    call.RangeToken;
+  public override RangeToken Range =>
+    call.Range;
 
   public override string Description =>
     $"ensures clause at {ensures.RangeString()} from call";
@@ -112,10 +133,11 @@ public class CallEnsuresDependency : ProofDependency {
   }
 }
 
+// Represents the fact that a particular call occurred.
 public class CallDependency : ProofDependency {
   private CallStmt call;
 
-  public override RangeToken RangeToken =>
+  public override RangeToken Range =>
     call.RangeToken;
 
   public override string Description =>
@@ -126,10 +148,11 @@ public class CallDependency : ProofDependency {
   }
 }
 
+// Represents the assumption of a predicate in an `assume` statement.
 public class AssumptionDependency : ProofDependency {
   private Expression expr;
 
-  public override RangeToken RangeToken =>
+  public override RangeToken Range =>
     expr.RangeToken;
 
   public override string Description =>
@@ -143,10 +166,11 @@ public class AssumptionDependency : ProofDependency {
   }
 }
 
+// Represents the invariant of a loop.
 public class InvariantDependency : ProofDependency {
   private Expression invariant;
 
-  public override RangeToken RangeToken =>
+  public override RangeToken Range =>
     invariant.RangeToken;
 
   public override string Description =>
@@ -157,19 +181,22 @@ public class InvariantDependency : ProofDependency {
   }
 }
 
+// Represents an assignment statement. This includes the assignment to an
+// out parameter implicit in a return statement.
 public class AssignmentDependency : ProofDependency {
-  public override RangeToken RangeToken { get; }
+  public override RangeToken Range { get; }
 
   public override string Description =>
      "assignment (or return)";
 
   public AssignmentDependency(RangeToken rangeToken) {
-    this.RangeToken = rangeToken;
+    this.Range = rangeToken;
   }
 }
 
+// Represents dependency of a proof on the definition of a specific function.
 public class FunctionDefinitionDependency : ProofDependency {
-  public override RangeToken RangeToken => function.RangeToken;
+  public override RangeToken Range => function.RangeToken;
 
   public override string Description =>
     $"function definition for {function.Name}";
