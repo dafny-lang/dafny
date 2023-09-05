930de2b6913e6dedd1e2c06d3e0f0a0ad2889882
Remy Willems <rwillems@amazon.com>
2023-09-05 18:43:59 +0200

    Include both locations in duplicate module name error messages (#4499)
    Fixes #4289
    
    <small>By submitting this pull request, I confirm that my contribution
    is made under the terms of the [MIT
    license](https://github.com/dafny-lang/dafny/blob/master/LICENSE.txt).</small>

--- Source/DafnyCore/AST/Modules/ModuleBindings.cs
+++ Source/DafnyCore/AST/Modules/ModuleBindings.cs
@@ -25,14 +25,14 @@ public class ModuleBindings {
     }
   }
 
-  public bool TryLookup(IToken name, out ModuleDecl m) {
+  public bool TryLookup(string name, out ModuleDecl m) {
     Contract.Requires(name != null);
     return TryLookupFilter(name, out m, l => true);
   }
 
-  public bool TryLookupFilter(IToken name, out ModuleDecl m, Func<ModuleDecl, bool> filter) {
+  public bool TryLookupFilter(string name, out ModuleDecl m, Func<ModuleDecl, bool> filter) {
     Contract.Requires(name != null);
-    if (modules.TryGetValue(name.val, out m) && filter(m)) {
+    if (modules.TryGetValue(name, out m) && filter(m)) {
       return true;
     } else if (parent != null) {
       return parent.TryLookupFilter(name, out m, filter);
@@ -56,7 +56,7 @@ public class ModuleBindings {
     Contract.Assert(qid != null);
     IToken root = qid.Path[0].StartToken;
     result = null;
-    bool res = TryLookupFilter(root, out result,
+    bool res = TryLookupFilter(root.val, out result,
       m => context != m && ((context.EnclosingModuleDefinition == m.EnclosingModuleDefinition && context.Exports.Count == 0) || m is LiteralModuleDecl));
     return res;
   }
@@ -72,7 +72,7 @@ public class ModuleBindings {
     Contract.Assert(qid != null);
     IToken root = qid.Path[0].StartToken;
     result = null;
-    bool res = TryLookupFilter(root, out result,
+    bool res = TryLookupFilter(root.val, out result,
       m => context != m && ((context.EnclosingModuleDefinition == m.EnclosingModuleDefinition && context.Exports.Count == 0) || m is LiteralModuleDecl));
     return res;
   }
@@ -82,7 +82,7 @@ public class ModuleBindings {
     Contract.Assert(qid != null);
     IToken root = qid.Path[0].StartToken;
     result = null;
-    bool res = TryLookupFilter(root, out result, m => m.EnclosingModuleDefinition != context);
+    bool res = TryLookupFilter(root.val, out result, m => m.EnclosingModuleDefinition != context);
     return res;
   }
 }
