e04fcfe1b517ada9e04a6e52aefcba033e877005
Shadaj Laddad <shadaj@users.noreply.github.com>
2023-09-06 15:41:42 -0700

    Fix Rust backend crashes due to match cases and static consts (#4458)
    Fix Rust backend crashes due to match cases and static consts
    
    <small>By submitting this pull request, I confirm that my contribution
    is made under the terms of the [MIT
    
    license](https://github.com/dafny-lang/dafny/blob/master/LICENSE.txt).</small>
    
    ---
    Stack created with [Sapling](https://sapling-scm.com). Best reviewed
    with [ReviewStack](https://reviewstack.dev/dafny-lang/dafny/pull/4458).
    * #4518
    * #4468
    * #4459
    * __->__ #4458

--- Source/DafnyCore/Compilers/Dafny/Compiler-dafny.cs
+++ Source/DafnyCore/Compilers/Dafny/Compiler-dafny.cs
@@ -1058,7 +1058,13 @@ namespace Microsoft.Dafny.Compilers {
             ));
             break;
           case StaticReceiverExpr:
-            throw new NotImplementedException();
+            if (e.Type.NormalizeExpandKeepConstraints() is UserDefinedType udt && udt.ResolvedClass is DatatypeDecl dt &&
+                DatatypeWrapperEraser.IsErasableDatatypeWrapper(Options, dt, out _)) {
+              baseExpr = (DAST.Expression)DAST.Expression.create_Companion(PathFromTopLevel(udt.ResolvedClass));
+            } else {
+              baseExpr = (DAST.Expression)DAST.Expression.create_Companion(PathFromTopLevel(e.Type.AsTopLevelTypeWithMembers));
+            }
+            break;
           default:
             DAST.Type baseType;
             if (e.Type.AsNewtype != null) {
@@ -1535,6 +1541,20 @@ namespace Microsoft.Dafny.Compilers {
         var retType = GenType(resultType);
 
         return new BuilderSyntaxTree<StatementContainer>(builder.Builder.Lambda(formals, retType));
+      } else {
+        throw new InvalidOperationException();
+      }
+    }
+
+    protected override void EmitLambdaApply(ConcreteSyntaxTree wr, out ConcreteSyntaxTree wLambda, out ConcreteSyntaxTree wArg) {
+      if (wr is BuilderSyntaxTree<StatementContainer> builder) {
+        var lambda = builder.Builder.Call();
+        wLambda = new BuilderSyntaxTree<ExprContainer>(lambda);
+        wArg = new BuilderSyntaxTree<ExprContainer>(lambda);
+      } else if (wr is BuilderSyntaxTree<ExprContainer> exprBuilder) {
+        var lambda = exprBuilder.Builder.Call();
+        wLambda = new BuilderSyntaxTree<ExprContainer>(lambda);
+        wArg = new BuilderSyntaxTree<ExprContainer>(lambda);
       } else {
         throw new InvalidOperationException();
       }
