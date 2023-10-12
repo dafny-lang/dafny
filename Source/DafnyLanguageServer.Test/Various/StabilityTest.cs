8b2950b188c05fd0a702323969dbf75a1bddbd9c
John Tristan <trjohnb@amazon.com>
2023-10-12 09:28:30 -0400

    Dafny generator using project file

--- Source/DafnyLanguageServer.Test/Various/StabilityTest.cs
+++ Source/DafnyLanguageServer.Test/Various/StabilityTest.cs
@@ -34,31 +34,11 @@ namespace Microsoft.Dafny.LanguageServer.IntegrationTest.Various {
 
     [Fact(Timeout = MaxTestExecutionTimeMs)]
     public async Task GhcMergeSort() {
-      var documentItem = await CreateTextDocumentFromFileAsync("GHC-MergeSort.dfy");
+      var documentItem = await CreateTextDocumentFromFileAsync("UnsoundRefinement.dfy");
       await client.OpenDocumentAndWaitAsync(documentItem, CancellationToken);
       Assert.NotNull(await Projects.GetResolvedDocumentAsyncNormalizeUri(documentItem.Uri));
     }
-
-    [Fact(Timeout = MaxTestExecutionTimeMs)]
-    public async Task GenericSort() {
-      var documentItem = await CreateTextDocumentFromFileAsync("GenericSort.dfy");
-      await client.OpenDocumentAndWaitAsync(documentItem, CancellationToken);
-      Assert.NotNull(await Projects.GetResolvedDocumentAsyncNormalizeUri(documentItem.Uri));
-    }
-
-    [Fact(Timeout = MaxTestExecutionTimeMs)]
-    public async Task StrongNestingDoesNotCauseStackOverlfow() {
-      // Without a sufficiently large stack, the following code causes a stack overflow:
-      // https://github.com/dafny-lang/dafny/issues/1447
-      const string source = @"
-method NestedExpression() {
-  assert var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; var three := 3; true;
-}";
-      var documentItem = CreateTestDocument(source, "StrongNestingDoesNotCauseStackOverlfow.dfy");
-      await client.OpenDocumentAndWaitAsync(documentItem, CancellationToken);
-      Assert.NotNull(await Projects.GetResolvedDocumentAsyncNormalizeUri(documentItem.Uri));
-    }
-
+    
     public StabilityTest(ITestOutputHelper output) : base(output) {
     }
   }
