a2730358cc66479d1e9936e08fc83dc1030fa7bd
Aaron Tomb <aarotomb@amazon.com>
2023-10-16 15:52:34 -0700

    Allow filtering of combined coverage reports
    Add a new `--only-label` option to `merge-coverage-reports` to include
    only one category of highlighting in the output. For example, merging
    coverage reports from test generation and verification using the option
    `--only-label NotCovered` will highlight only the regions not covered by
    either testing or verification.

--- Source/DafnyDriver/Commands/CoverageReportCommand.cs
+++ Source/DafnyDriver/Commands/CoverageReportCommand.cs
@@ -7,20 +7,26 @@ using System.CommandLine;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using DafnyCore;
 
 namespace Microsoft.Dafny; 
 
 static class CoverageReportCommand {
 
+  public static readonly Option<CoverageLabel?> OnlyLabelOption = new("--only-label",
+    "Skip coverage labels other than the given one, after merging labels from the input reports.");
+
   public static IEnumerable<Option> Options =>
     new Option[] {
       CommonOptionBag.NoTimeStampForCoverageReport,
+      OnlyLabelOption
     };
 
   static CoverageReportCommand() {
     ReportsArgument = new("reports", r => {
       return r.Tokens.Where(t => !string.IsNullOrEmpty(t.Value)).Select(t => new FileInfo(t.Value)).ToList();
     }, true, "directories with coverage reports to be merged");
+    DooFile.RegisterNoChecksNeeded(OnlyLabelOption);
   }
 
   // FilesArgument is intended for Dafny files, so CoverageReportCommand adds its own argument instead
