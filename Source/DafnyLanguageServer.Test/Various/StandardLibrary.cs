61f1345956ab06f951a6c71afd661b2f3b01041e
Remy Willems <rwillems@amazon.com>
2023-11-10 23:20:49 +0100

    Fixes

--- Source/DafnyLanguageServer.Test/Various/StandardLibrary.cs
+++ Source/DafnyLanguageServer.Test/Various/StandardLibrary.cs
@@ -2,6 +2,7 @@ using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Dafny.LanguageServer.IntegrationTest.Util;
 using Microsoft.Extensions.Logging;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -11,7 +12,7 @@ public class StandardLibrary : ClientBasedLanguageServerTest {
   [Fact]
   public async Task CanUseWrappers() {
     var source = @"
-import opened DafnyStdLibs.Wrappers;
+import opened DafnyStdLibs.Wrappers
 
 method Foo() returns (s: Option<int>) { 
   return Some(3); 
@@ -20,9 +21,14 @@ method Foo() returns (s: Option<int>) {
     var projectSource = @"
 [options]
 standard-libraries = true";
+
+    var withoutStandardLibraries = CreateAndOpenTestDocument(source);
+    var diagnostics1 = await GetLastDiagnostics(withoutStandardLibraries);
+    Assert.Single(diagnostics1);
+
     var directory = Path.GetTempFileName();
     CreateAndOpenTestDocument(projectSource, Path.Combine(directory, DafnyProject.FileName));
-    CreateAndOpenTestDocument(source, Path.Combine(directory, "document"));
+    CreateAndOpenTestDocument(source, Path.Combine(directory, "document.dfy"));
     await AssertNoDiagnosticsAreComing(CancellationToken);
   }
 
