6687241324aefe4b7713f2fd7c37dd6717b61f56
Alex Porter <bomber8013@gmail.com>
2023-12-30 17:37:27 -0800

    Fixed bad command for batch programs

--- Source/DafnyRuntime/DafnyRuntimeJava/gradlew.bat
+++ Source/DafnyRuntime/DafnyRuntimeJava/gradlew.bat
@@ -74,7 +74,7 @@ set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 for %%i in (1 2 3 4 5) do (
   "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
   if errorlevel 1 (
-    sleep 15
+    timeout 15 > NUL
   ) else (
     goto end
   )
