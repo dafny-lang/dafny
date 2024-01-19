00fa2a5624cab45dd9668133285fb4d9c40afa85
Aleksandr Fedchin <fedchina@amazon.com>
2024-01-18 17:10:13 -0800

    Fix test-generation translation pass and Update tests

--- Source/DafnyCore/Verifier/CaptureStateExtensions.cs
+++ Source/DafnyCore/Verifier/CaptureStateExtensions.cs
@@ -6,7 +6,7 @@ namespace Microsoft.Dafny {
   static class CaptureStateExtensions {
 
     public static void AddCaptureState(this BoogieStmtListBuilder builder, Statement statement) {
-      if (builder.Options.ExpectingModel) {
+      if (builder.Options.ExpectingModel || builder.Options.TestGenOptions.Mode != TestGenerationOptions.Modes.None) {
         builder.Add(CaptureState(builder.Options, statement));
       }
     }
@@ -18,7 +18,7 @@ namespace Microsoft.Dafny {
     }
 
     public static void AddCaptureState(this BoogieStmtListBuilder builder, IToken tok, bool isEndToken, string /*?*/ additionalInfo) {
-      if (builder.Options.ExpectingModel) {
+      if (builder.Options.ExpectingModel || builder.Options.TestGenOptions.Mode != TestGenerationOptions.Modes.None) {
         builder.Add(CaptureState(builder.Options, tok, isEndToken, additionalInfo));
       }
     }
