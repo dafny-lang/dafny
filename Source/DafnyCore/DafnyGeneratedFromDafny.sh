2c77435847b505e4c1f49deac638d8a9adb33670
Mikaël Mayer <MikaelMayer@users.noreply.github.com>
2024-02-06 18:38:11 -0600

    Feat: Dafny-to-Rust code indentation and identifiers (#4974)
    # Improvements of the DAST to Rust code generator:
    
    * The code generator emits a Rust-like AST that is then formatted with
    proper indentation
    * Ensured Rust-like identifiers in Dafny stay the same in Rust (like
    "one_variable")
    * Added Initial implementation of sequences and maps in the
    DafnyRuntimeRust, following the implementation of the Go
    * Added module to perform conversions from and to Dafny types to and
    from native types
    
    # Code enhancement
    
    * Added support for using the standard library's Option type rather than
    the hard-coded Optional type.
    
    # Tests
    
    * Added proof that the identifier mapping is fully reversible and won't
    conflict with other internal constructs
    * Added a Rust test module for the DafnyRuntimeRust and wrote tests for
    testing the encoding Dafny/Rust
    
    # Tooling improvement
    
    - The makefile now works even if there is a space in the path
    - `make dfydev` does not perform verification and formatting of the
    resulting C# file, but `make dfyprod` does it. Both build Dafny
    afterwards
    ' `make dfydevinit` and `make dfyprodinit` only perform the conversion
    from dafny files to C# files, without rebuilding Dafny.
     
    
    
    <small>By submitting this pull request, I confirm that my contribution
    is made under the terms of the [MIT
    license](https://github.com/dafny-lang/dafny/blob/master/LICENSE.txt).</small>

--- Source/DafnyCore/DafnyGeneratedFromDafny.sh
+++ Source/DafnyCore/DafnyGeneratedFromDafny.sh
@@ -10,6 +10,22 @@
 #       <PackageReference Include="Microsoft.Build.Utilities.Core" Version="16.5.0" PrivateAssets="All" />
 #       <PackageReference Include="System.Linq.Parallel" Version="4.3.0" PrivateAssets="All" />
 
+# If the argument --no-verify is passed to the script, we pop it and will pass it to the dafny command below
+if [ "$#" != 0 ] && [ "$1" == "--no-verify" ]; then
+  noverify="--no-verify"
+  shift
+else
+  noverify=""
+fi
+
+# If the argument --no-format is passed to the script, we pop it and won't format the resulting cs
+if [ "$#" != 0 ] && [ "$1" == "--no-format" ]; then
+  noformat="true"
+  shift
+else
+  noformat="false"
+fi
+  
 # If an argument is passed to the script, store it in this variable. Otherwise use the default "GeneratedFromDafny.cs"
 # Something like output = if no arguments then  "GeneratedFromDafny.cs" else first argument
 
@@ -19,13 +35,20 @@ else
   output="GeneratedFromDafny"
 fi
 
-../../Scripts/dafny translate cs dfyconfig.toml --output $output.cs
+../../Scripts/dafny translate cs dfyconfig.toml --output $output.cs $noverify
+
+# We will remove all the namespaces Std.* except Std.Wrappers
 python3 -c "
 import re
 with open ('$output.cs', 'r' ) as f:
   content = f.read()
-  content_new = re.sub('\\[assembly[\\s\\S]*?(?=namespace Formatting)|namespace\\s+\\w+\\s*\\{\\s*\\}\\s*//.*|_\\d_', '', content, flags = re.M)
+  content_trimmed = re.sub('\\[assembly[\\s\\S]*?(?=namespace Formatting)|namespace\\s+\\w+\\s*\\{\\s*\\}\\s*//.*|_\\d_', '', content, flags = re.M)
+  content_new = re.sub('\\r?\\nnamespace\\s+(Std\\.(?!Wrappers)(?!Strings)(?!Collections.Seq)(?!Arithmetic)(?!Math)\\S+)\\s*\\{[\\s\\S]*?\\}\\s*// end of namespace \\\\1', '', content_trimmed, flags = re.M)
+
 with open('$output.cs', 'w') as w:
   w.write(content_new)
 "
-dotnet format whitespace --include $output.cs
+
+if [ "$noformat" == "false" ]; then
+  dotnet format whitespace --include $output.cs 
+fi
