c66a3889bc47dd0bb147ea4312d5a8fc2c27f7de
Remy Willems <rwillems@amazon.com>
2024-02-26 11:37:09 +0100

    Fix bugs in CombinedWriter async methods

--- Source/XUnitExtensions/Lit/CombinedWriter.cs
+++ Source/XUnitExtensions/Lit/CombinedWriter.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace XUnitExtensions.Lit;
@@ -45,5 +47,22 @@ class CombinedWriter : TextWriter {
 
   public override Task WriteAsync(char[] buffer, int index, int count) {
     return Task.WhenAll(writers.Select(w => w.WriteAsync(buffer, index, count)));
+  }
+
+  public override Task WriteLineAsync(char[] buffer, int index, int count) {
+    return Task.WhenAll(writers.Select(w => w.WriteLineAsync(buffer, index, count)));
+
+  }
+
+  public override Task WriteLineAsync(string? value) {
+    return Task.WhenAll(writers.Select(w => w.WriteLineAsync(value)));
+  }
+
+  public override Task WriteLineAsync(char value) {
+    return Task.WhenAll(writers.Select(w => w.WriteLineAsync(value)));
+  }
+
+  public override Task WriteLineAsync(ReadOnlyMemory<char> buffer, CancellationToken cancellationToken = new()) {
+    return Task.WhenAll(writers.Select(w => w.WriteLineAsync(buffer, cancellationToken)));
   }
 }
