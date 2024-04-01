10b59f558f090bcaee7a9488778660df1c5b8ad1
Mikaël Mayer <MikaelMayer@users.noreply.github.com>
2024-04-01 15:20:59 -0500

    Fix: Ability to cast a datatype to its trait when overriding functions (#4824)
    This PR fixes #4823
    There were two issues there:
    - The function override check was casting to ClassLikeDecl, when it
    should now be TopLevelDeclWithMembers as IndDatatypeDecl can also
    override trait declarations.
    - The generated axiom used to rely on types being unboxed, but since now
    we might compare a trait with a datatype value, we need to ensure
    correct boxing for Boogie to type-check.
    I added the corresponding test.
    
    <small>By submitting this pull request, I confirm that my contribution
    is made under the terms of the [MIT
    license](https://github.com/dafny-lang/dafny/blob/master/LICENSE.txt).</small>

--- Source/DafnyCore/DooFile.cs
+++ Source/DafnyCore/DooFile.cs
@@ -54,7 +54,7 @@ public class DooFile {
     }
 
     public void Write(TextWriter writer) {
-      writer.Write(Toml.FromModel(this, new TomlModelOptions()));
+      writer.Write(Toml.FromModel(this, new TomlModelOptions()).Replace("\r\n", "\n"));
     }
   }
 
@@ -103,7 +103,9 @@ public class DooFile {
   }
 
   public DooFile(Program dafnyProgram) {
-    var tw = new StringWriter();
+    var tw = new StringWriter {
+      NewLine = "\n"
+    };
     var pr = new Printer(tw, ProgramSerializationOptions, PrintModes.Serialization);
     // afterResolver is false because we don't yet have a way to safely skip resolution
     // when reading the program back into memory.
@@ -191,7 +193,7 @@ public class DooFile {
     var manifestWr = wr.NewFile(ManifestFileEntry);
     using var manifestWriter = new StringWriter();
     Manifest.Write(manifestWriter);
-    manifestWr.Write(manifestWriter.ToString());
+    manifestWr.Write(manifestWriter.ToString().Replace("\r\n", "\n"));
 
     var programTextWr = wr.NewFile(ProgramFileEntry);
     programTextWr.Write(ProgramText);
