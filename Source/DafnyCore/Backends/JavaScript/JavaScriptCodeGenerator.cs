3e38580b55ec29c09abf43ee214a1081741f6e23
Mikaël Mayer <MikaelMayer@users.noreply.github.com>
2024-04-26 12:09:25 -0500

    Feat: Rust operators and immutable collections (#5081)
    ### Description
    This PR implements most immutable types in Dafny (Sequence, Set,
    Multiset, Map) and implements most if not all operators on them. It also
    sets an architecture that can now be relied on.
    List of features in this PR:
    - More comprehensive Rust AST, precedence and associativity to render
    parentheses
    - The Rust compiler no longer crash when not in /runAllTests, but emits
    invalid code. It makes it possible to see which features are missing by
    searching for `<i>` in the generated code.
    - Made verification of GenExpr less brittle
    - Previous PR review comments
    - Ability to use shadowed identifiers to avoid numbers in names. Rust
    has the same rules as Dafny for shadowing, and I will continue to
    monitor cases where semantics might differ.
    
    ### How has this been tested?
    I added an integration test for compiling Dafny to Rust, and I added
    unit tests for the runtime in Rust
    *All tests for each compiler now have a `.rs.check` if they fail.*.
    
    <small>By submitting this pull request, I confirm that my contribution
    is made under the terms of the [MIT
    license](https://github.com/dafny-lang/dafny/blob/master/LICENSE.txt).</small>
    
    ---------
    
    Co-authored-by: Robin Salkeld <salkeldr@amazon.com>

--- Source/DafnyCore/Backends/JavaScript/JavaScriptCodeGenerator.cs
+++ Source/DafnyCore/Backends/JavaScript/JavaScriptCodeGenerator.cs
@@ -2570,9 +2570,10 @@ namespace Microsoft.Dafny.Compilers {
       return termLeftWriter;
     }
 
-    protected override string GetCollectionBuilder_Build(CollectionType ct, IToken tok, string collName, ConcreteSyntaxTree wr) {
+    protected override void GetCollectionBuilder_Build(CollectionType ct, IToken tok, string collName,
+      ConcreteSyntaxTree wr, ConcreteSyntaxTree wStmt) {
       // collections are built in place
-      return collName;
+      wr.Write(collName);
     }
 
     protected override void EmitSingleValueGenerator(Expression e, bool inLetExprBody, string type, ConcreteSyntaxTree wr, ConcreteSyntaxTree wStmts) {
