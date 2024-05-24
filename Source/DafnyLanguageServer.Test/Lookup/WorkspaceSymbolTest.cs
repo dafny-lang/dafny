0d83f8dfa900ec5a2f718026240905f7b3c3a0d4
Remy Willems <rwillems@amazon.com>
2024-05-24 13:04:07 +0200

    No longer show generated symbols as workspace symbols (#5484)
    ### Description
    - No longer show generated symbols as workspace symbols
    
    ### How has this been tested?
    - Added more assertions to test `AllRelevantSymbolKindsDetected`
    
    <small>By submitting this pull request, I confirm that my contribution
    is made under the terms of the [MIT
    license](https://github.com/dafny-lang/dafny/blob/master/LICENSE.txt).</small>

--- Source/DafnyLanguageServer.Test/Lookup/WorkspaceSymbolTest.cs
+++ Source/DafnyLanguageServer.Test/Lookup/WorkspaceSymbolTest.cs
@@ -51,11 +51,13 @@ namespace Microsoft.Dafny.LanguageServer.IntegrationTest.Lookup {
     [Fact]
     public async Task AllRelevantSymbolKindsDetected() {
       var documentItem = await GetDocumentItem(@"
+
 class TestClass {}
 
 module TestModule {}
 
-function TestFunction(): int { 42 }
+function TransparentTestFunction(): int { 42 }
+opaque function OpaqueTestFunction(): int { 42 }
 
 method TestMethod() returns (x: int) {
   x := 42;
@@ -72,18 +74,25 @@ predicate TestPredicate() { false }", "test-workspace-symbols.dfy", false);
       var testSymbols = new List<string> {
         "TestClass",
         "TestModule",
-        "TestFunction",
+        "TestMethod",
+        "TransparentTestFunction",
+        "OpaqueTestFunction",
         "TestDatatype",
         "TestConstructor",
         "TestTrait",
         "TestPredicate",
       };
 
-      var response = await client.RequestWorkspaceSymbols(new WorkspaceSymbolParams {
+      var allResponse = await client.RequestWorkspaceSymbols(new WorkspaceSymbolParams {
         Query = "test"
       });
+      Assert.Equal(testSymbols.Count, allResponse.Count());
       foreach (var testSymbol in testSymbols) {
-        Assert.True(response.Any(symbol => symbol.Name.Contains(testSymbol)),
+        var singleResponse = await client.RequestWorkspaceSymbols(new WorkspaceSymbolParams() {
+          Query = testSymbol
+        });
+        Assert.Single(singleResponse);
+        Assert.True(allResponse.Count(symbol => symbol.Name == testSymbol) == 1,
           $"Could not find {testSymbol}");
       }
     }
