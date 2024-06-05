126344a91f64b8f361ba744efe660ee9f8883824
Remy Willems <rwillems@amazon.com>
2024-06-05 12:52:59 +0200

    Reintroduce flattening for Java

--- Source/DafnyCore/Backends/Java/JavaCodeGenerator.cs
+++ Source/DafnyCore/Backends/Java/JavaCodeGenerator.cs
@@ -4322,5 +4322,19 @@ namespace Microsoft.Dafny.Compilers {
       catchBlock.WriteLine($"dafny.DafnySequence<Character> {haltMessageVarName} = dafny.DafnySequence.asString(e.getMessage());");
       TrStmt(recoveryBody, catchBlock);
     }
+
+    protected override void EmitNestedMatchExpr(NestedMatchExpr match, bool inLetExprBody, ConcreteSyntaxTree output,
+      ConcreteSyntaxTree wStmts) {
+      EmitExpr(match.Flattened, inLetExprBody, output, wStmts);
+    }
+
+    protected override void TrOptNestedMatchExpr(NestedMatchExpr match, Type resultType, ConcreteSyntaxTree wr, ConcreteSyntaxTree wStmts,
+      bool inLetExprBody, IVariable accumulatorVar) {
+      TrExprOpt(match.Flattened, resultType, wr, wStmts, inLetExprBody, accumulatorVar);
+    }
+
+    protected override void EmitNestedMatchStmt(NestedMatchStmt match, ConcreteSyntaxTree writer) {
+      TrStmt(match.Flattened, writer);
+    }
   }
 }
