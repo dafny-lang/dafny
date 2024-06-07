f03d947aaba454c2b9928e62f034e91f06e6ce82
Rustan Leino <leino@amazon.com>
2024-06-07 16:55:27 -0700

    fix: Emit tytagConstants only after all of them have been generated (via AddTraitParentAxioms)

--- Source/DafnyCore/Verifier/BoogieGenerator.cs
+++ Source/DafnyCore/Verifier/BoogieGenerator.cs
@@ -803,14 +803,14 @@ namespace Microsoft.Dafny {
 
       filterOnlyMembers = false;
 
+      AddTraitParentAxioms();
+
       foreach (var c in tytagConstants.Values) {
         sink.AddTopLevelDeclaration(c);
       }
       foreach (var c in fieldConstants.Values) {
         sink.AddTopLevelDeclaration(c);
       }
-
-      AddTraitParentAxioms();
 
       if (InsertChecksums) {
         foreach (var impl in sink.Implementations) {
