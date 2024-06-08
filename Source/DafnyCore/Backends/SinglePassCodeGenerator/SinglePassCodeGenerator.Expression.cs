897732930952693b705c5cd47a5f317048bf7c67
Rustan Leino <leino@amazon.com>
2024-06-08 15:51:11 -0700

    Fix merge

--- Source/DafnyCore/Backends/SinglePassCodeGenerator/SinglePassCodeGenerator.Expression.cs
+++ Source/DafnyCore/Backends/SinglePassCodeGenerator/SinglePassCodeGenerator.Expression.cs
@@ -295,6 +295,10 @@ namespace Microsoft.Dafny.Compilers {
               var w = EmitCoercionIfNecessary(e.E.Type, e.ToType, e.tok, wr);
               w = EmitDowncastIfNecessary(e.E.Type, e.ToType, e.tok, w);
               EmitExpr(e.E, inLetExprBody, w, wStmts);
+            } else if (e.E.Type.IsSubtypeOf(e.ToType, false, false)) {
+              // conversion is a no-op -- almost, because it may need a cast to deal with bounded type parameters
+              var w = EmitDowncastIfNecessary(e.E.Type, e.ToType, e.tok, wr);
+              EmitExpr(e.E, inLetExprBody, w, wStmts);
             } else {
               EmitConversionExpr(e.E, fromType, toType, inLetExprBody, wr, wStmts);
             }
