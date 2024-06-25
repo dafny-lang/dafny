802d6f8ff6d8c71791289d3d5dc582f9de833992
Mikael Mayer <mimayere@amazon.com>
2024-06-25 06:09:36 -0500

    Chore: Internal backends should not fail CI Until the Rust code generator is finished, we shouldn't block everyone else's work by requiring .rs.check files or even update them every time, since the process is manual and very lengthy

--- Source/TestDafny/MultiBackendTest.cs
+++ Source/TestDafny/MultiBackendTest.cs
@@ -423,6 +423,12 @@ public class MultiBackendTest {
       }
 
       await output.WriteLineAsync(diffMessage);
+      if (backend.IsInternal) {
+        await output.WriteLineAsync(
+          $"(non-blocking) The '{backend.TargetName}' code generator is internal. Not having a '*.{backend.TargetName}.check' file is acceptable for now.");
+        return 0;
+      }
+
       return 1;
     }
 
@@ -456,9 +462,25 @@ public class MultiBackendTest {
         }
       }
 
+      if (backend.IsInternal && checkResult != 0) {
+        await output.WriteLineAsync(
+          $"(non-blocking) The '{backend.TargetName}' code generator is internal. An unmatched '*.{backend.TargetName}.check' file is acceptable for now.");
+        return 0;
+      }
+
       return checkResult;
     }
 
+
+    if (backend.IsInternal) {
+      await output.WriteLineAsync($"(non-blocking) Execution failed for the internal code generator to {backend.TargetName}, for reasons other than known unsupported features. Output:");
+      await output.WriteLineAsync(outputString);
+      await output.WriteLineAsync("Error:");
+      await output.WriteLineAsync(error);
+      await output.WriteLineAsync(
+        $"The '{backend.TargetName}' code generator is internal. An unmatched '*.{backend.TargetName}.check' file is acceptable for now.");
+      return 0;
+    }
     await output.WriteLineAsync("Execution failed, for reasons other than known unsupported features. Output:");
     await output.WriteLineAsync(outputString);
     await output.WriteLineAsync("Error:");
