3553e8c76f86fcda5af64dfc4d003b7cd9881646
Mikael Mayer <mimayere@amazon.com>
2024-08-06 11:37:14 -0500

    Fixed CI

--- Source/DafnyCore/Backends/Dafny/DafnyCodeGenerator.cs
+++ Source/DafnyCore/Backends/Dafny/DafnyCodeGenerator.cs
@@ -814,7 +814,7 @@ namespace Microsoft.Dafny.Compilers {
         ? (Option<DAST.Type>)Option<DAST.Type>.create_Some(GenType(replacementReceiver.Type))
         : (Option<DAST.Type>)Option<DAST.Type>.create_None();
       if (receiverBeforeName) {
-        var name = replacementReceiver is IdentifierExpr { Var: var variable } && variable.CompileName(enclosingDeclaration.CompilationIdGenerator) is var compileName
+        var name = replacementReceiver is IdentifierExpr { Var: { } variable } && variable.CompileName(enclosingDeclaration.CompilationIdGenerator) is var compileName
           ? compileName
           : "receiver";
         receiverArg = (Option<_IFormal>)Option<DAST._IFormal>.create_Some(
