bb0bdd26b0596eb440c635e5ffc6eda096c51c67
Remy Willems <rwillems@amazon.com>
2024-09-06 13:54:10 +0200

    Add resolution to OpaqueBlock

--- Source/DafnyCore/AST/Statements/OpaqueBlock.cs
+++ Source/DafnyCore/AST/Statements/OpaqueBlock.cs
@@ -12,10 +12,31 @@ public class OpaqueBlock : BlockStmt {
     Modifies = cloner.CloneSpecFrameExpr(original.Modifies);
   }
 
-  public OpaqueBlock(RangeToken rangeToken, List<Statement> body, 
-    List<AttributedExpression> ensures, 
+  public OpaqueBlock(RangeToken rangeToken, List<Statement> body,
+    List<AttributedExpression> ensures,
     Specification<FrameExpression> modifies) : base(rangeToken, body) {
     Ensures = ensures;
     Modifies = modifies;
+  }
+
+  public override void GenResolve(INewOrOldResolver resolver, ResolutionContext resolutionContext) {
+
+    resolver.Scope.PushMarker();
+    foreach (Statement ss in Body) {
+      resolver.ResolveStatementWithLabels(ss, resolutionContext);
+    }
+    resolver.Scope.PopMarker();
+
+    resolver.ResolveAttributes(Modifies, resolutionContext);
+    foreach (var fe in Modifies.Expressions) {
+      resolver.ResolveFrameExpression(fe, FrameExpressionUse.Modifies, resolutionContext);
+    }
+
+    foreach (var e in Ensures) {
+      resolver.ResolveAttributes(e, resolutionContext);
+      resolver.ResolveExpression(e.E, resolutionContext);
+      resolver.ConstrainTypeExprBool(e.E, "Postcondition must be a boolean (got {0})");
+    }
+    base.GenResolve(resolver, resolutionContext);
   }
 }
