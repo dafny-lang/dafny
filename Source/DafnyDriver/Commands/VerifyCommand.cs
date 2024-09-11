65968ed223b966e06085d153e12e748727d795ba
Remy Willems <rwillems@amazon.com>
2024-09-11 14:32:38 +0200

    Refactor

--- Source/DafnyDriver/Commands/VerifyCommand.cs
+++ Source/DafnyDriver/Commands/VerifyCommand.cs
@@ -92,17 +92,15 @@ Extract Dafny types, functions, and lemmas to Boogie.
       await verificationResultsLogged;
       await proofDependenciesReported;
 
-      if (resolution.HasErrors || options.BoogieExtractionTargetFile == null) {
-        return await compilation.GetAndReportExitCode();
-      }
-
-      using var engine = ExecutionEngine.CreateWithoutSharedCache(options);
-      try {
-        var extractedProgram = BoogieExtractor.Extract(resolution.ResolvedProgram);
-        engine.PrintBplFile(options.BoogieExtractionTargetFile, extractedProgram, true, pretty: true);
-      } catch (ExtractorError extractorError) {
-        await options.OutputWriter.WriteLineAsync($"Boogie axiom extraction error: {extractorError.Message}");
-        return 1;
+      if (!resolution.HasErrors && options.BoogieExtractionTargetFile != null) {
+        using var engine = ExecutionEngine.CreateWithoutSharedCache(options);
+        try {
+          var extractedProgram = BoogieExtractor.Extract(resolution.ResolvedProgram);
+          engine.PrintBplFile(options.BoogieExtractionTargetFile, extractedProgram, true, pretty: true);
+        } catch (ExtractorError extractorError) {
+          await options.OutputWriter.WriteLineAsync($"Boogie axiom extraction error: {extractorError.Message}");
+          return 1;
+        }
       }
     }
 
