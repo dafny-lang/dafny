7927fa6b4162a9a8a049c7ca839f6599ce2e47a2
Remy Willems <rwillems@amazon.com>
2024-10-07 16:25:09 +0200

    Remove obsolete comment

--- Source/DafnyCore/AST/Expressions/Conditional/NestedMatchExpr.cs
+++ Source/DafnyCore/AST/Expressions/Conditional/NestedMatchExpr.cs
@@ -29,9 +29,6 @@ public class NestedMatchExpr : Expression, ICloneable<NestedMatchExpr>, ICanForm
     this.Cases = original.Cases.Select(cloner.CloneNestedMatchCaseExpr).ToList();
     this.UsesOptionalBraces = original.UsesOptionalBraces;
 
-    // The method MakeAbstractSignature makes an unresolved clone
-    // And then does not apply the rewriters to this clone,
-    // so unless we copy the flattened field
     if (cloner.CloneResolvedFields) {
       Flattened = cloner.CloneExpr(original.Flattened);
     }
