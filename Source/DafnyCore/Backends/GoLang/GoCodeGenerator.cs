53284e6ee323750b78c6b5ed311eed1f19bdd56e
Remy Willems <rwillems@amazon.com>
2024-10-16 11:16:59 +0200

    Take out unused code

--- Source/DafnyCore/Backends/GoLang/GoCodeGenerator.cs
+++ Source/DafnyCore/Backends/GoLang/GoCodeGenerator.cs
@@ -1384,9 +1384,6 @@ namespace Microsoft.Dafny.Compilers {
         } else if (thisContext != null) {
           w = w.NewBlock("", open: BlockStyle.Brace);
           for (var i = 0; i < inParams.Count; i++) {
-            if (inParams[i].IsGhost) {
-              continue;
-            }
             var p = (overriddenInParams ?? inParams)[i];
             var instantiatedType = p.Type.Subst(thisContext.ParentFormalTypeParametersToActuals);
             if (!p.IsGhost && !instantiatedType.Equals(p.Type)) {
