c251ca79357450f2b3eb450753f188d7bc2915b0
Remy Willems <rwillems@amazon.com>
2024-12-11 12:41:15 +0100

    Move RangeNode to separate class

--- Source/DafnyCore/Generic/Node.cs
+++ Source/DafnyCore/Generic/Node.cs
@@ -201,21 +201,3 @@ public abstract class Node : INode {
     return trivia is not ("" or null);
   }
 }
-
-public abstract class RangeNode : Node { // TODO merge into Node when TokenNode is gone.
-
-  public override IOrigin Tok => StartToken; // TODO rename to ReportingToken in separate PR
-
-  public IOrigin tok => Tok; // TODO replace with Tok in separate PR
-
-  // TODO rename to Range in separate PR
-  public override RangeToken Origin { get; set; } // TODO remove setter when TokenNode is gone.
-
-  protected RangeNode(Cloner cloner, RangeNode original) {
-    Origin = cloner.Tok(original.Origin);
-  }
-
-  protected RangeNode(RangeToken rangeOrigin) {
-    Origin = rangeOrigin;
-  }
-}
---
+++ Source/DafnyCore/Generic/RangeNode.cs
@@ -0,0 +1,19 @@
+namespace Microsoft.Dafny;
+
+public abstract class RangeNode : Node { // TODO merge into Node when TokenNode is gone.
+
+  public override IOrigin Tok => StartToken; // TODO rename to ReportingToken in separate PR
+
+  public IOrigin tok => Tok; // TODO replace with Tok in separate PR
+
+  // TODO rename to Range in separate PR
+  public override RangeToken Origin { get; set; } // TODO remove setter when TokenNode is gone.
+
+  protected RangeNode(Cloner cloner, RangeNode original) {
+    Origin = cloner.Tok(original.Origin);
+  }
+
+  protected RangeNode(RangeToken rangeOrigin) {
+    Origin = rangeOrigin;
+  }
+}
