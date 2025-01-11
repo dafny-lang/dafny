487f0614ae1e9d033f6887b57da34391f572008f
Rustan Leino <leino@amazon.com>
2025-01-10 18:26:31 -0800

    chore: Improve C#

--- Source/DafnyCore/Resolver/ExtremeLemmaChecks_Visitor.cs
+++ Source/DafnyCore/Resolver/ExtremeLemmaChecks_Visitor.cs
@@ -11,26 +11,24 @@ class ExtremeLemmaChecks_Visitor : ResolverBottomUpVisitor {
     this.context = context;
   }
   protected override void VisitOneStmt(Statement stmt) {
-    if (stmt is CallStmt) {
-      var s = (CallStmt)stmt;
-      if (s.Method is ExtremeLemma || s.Method is PrefixLemma) {
+    if (stmt is CallStmt callStmt) {
+      if (callStmt.Method is ExtremeLemma or PrefixLemma) {
         // all is cool
       } else {
         // the call goes from an extreme lemma context to a non-extreme-lemma callee
-        if (ModuleDefinition.InSameSCC(context, s.Method)) {
+        if (ModuleDefinition.InSameSCC(context, callStmt.Method)) {
           // we're looking at a recursive call (to a non-extreme-lemma)
-          resolver.reporter.Error(MessageSource.Resolver, s.Origin, "a recursive call from a {0} can go only to other {0}s and prefix lemmas", context.WhatKind);
+          resolver.reporter.Error(MessageSource.Resolver, callStmt.Origin, "a recursive call from a {0} can go only to other {0}s and prefix lemmas", context.WhatKind);
         }
       }
     }
   }
   protected override void VisitOneExpr(Expression expr) {
-    if (expr is FunctionCallExpr) {
-      var e = (FunctionCallExpr)expr;
+    if (expr is FunctionCallExpr callExpr) {
       // the call goes from a greatest lemma context to a non-greatest-lemma callee
-      if (ModuleDefinition.InSameSCC(context, e.Function)) {
+      if (ModuleDefinition.InSameSCC(context, callExpr.Function)) {
         // we're looking at a recursive call (to a non-greatest-lemma)
-        resolver.reporter.Error(MessageSource.Resolver, e.Origin, "a recursive call from a greatest lemma can go only to other greatest lemmas and prefix lemmas");
+        resolver.reporter.Error(MessageSource.Resolver, callExpr.Origin, "a recursive call from a greatest lemma can go only to other greatest lemmas and prefix lemmas");
       }
     }
   }
