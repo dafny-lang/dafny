8f4284f72e2a4ad93ba79917bd311c9138122fc8
Rustan Leino <leino@amazon.com>
2025-01-14 13:48:47 -0800

    Unnested if’s

--- Source/DafnyCore/Resolver/DetectUnsoundFunctionReferencesVisitor.cs
+++ Source/DafnyCore/Resolver/DetectUnsoundFunctionReferencesVisitor.cs
@@ -13,25 +13,21 @@ class DetectUnsoundFunctionReferencesVisitor : ResolverBottomUpVisitor {
   }
 
   protected override void VisitOneExpr(Expression expr) {
-    if (!DoDecreasesChecks && expr is MemberSelectExpr { Member: Function fn }) {
-      if (ModuleDefinition.InSameSCC(context, fn)) {
-        resolver.reporter.Error(MessageSource.Resolver, expr.Origin,
-          "cannot use naked function in recursive setting. Possible solution: eta expansion.");
-      }
+    if (!DoDecreasesChecks && expr is MemberSelectExpr { Member: Function fn } && ModuleDefinition.InSameSCC(context, fn)) {
+      resolver.reporter.Error(MessageSource.Resolver, expr.Origin,
+        "cannot use naked function in recursive setting. Possible solution: eta expansion.");
     }
 
-    if (DoDecreasesChecks && expr is FunctionCallExpr callExpr) {
-      if (ModuleDefinition.InSameSCC(context, callExpr.Function)) {
-        string msg;
-        if (context == callExpr.Function) {
-          msg = "a decreases clause is not allowed to call the enclosing function";
-        } else {
-          msg = $"the decreases clause of {context.WhatKind} '{context.NameRelativeToModule}' is not allowed to call '{callExpr.Function}', " +
-                "because they are mutually recursive";
-        }
-
-        resolver.reporter.Error(MessageSource.Resolver, callExpr.Origin, msg);
+    if (DoDecreasesChecks && expr is FunctionCallExpr callExpr && ModuleDefinition.InSameSCC(context, callExpr.Function)) {
+      string msg;
+      if (context == callExpr.Function) {
+        msg = "a decreases clause is not allowed to call the enclosing function";
+      } else {
+        msg = $"the decreases clause of {context.WhatKind} '{context.NameRelativeToModule}' is not allowed to call '{callExpr.Function}', " +
+              "because they are mutually recursive";
       }
+
+      resolver.reporter.Error(MessageSource.Resolver, callExpr.Origin, msg);
     }
   }
 }
