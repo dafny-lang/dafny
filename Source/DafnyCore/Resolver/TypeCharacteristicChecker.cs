852e2c48e9daa458bdafbc5a65efb0e5a0c23600
Mikael Mayer <mimayere@amazon.com>
2025-01-15 07:34:25 -0600

    code review

--- Source/DafnyCore/Resolver/TypeCharacteristicChecker.cs
+++ Source/DafnyCore/Resolver/TypeCharacteristicChecker.cs
@@ -41,22 +41,13 @@ namespace Microsoft.Dafny {
             foreach (var tp in dt.TypeArgs) {
               if (tp.Characteristics.EqualitySupport == TypeParameter.EqualitySupportValue.Unspecified) {
                 // here's our chance to infer the need for equality support
-                foreach (var ctor in dt.Ctors) {
-                  foreach (var arg in ctor.Formals) {
-                    if (InferAndSetEqualitySupport(tp, arg.Type, reporter)) {
-                      inferredSomething = true;
-                      goto DONE_DT; // break out of the doubly-nested loop
-                    }
-                  }
-                }
-
-                foreach (var parentType in dt.ParentTraits) {
-                  if (InferAndSetEqualitySupport(tp, parentType, reporter)) {
-                    inferredSomething = true;
-                    goto DONE_DT; // break out of the doubly-nested loop
-                  }
-                }
-              DONE_DT:;
+                inferredSomething = dt.Ctors.Any(ctor =>
+                  ctor.Formals.Any(arg =>
+                    InferAndSetEqualitySupport(tp, arg.Type, reporter)
+                  )
+                ) || dt.ParentTraits.Any(parentType =>
+                  InferAndSetEqualitySupport(tp, parentType, reporter)
+                );
               }
             }
           } else if (d is TypeSynonymDecl syn) {
