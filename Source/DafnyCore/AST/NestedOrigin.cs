e140175737c0202057020ffcbdbbfa3bc39d9efb
Remy Willems <rwillems@amazon.com>
2025-03-06 11:59:52 +0100

    Remove default message

--- Source/DafnyCore/AST/NestedOrigin.cs
+++ Source/DafnyCore/AST/NestedOrigin.cs
@@ -3,7 +3,11 @@ using System.Diagnostics.Contracts;
 namespace Microsoft.Dafny;
 
 public class NestedOrigin : OriginWrapper {
-  public NestedOrigin(IOrigin outer, IOrigin inner, string message = "this proposition could not be proved")
+  public NestedOrigin(IOrigin outer, IOrigin inner)
+    : this(outer, inner, null) {
+  }
+
+  public NestedOrigin(IOrigin outer, IOrigin inner, string message)
     : base(outer) {
     Contract.Requires(outer != null);
     Contract.Requires(inner != null);
