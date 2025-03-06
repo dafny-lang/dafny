8e8ff82cc89e879c8433f6da913d33a63b007a38
Remy Willems <rwillems@amazon.com>
2025-03-06 11:53:38 +0100

    Bring back commented code

--- Source/DafnyCore/Triggers/QuantifierSplitter.cs
+++ Source/DafnyCore/Triggers/QuantifierSplitter.cs
@@ -75,25 +75,30 @@ namespace Microsoft.Dafny.Triggers {
       var binary = body as BinaryExpr;
 
       if (quantifier is ForallExpr) {
-        IEnumerable<Expression> stream;
+        IReadOnlyList<Expression> stream;
         if (binary != null && (binary.Op == BinaryExpr.Opcode.Imp || binary.Op == BinaryExpr.Opcode.Or)) {
-          stream = SplitAndStitch(binary, BinaryExpr.Opcode.And);
+          stream = SplitAndStitch(binary, BinaryExpr.Opcode.And).ToList();
         } else {
-          stream = SplitExpr(body, BinaryExpr.Opcode.And);
+          stream = SplitExpr(body, BinaryExpr.Opcode.And).ToList();
         }
+
         foreach (var e in stream) {
-          var tok = quantifier.Origin; //new NestedOrigin(quantifier.Origin, e.Origin, "in subexpression at");
+          var tok = stream.Count > 1
+            ? new NestedOrigin(quantifier.Origin, e.Origin, "in subexpression at")
+            : quantifier.Origin;
           yield return new ForallExpr(tok, quantifier.BoundVars, quantifier.Range, e, TriggerUtils.CopyAttributes(quantifier.Attributes)) { Type = quantifier.Type, Bounds = quantifier.Bounds };
         }
       } else if (quantifier is ExistsExpr) {
-        IEnumerable<Expression> stream;
+        IReadOnlyList<Expression> stream;
         if (binary != null && binary.Op == BinaryExpr.Opcode.And) {
-          stream = SplitAndStitch(binary, BinaryExpr.Opcode.Or);
+          stream = SplitAndStitch(binary, BinaryExpr.Opcode.Or).ToList();
         } else {
-          stream = SplitExpr(body, BinaryExpr.Opcode.Or);
+          stream = SplitExpr(body, BinaryExpr.Opcode.Or).ToList();
         }
         foreach (var e in stream) {
-          var tok = quantifier.Origin; //body?.Origin == e.Origin ? quantifier.Origin : new NestedOrigin(quantifier.Origin, e.Origin, "in subexpression at");
+          var tok = body?.Origin == e.Origin || stream.Count == 1
+            ? quantifier.Origin
+            : new NestedOrigin(quantifier.Origin, e.Origin, "in subexpression at");
           yield return new ExistsExpr(tok, quantifier.BoundVars, quantifier.Range, e, TriggerUtils.CopyAttributes(quantifier.Attributes)) { Type = quantifier.Type, Bounds = quantifier.Bounds };
         }
       } else {
