aedada37affee70e0a847bd041d645ce1f823c07
Robin Salkeld <salkeldr@amazon.com>
2025-05-14 16:26:33 -0700

    Funamental change, let’s see what breaks :)

--- Source/DafnyCore/Backends/InternalBackendsPluginConfiguration.cs
+++ Source/DafnyCore/Backends/InternalBackendsPluginConfiguration.cs
@@ -8,13 +8,7 @@ internal class InternalBackendsPluginConfiguration : Plugins.PluginConfiguration
   public override IExecutableBackend[] GetCompilers(DafnyOptions options) {
     return [
       new CsharpBackend(options),
-      new JavaScriptBackend(options),
-      new GoBackend(options),
-      new JavaBackend(options),
-      new PythonBackend(options),
-      new CppBackend(options),
       new LibraryBackend(options),
-      new RustBackend(options),
       new ResolvedDesugaredExecutableDafnyBackend(options)
     ];
   }
