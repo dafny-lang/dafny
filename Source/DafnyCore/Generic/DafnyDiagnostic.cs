1a53c7d39160cad7df723a70618c068fb9711375
Remy Willems <rwillems@amazon.com>
2025-05-29 17:49:44 +0200

    Fixes

--- Source/DafnyCore/Generic/DafnyDiagnostic.cs
+++ Source/DafnyCore/Generic/DafnyDiagnostic.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Reflection;
 using System.Resources;
 using System.Text.RegularExpressions;
+using Microsoft.Boogie;
 
 namespace Microsoft.Dafny;
 
@@ -17,8 +18,13 @@ public record DafnyDiagnostic(MessageSource Source, string ErrorId, TokenRange R
   private static readonly IDictionary<string, string> MessageIdToMessage = new Dictionary<string, string>();
 
   static DafnyDiagnostic() {
+    InitializeMessageIdToMessage();
+  }
+
+  private static void InitializeMessageIdToMessage()
+  {
     var assembly = Assembly.GetExecutingAssembly();
-    using Stream stream = assembly.GetManifestResourceStream("messages.txt")!;
+    using Stream stream = assembly.GetManifestResourceStream("DafnyCore.assets.messages.txt")!;
     using StreamReader reader = new StreamReader(stream);
     while (true) {
       var line = reader.ReadLine();
@@ -31,23 +37,45 @@ public record DafnyDiagnostic(MessageSource Source, string ErrorId, TokenRange R
   }
 
   public static string[] ResolveMessageIds(IEnumerable<string> messageParts) {
-    return messageParts.Where(s => s.StartsWith('$')).Select(s => MessageIdToMessage[s]).ToArray();
+    return messageParts.Where(IsMessageId).Select(s => MessageIdToMessage[s]).ToArray();
   }
   
   public static string MessageFromParts(IReadOnlyList<string> messageParts) {
-    var stack = new Stack<string>(messageParts);
+    var stack = new Stack<string>(messageParts.Reversed());
     string MessageFromStack() {
       var current = stack.Pop();
-      var resolved = current.StartsWith('$') ? MessageIdToMessage[current] : current;
-      var argumentCount = CountArgumentsOfFormatMessage(resolved);
+      var resolved = IsMessageId(current) ? MessageIdToMessage[current] : current;
+      
+      // Escape braces that don't contain just numbers
+      var safeResolved = SafeFormat(resolved);
+      
+      var argumentCount = CountArgumentsOfFormatMessage(safeResolved);
       var arguments = new object[argumentCount];
       for (int index = 0; index < argumentCount; index++) {
         arguments[index] = MessageFromStack();
       }
 
-      return string.Format(resolved, arguments);
+      return string.Format(safeResolved, arguments);
     }
     return MessageFromStack();
+  }
+
+  private static string SafeFormat(string format)
+  {
+    // Replace { not followed by \d} with {{
+    string escaped = Regex.Replace(format, @"\{(?!\d\})", "{{");
+    
+    // Replace } not preceded by {\d with }}
+    return Regex.Replace(escaped, @"(?<!\{\d)\}", "}}");
+  }
+  private static bool IsNumericOnly(string text)
+  {
+    return !string.IsNullOrEmpty(text) && text.All(char.IsDigit);
+  }
+
+  private static bool IsMessageId(string current)
+  {
+    return current.StartsWith('$');
   }
 
   private static int CountArgumentsOfFormatMessage(string formatMessage) {
