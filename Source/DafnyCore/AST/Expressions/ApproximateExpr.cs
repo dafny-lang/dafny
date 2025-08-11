4734dfbf3d3e5b4894d384db43c64e11b45db710
Fabio Madge <fmadge@amazon.com>
2025-08-12 01:42:04 +0200

    review

--- Source/DafnyCore/AST/Expressions/ApproximateExpr.cs
+++ Source/DafnyCore/AST/Expressions/ApproximateExpr.cs
@@ -6,7 +6,7 @@ namespace Microsoft.Dafny;
 /// <summary>
 /// Represents an expression prefixed with ~, which is only valid for inexact fp64 literals.
 /// This allows the parser to accept ~expr for any expression, and the resolver will
-/// validate that it's only used on numeric literals.
+/// validate that it's only used on decimal literals.
 /// </summary>
 public class ApproximateExpr : ConcreteSyntaxExpression, ICloneable<ApproximateExpr> {
   public Expression Expr { get; }
@@ -18,7 +18,7 @@ public class ApproximateExpr : ConcreteSyntaxExpression, ICloneable<ApproximateE
     Expr = expr;
   }
 
-  public ApproximateExpr(Cloner cloner, ApproximateExpr original) : base(cloner, original) {
+  private ApproximateExpr(Cloner cloner, ApproximateExpr original) : base(cloner, original) {
     Expr = cloner.CloneExpr(original.Expr);
   }
 
@@ -30,9 +30,6 @@ public class ApproximateExpr : ConcreteSyntaxExpression, ICloneable<ApproximateE
     get {
       if (ResolvedExpression != null) {
         yield return ResolvedExpression;
-      } else {
-        // the expression hasn't yet been turned into a resolved expression, so use .Expr as the subexpression
-        yield return Expr;
       }
     }
   }
