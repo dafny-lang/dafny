813bb5d27820c832a5aa5f0cf429bc66bd128466
Mikael Mayer <mimayere@amazon.com>
2025-08-20 16:32:19 +0000

    Removed build files

--- Source/DafnyStandardLibraries/examples/SimpleTest-rust/target/debug/deps/num_rational-e9fde7c9ab28a1d1.d
+++
@@ -1,8 +0,0 @@
-/local/home/mimayere/dafny2/Source/DafnyStandardLibraries/examples/SimpleTest-rust/target/debug/deps/num_rational-e9fde7c9ab28a1d1.d: /home/mimayere/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-rational-0.4.2/src/lib.rs /home/mimayere/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-rational-0.4.2/src/pow.rs
-
-/local/home/mimayere/dafny2/Source/DafnyStandardLibraries/examples/SimpleTest-rust/target/debug/deps/libnum_rational-e9fde7c9ab28a1d1.rlib: /home/mimayere/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-rational-0.4.2/src/lib.rs /home/mimayere/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-rational-0.4.2/src/pow.rs
-
-/local/home/mimayere/dafny2/Source/DafnyStandardLibraries/examples/SimpleTest-rust/target/debug/deps/libnum_rational-e9fde7c9ab28a1d1.rmeta: /home/mimayere/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-rational-0.4.2/src/lib.rs /home/mimayere/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-rational-0.4.2/src/pow.rs
-
-/home/mimayere/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-rational-0.4.2/src/lib.rs:
-/home/mimayere/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-rational-0.4.2/src/pow.rs:
--- Source/DafnyStandardLibraries/examples/WorkingFileIOTest-rust/target/debug/deps/num-e0e9096b193508cd.d
+++
@@ -1,7 +0,0 @@
-/local/home/mimayere/dafny2/Source/DafnyStandardLibraries/examples/WorkingFileIOTest-rust/target/debug/deps/num-e0e9096b193508cd.d: /home/mimayere/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-0.4.3/src/lib.rs
-
-/local/home/mimayere/dafny2/Source/DafnyStandardLibraries/examples/WorkingFileIOTest-rust/target/debug/deps/libnum-e0e9096b193508cd.rlib: /home/mimayere/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-0.4.3/src/lib.rs
-
-/local/home/mimayere/dafny2/Source/DafnyStandardLibraries/examples/WorkingFileIOTest-rust/target/debug/deps/libnum-e0e9096b193508cd.rmeta: /home/mimayere/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-0.4.3/src/lib.rs
-
-/home/mimayere/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-0.4.3/src/lib.rs:
