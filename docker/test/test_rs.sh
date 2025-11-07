3149f367932c2c5a17a707b4aa5189f124d88e8f
Jeff Wong <42687471+jeff-yc-wong@users.noreply.github.com>
2025-11-07 13:04:33 -1000

    test: Fix bug with Rust test script

--- docker/test/test_rs.sh
+++ docker/test/test_rs.sh
@@ -16,7 +16,7 @@ echo "Dafny program verifier finished with 0 verified, 0 errors" >> $tmp
 echo "(42, 131)" >> $tmp
 
 echo -n "[Test 1/2] Running with Rust ..."
-if $DAFNY run -t:rs c.dfy | diff - $tmp; then
+if $DAFNY run -t:rs --enforce-determinism c.dfy | diff - $tmp; then
     echo -e "\r[Test 1/2] Running with Rust [\033[32m✓\033[0m]"
 else
     echo -e "\r[Test 1/2] Running with Rust [\033[31m✗\033[0m]"
@@ -31,7 +31,7 @@ echo "Dafny program verifier finished with 0 verified, 0 errors" >> $tmp
 echo "(42, 131)" > $tmpx
 
 echo -n "[Test 2/2] Building with Rust ..."
-if $DAFNY build -t:rs c.dfy | diff - $tmp && ./c-rust/target/debug/c | diff - $tmpx; then
+if $DAFNY build -t:rs --enforce-determinism c.dfy | diff - $tmp && ./c-rust/target/debug/c | diff - $tmpx; then
     echo -e "\r[Test 2/2] Building with Rust [\033[32m✓\033[0m]"
 else
     echo -e "\r[Test 2/2] Building with Rust [\033[31m✗\033[0m]"
